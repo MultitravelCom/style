e44890f4d2b73505e596908ec094bc7cc5761ace
Ignacio Peralta <112433497+ElOtroNach0@users.noreply.github.com>
2023-03-03 11:10:57 -0300

    c

--- Services/Componente.js
+++ Services/Componente.js
@@ -1,8 +1,8 @@
 
-function Componente() {
-    return(
-        <h1>Esto es un componente</h1>
-    )
+class Componente extends React.Componet {
+    render(){
+        return <h1>Esto es un componente</h1>
+    }
 }
 
 export default Componente;
