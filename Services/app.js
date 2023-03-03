faa164b07d393bc1f671ec458bbeff20ce681994
Ignacio Peralta <112433497+ElOtroNach0@users.noreply.github.com>
2023-03-02 22:09:09 -0300

    b

--- Services/app.js
+++ Services/app.js
@@ -1,7 +1,6 @@
-
 function Test(){
     return(
         <h1>Hola, que tal?</h1>
     )
 }
-ReactDOM.render(<Test />, document.querySelector("#root")) 
+ReactDOM.render(<Test />, document.querySelector("#root"));
