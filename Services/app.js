af31233c67957c4f4aac95ac092068f5d2f366e7
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-03-03 12:43:47 -0300

    c

--- Services/app.js
+++ Services/app.js
@@ -1,10 +1,7 @@
-import Componente from "https://multitravelcom.github.io/style/Services/Componente.js";
-
 function App() {
   return (
     <>
       <h1>Hola, que tal?</h1>
-      <Componente />
     </>
   );
 }
