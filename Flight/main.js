cb1ba99edf31e259b3d68eef77afaa51dc9dfa4f
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-03-06 17:47:34 -0300

    Update main.js

--- Flight/main.js
+++ Flight/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', ()=>{
     let divToMove = document.getElementsByClassName("checkbox")[0];
-    let divDestination = document.getElementsByClassName("searcher-row")[0];
+    let divDestination = document.getElementsByClassName("singledestination-only")[0];
 
     divDestination.appendChild(divToMove);
 });
