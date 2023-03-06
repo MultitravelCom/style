76c02b2b5ed79468ce1f98bad9b771778867283a
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-03-06 12:08:19 -0300

    Add files via upload

---
+++ Home/main.js
@@ -0,0 +1,6 @@
+// Resaltar solpa Fligh en la home.
+let solapaFlight = document.querySelector('.nav-elem')[1];
+
+document.addEventListener('DOMContentLoadedd', () =>{
+    solapaFlight.classList.toggle('active');
+});
