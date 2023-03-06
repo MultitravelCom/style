43169785ddf85f0aeb0546b173329d7bc723246c
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-03-06 13:05:57 -0300

    Update main.js

--- Home/main.js
+++ Home/main.js
@@ -1,6 +1,6 @@
 // Resaltar solpa Fligh en la home.
-let solapaFlight = document.querySelectorAll('ul li:nth-child(2)')[0]
+let solapaFlight = document.querySelectorAll('#main-menu-products li:nth-child(2)')[0]
 
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', function(){
     solapaFlight.classList.add('active');
 });
