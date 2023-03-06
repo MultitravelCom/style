f6c5b1c94d01fe9e1b8b5750dbd3c8ffbd18a2f8
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-03-06 16:12:20 -0300

    Update main.js

--- Home/main.js
+++ Home/main.js
@@ -1,5 +1,5 @@
 // Resaltar solpa Fligh en la home.
-windows.addEventListener("load", function() {
+window.addEventListener("load", function() {
   var nav = document.querySelector("#main-menu ul.nav");
   var firstLi = nav.querySelector("li:nth-child(2)");
   firstLi.classList.add("active");
