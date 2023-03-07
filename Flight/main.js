3316def95b5a06e36932bf3e98c1a5ddd707aa73
Multitravel.com <125476676+MultitravelCom@users.noreply.github.com>
2023-03-07 14:57:43 -0300

    Update main.js

--- Flight/main.js
+++ Flight/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+windows.addEventListener('DOMContentLoaded', ()=>{
 // Mover div fuera del mas opciones.
     let divToMove = document.getElementsByClassName("checkbox")[0];
     let divDestination = document.getElementsByClassName("searcher-row")[0];
@@ -6,8 +6,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     divDestination.appendChild(divToMove);
     
 //    Cambio de copy de Multiple vuelos a multiple destinos.
-let multiFlightRadioLabel = document.querySelector('label[for="flight-searcher-roundtrip-multi"]');
-multiFlightRadioLabel.textContent = "Multidestino";
+document.getElementById("flight-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
 });
 
 
