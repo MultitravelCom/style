3b7afa89f8be5784093a2a8ad63c46d13fedb760
Ignacio Peralta <112433497+ElOtroNach0@users.noreply.github.com>
2023-03-08 20:41:53 -0300

    a

--- Services/main.js
+++ Services/main.js
@@ -2,7 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
     let divToMove = document.getElementsByClassName("sliding-offers-v3");
     let divDestination = document.getElementById("test-card");
   
-    if (divToMove && divDestination) { // Verifica si los elementos existen
-      divDestination.appendChild(divToMove);
-    }
+        divDestination.appendChild(divToMove);
   });
