1ccd7f8a02652797c61d86b3fe9911457cbd53c9
Ignacio Peralta <112433497+ElOtroNach0@users.noreply.github.com>
2023-03-08 18:19:44 -0300

    test-cards

--- Services/main.js
+++ Services/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     // Mover div fuera del mas opciones.
-        let divToMove = document.getElementsById("home-content-1")[0];
+    let divToMove = document.getElementById("home-content-1");
         let divDestination = document.getElementsByClassName("main_conteiner__s1_medio top_mkt")[0];
     
         divDestination.appendChild(divToMove);
