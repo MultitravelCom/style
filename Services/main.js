a191eec43d79c81106fa43e1e007053533dc0c3e
Ignacio Peralta <112433497+ElOtroNach0@users.noreply.github.com>
2023-03-08 18:28:44 -0300

    test-card

--- Services/main.js
+++ Services/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     // Mover div fuera del mas opciones.
     let divToMove = document.getElementById("home-content-1");
-        let divDestination = document.getElementsByClassName("main_conteiner__s1_medio top_mkt")[0];
+    let divDestination = document.getElementById("test-card");
     
         divDestination.appendChild(divToMove);
     });
