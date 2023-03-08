ee22c4c68d3d7bf02b2bd05b0499dadb6f5a80f9
Ignacio Peralta <112433497+ElOtroNach0@users.noreply.github.com>
2023-03-08 19:45:12 -0300

    a

--- Services/main.js
+++ Services/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let divToMove = document.getElementById("test-card");
-    let divDestination = document.getElementById("home-content-1");
+    let divToMove = document.getElementById("home-sliding-offers-2");
+    let divDestination = document.getElementById("test-card");
   
     if (divToMove && divDestination) { // Verifica si los elementos existen
       divDestination.appendChild(divToMove);
