29e1263286fd9745a5cbc074c94f5e55c49f8022
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-09 12:10:04 -0300

    test

---
+++ Transfers/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let moverDiv = document.querySelector('#test');
+    let moverA = document.querySelector('#home-content-1');
+
+    moverA.appendChild(moverDiv);
+});
