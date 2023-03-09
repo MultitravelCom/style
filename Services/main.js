13f7d87442c6e27db0887d20c7f2a97dc660327b
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-08 22:45:17 -0300

    a

--- Services/main.js
+++ Services/main.js
@@ -1,4 +1,4 @@
-const homeContent1 = document.getElementById("home-content-1");
+const homeContent1 = document.getElementById("home-sliding-offers-2");
 const destinationDiv = document.querySelector(".main__conteiner main__conteiner-principal");
 
 destinationDiv.appendChild(homeContent1);
