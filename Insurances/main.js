493f207123c8c67edc9f57cd3260b900a55eb1b0
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-14 13:24:09 -0300

    mover div banner-central

--- Insurances/main.js
+++ Insurances/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let moverA = document.querySelector('#home-content-1');
-    let moverEn = document.querySelector('.main_conteiner__s1_medio__paquetes');
+    let moverEn = document.querySelector('#banner__central');
 
     moverEn.appendChild(moverA);
 });
