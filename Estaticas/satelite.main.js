753495480bd50e531468b506b7a8a4d5a2d725a0
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-14 17:53:06 -0300

    carrusel

--- Estaticas/satelite.main.js
+++ Estaticas/satelite.main.js
@@ -1,12 +1,31 @@
 window.addEventListener('load', function (e) {
     event.preventDefault()
     new Glider(document.querySelector(".carusel__lista"), {
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 2,
+        slidesToScroll: 2,
         draggable: true,
         arrows: {
             prev: '.carrusel__anterior',
             next: '.carrusel__siguiente'
-        }
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 450,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: '3',
+                slidesToScroll: '3',
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+
+              }
+            }
+          ]
         });
 });
