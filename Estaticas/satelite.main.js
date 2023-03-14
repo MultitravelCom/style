74e1316339094ba56ad130cf12ebbf7fdc0a21da
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-14 17:44:01 -0300

    carrusel

--- Estaticas/satelite.main.js
+++ Estaticas/satelite.main.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function (e) {
+    event.preventDefault()
     new Glider(document.querySelector(".carusel__lista"), {
         slidesToShow: 4,
         slidesToScroll: 4,
         draggable: true,
-        dots: '.dots',
         arrows: {
             prev: '.carrusel__anterior',
             next: '.carrusel__siguiente'
