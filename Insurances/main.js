6d456819239820e0bf637996f520533781808f8b
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-14 13:45:42 -0300

    arreglo bannerCentral

--- Insurances/main.js
+++ Insurances/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    let moverA = document.querySelector('#home-content-1');
-    let moverEn = document.querySelector('#banner__central');
+    let moverA = document.querySelector('#banner__central');
+    let moverEn = document.querySelector('#home-content-1');
 
     moverA.appendChild(moverEn);
 });
