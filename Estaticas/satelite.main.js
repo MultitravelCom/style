63d31fcce14c4153cfbd5d3732c7b1bc9b11657f
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-14 18:45:43 -0300

    carrusel

--- Estaticas/satelite.main.js
+++ Estaticas/satelite.main.js
@@ -1,38 +1,46 @@
-new Glider(document.querySelector(".carrusel__lista"), {
-    slidesToShow: 1,
-    slidesToScroll: 2,
-    draggable: true,
-    arrows: {
-      prev: '.carrusel__anterior',
-      next: '.carrusel__siguiente',
-      // Agrega el manejador de eventos `click` al botón de siguiente (`next`)
-      // del carrusel para detener la recarga de la página y mostrar el siguiente
-      // elemento en el carrusel.
-      nextFunc: function() {
-        const btn = document.querySelector('.carrusel__siguiente');
-        btn.addEventListener('click', function(event) {
-          event.preventDefault();
-          // Agrega la lógica para mostrar el siguiente elemento en el carrusel.
-          // ...
-        });
-      }
-    },
-    responsive: [
-      {
-        // screens greater than >= 775px
-        breakpoint: 450,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: '2',
-          slidesToScroll: '2',
-        }
-      },{
-        // screens greater than >= 1024px
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        }
-      }
-    ]
+window.addEventListener('load', function () {
+
+    const btnLesft = document.getElementById('btnLeft');
+    const btnRight = document.getElementById('btnRight');
+
+    btnLesft.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita que la página se recargue
+    
+    // Agrega la lógica para mostrar el siguiente elemento en el carrusel
   });
+
+  btnRight.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita que la página se recargue
+    
+    // Agrega la lógica para mostrar el siguiente elemento en el carrusel
+  });
+  
+    new Glider(document.querySelector(".carrusel__lista"), {
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        draggable: true,
+        arrows: {
+            prev: '.carrusel__anterior',
+            next: '.carrusel__siguiente'
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 450,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: '2',
+                slidesToScroll: '2',
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+
+              }
+            }
+          ]
+        });
+});
