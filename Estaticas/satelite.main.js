2ee2fe56e6a172c8f4e726e4bd6ffc369d80a9de
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-15 17:31:17 -0300

    carrusel

--- Estaticas/satelite.main.js
+++ Estaticas/satelite.main.js
@@ -1,122 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const btnLesft = document.querySelector('.btnLeft');
-    const btnRight = document.querySelector('.btnRight');
+  const items = [
+    { btnLeft: '.btnLeft', btnRight: '.btnRight', carrusel: '.carrusel__lista' },
+    { btnLeft: '.btnLeft2', btnRight: '.btnRight2', carrusel: '.carrusel__lista2' },
+    { btnLeft: '.btnLeft3', btnRight: '.btnRight3', carrusel: '.carrusel__lista3' }
+  ];
 
-    const btnLesft2 = document.querySelector('.btnLeft2');
-    const btnRight2 = document.querySelector('.btnRight2');
+  items.forEach(function (item) {
+    const btnLesft = document.querySelector('.item.btnLeft');
+    const btnRight = document.querySelector('.item.btnRight');
 
-    const btnLesft3 = document.querySelector('.btnLeft3');
-    const btnRight3 = document.querySelector('.btnRight3');
-
-    btnLesft.addEventListener('click', function(event) {
-    event.preventDefault(); 
-  });
-
-  btnRight.addEventListener('click', function(event) {
-    event.preventDefault(); 
-  });
-
-  btnLesft2.addEventListener('click', function(event) {
-    event.preventDefault(); 
-  });
-
-  btnRight2.addEventListener('click', function(event) {
-    event.preventDefault(); 
-  });
-
-  btnLesft3.addEventListener('click', function(event) {
-    event.preventDefault(); 
-  });
-
-  btnRight3.addEventListener('click', function(event) {
-    event.preventDefault(); 
-  });
-  
-    new Glider(document.querySelector(".carrusel__lista"), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        arrows: {
-            prev: '.carrusel__anterior',
-            next: '.carrusel__siguiente'
-        },
-        responsive: [
-            {
-              // screens greater than >= 775px
-              breakpoint: 450,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: '2',
-                slidesToScroll: '1',
-              }
-            },{
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-
-              }
-            }
-          ]
-      });
-    new Glider(document.querySelector(".carrusel__lista2"), {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          draggable: true,
-          arrows: {
-              prev: '.btnLeft2',
-              next: '.btnRight2'
-          },
-          responsive: [
-              {
-                // screens greater than >= 775px
-                breakpoint: 450,
-                settings: {
-                  // Set to `auto` and provide item width to adjust to viewport
-                  slidesToShow: '2',
-                  slidesToScroll: '1',
-                }
-              },{
-                // screens greater than >= 1024px
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 4,
-                  slidesToScroll: 1,
-  
-                }
-              }
-            ]
-      });
-    new Glider(document.querySelector(".carrusel__lista3"), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        arrows: {
-            prev: '.btnLeft3',
-            next: '.btnRight3'
-        },
-        responsive: [
-            {
-              // screens greater than >= 775px
-              breakpoint: 450,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: '2',
-                slidesToScroll: '1',
-              }
-            },{
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-
-              }
-            }
-          ]
+    btnLesft.addEventListener('click', function (event) {
+      event.preventDefault();
     });
-});
 
+    btnRight.addEventListener('click', function (event) {
+      event.preventDefault();
+    });
+
+    new Glider(document.querySelector(item.carrusel), {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: true,
+      arrows: {
+        prev: item.btnLeft,
+        next: item.btnRight
+      },
+      responsive: [
+        {
+          // screens greater than >= 775px
+          breakpoint: 450,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: '2',
+            slidesToScroll: '1',
+          }
+        }, {
+          // screens greater than >= 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+
+          }
+        }
+      ]
+    });
+  });
+});
 
