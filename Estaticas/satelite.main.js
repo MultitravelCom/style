4dec4d60297b1ba4a0e3d3e9f2447fc916e3dde7
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-15 17:04:27 -0300

    carrusel

--- Estaticas/satelite.main.js
+++ Estaticas/satelite.main.js
@@ -3,16 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnLesft = document.querySelector('.btnLeft');
     const btnRight = document.querySelector('.btnRight');
 
+    const btnLesft2 = document.querySelector('.btnLeft2');
+    const btnRight2 = document.querySelector('.btnRight2');
+
     btnLesft.addEventListener('click', function(event) {
-    event.preventDefault(); // Evita que la página se recargue
-    
-    // Agrega la lógica para mostrar el siguiente elemento en el carrusel
+    event.preventDefault(); 
   });
 
   btnRight.addEventListener('click', function(event) {
-    event.preventDefault(); // Evita que la página se recargue
-    
-    // Agrega la lógica para mostrar el siguiente elemento en el carrusel
+    event.preventDefault(); 
+  });
+
+  btnLesft2.addEventListener('click', function(event) {
+    event.preventDefault(); 
+  });
+
+  btnRight2.addEventListener('click', function(event) {
+    event.preventDefault(); 
   });
   
     new Glider(document.querySelector(".carrusel__lista"), {
@@ -42,5 +49,36 @@ document.addEventListener('DOMContentLoaded', function () {
               }
             }
           ]
-        });
+      });
+
+    new Glider(document.querySelector(".carrusel__lista2"), {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          draggable: true,
+          arrows: {
+              prev: '.carrusel__anterior',
+              next: '.carrusel__siguiente'
+          },
+          responsive: [
+              {
+                // screens greater than >= 775px
+                breakpoint: 450,
+                settings: {
+                  // Set to `auto` and provide item width to adjust to viewport
+                  slidesToShow: '2',
+                  slidesToScroll: '1',
+                }
+              },{
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 1,
+  
+                }
+              }
+            ]
+      });
 });
+
+
