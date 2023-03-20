d5edcb6c7196e2c898c17b55a4cd256a855f2ad4
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-20 17:16:07 -0300

    MULTI-70

---
+++ Packages/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', ()=>{
+  let changeIcon = document.querySelector('.result-option-package__item-product');
+
+    changeIcon.forEach(el => {
+     if(el.classList.contains("glyphicon-supplement")){
+           el. classList.remove("glyphicon-supplement");
+           el.classList.add("glyphicon-check-whl");
+     }
+  });
+});
