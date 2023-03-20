7fec7a591dec8817e259da4c75275a5b91a11bae
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-20 15:28:35 -0300

    MULT-69

--- Flighthotel/main.js
+++ Flighthotel/main.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const removeHidden = document.querySelectorAll(".master-selector-option");
     const displayMaster = document.querySelector(".master-selector");
-   
-    if(removeHidden.classList.contains("hidden")){
-        removeHidden.classList.remove("hidden");
-        displayMaster.style.display = 'inline-block';
-    };
+    
+    removeHidden.classList.remove("hidden");
+    displayMaster.style.display = 'inline-block';
 });
