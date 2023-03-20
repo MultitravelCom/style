c154f281b8738cee292002081ff5865eb55ecec2
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-20 17:28:45 -0300

    MULTI-70

--- Flighthotel/main.js
+++ Flighthotel/main.js
@@ -6,4 +6,16 @@ window.addEventListener('load', ()=>{
         el.classList.remove("hidden")
     });
         displayMaster.style.display = 'inline-block';
+
+        // ***********************************************************************************
+
+        let changeIcon = document.querySelector('.result-option-package__item-product');
+
+        changeIcon.forEach(el => {
+         if(el.classList.contains("glyphicon-supplement")){
+               el. classList.remove("glyphicon-supplement");
+               el.classList.add("glyphicon-check-whl");
+         }
+      });
+
 });
