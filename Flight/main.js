92ddc8136cdd871972862a9e11f39ea1d1facf86
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-21 15:56:21 -0300

    MULT-43

--- Flight/main.js
+++ Flight/main.js
@@ -3,9 +3,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let divToMove = document.getElementsByClassName("checkbox")[0];
     let divDestination = document.getElementsByClassName("singledestination-only")[0];
 
-    divDestination.appendChild(divToMove);
-    
+    divDestination.appendChild(divToMove);  
 //    Cambio de copy de Multiple vuelos a multiple destinos.
 document.getElementById("flight-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
 document.querySelector('.radio').style.display = 'inline-block';
 });
+
+// Cambiar texto al btn filtrar.
+let filtrar = Document.querySelector('.results-list__filter-toggle-wrapper').nextSibling.nodeValue = "Filtrar";
+
