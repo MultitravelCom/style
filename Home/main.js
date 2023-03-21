2deeb13d5e42aa6d50048400764eaa15785b5ebf
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-21 12:58:31 -0300

    vuelos active

--- Home/main.js
+++ Home/main.js
@@ -1,4 +1,9 @@
 window.addEventListener('load', () => {
+    // Activa la solapa vuelos
+    const secondNavItem = document.querySelectorAll('.nav-tabs').getElementsByTagName('li')[1];
+
+    secondNavItem.classList.contains('active') ? null : secondNavItem.classList.add('active');
+
     // Mover div fuera del mas opciones.
     let divToMove = document.getElementsByClassName("checkbox")[0];
     let divDestination = document.getElementsByClassName("singledestination-only")[0];
@@ -8,8 +13,5 @@ window.addEventListener('load', () => {
     //    Cambio de copy de Multiple vuelos a multiple destinos.
     document.getElementById("flights-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
     document.querySelector('.radio').style.display = 'inline-block';
-    
-    // Activa la solapa vuelos
-    const secondNavItem = document.querySelector('.nav-elem:nth-child(2)');
-        secondNavItem.classList.contains('active') ? null : secondNavItem.classList.add('active');
+
 });
