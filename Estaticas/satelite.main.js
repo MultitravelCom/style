bacd8338b6c00bb77b319a9131d1144224c06db5
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-22 21:33:09 -0300

    probando react

--- Estaticas/satelite.main.js
+++ Estaticas/satelite.main.js
@@ -2,9 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const items = [
     { btnLeft: '.btnLeft', btnRight: '.btnRight', carrusel: '.carrusel__lista' },
-    { btnLeft: '.btnLeft2', btnRight: '.btnRight2', carrusel: '.carrusel__lista2' },
-    { btnLeft: '.btnLeft3', btnRight: '.btnRight3', carrusel: '.carrusel__lista3' }
-
+    { btnLeft: '.btnLeft2', btnRight: '.btnRight2', carrusel: '.carrusel__lista2' }
   ];
 
   items.forEach(function(item) {
