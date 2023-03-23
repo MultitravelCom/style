8c023b6c4042b66ab9c870c54bc4b894208d1034
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-22 21:25:54 -0300

    probando react

---
+++ Estaticas/satelite.banners.js
@@ -0,0 +1,9 @@
+const Banners = () => {
+    return (
+      <div>
+        <h1>Hola mundo desde React!</h1>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<Banners />, document.getElementById('test'));
