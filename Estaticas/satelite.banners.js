ae21bef573fc708c97b4b4fbef506629e094e03a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-22 21:53:03 -0300

    probando react

--- Estaticas/satelite.banners.js
+++ Estaticas/satelite.banners.js
@@ -1,11 +1,14 @@
+import Componente from  "https://multitravelcom.github.io/style/Estaticas/satelite.componentes.js"
+
 const Banners = () => {
     return (
         <>
-      <div>
-        <h1>Hola mundo desde React!</h1>
-      </div>
+        <Componente />
+            <div>
+                <h1>Hola mundo desde React!</h1>
+            </div>
         </>
     );
-  }
-  
-  ReactDOM.render(<Banners />, document.getElementById('test'));
+}
+
+ReactDOM.render(<Banners />, document.getElementById('test'));
---
+++ Estaticas/satelite.componentes.js
@@ -0,0 +1,7 @@
+const Componente = () => {
+    return(
+        <h2>Otro componente</h2>
+    );
+}
+
+export default Componente;
