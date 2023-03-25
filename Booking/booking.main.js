2b57a9c8a8c225488c4a2a61f1b3c8a50933f19c
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-25 20:58:34 -0300

    agregar div booking3

---
+++ Booking/booking.main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    let bookingData = document.querySelector('.booking-data__data');
+    let divs = bookingData.querySelectorAll('div');
+    let newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+        <h1>Test</h1>
+    `
+    bookingData.insertBefore(newDiv, divs[3]);
+});
