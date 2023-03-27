6dd6eb28f8b2fd9855f7344bbe7f0981c86e609e
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-27 17:22:11 -0300

    arreglo

--- General/app.js
+++ General/app.js
@@ -1,27 +1,30 @@
+import React from "react";
+import { props } from "./secciones";
+
 const App = () => {
     return (
         <>
-            <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
+            <h2 className="main__conteiner__titulo">{props.titulo}</h2>
             <div className="main__conteiner">
                 <div className="main__conteiner__s3__piensa row-cols-3">
                     <div className="col-md-4 col-xs-12 uno__s3 hover__s3">
                         <a href="https://www.multitravel.com/hotels/" target="_blank">
                             <div className="main__conteiner__s3__piensa__parrafo">
-                                <p>Alojamientos</p>
+                                <p>{props.alojamiento}</p>
                             </div>
                         </a>
                     </div>
                     <div className="col-md-4 col-xs-12 dos__s3 hover__s3">
                         <a href="https://www.multitravel.com/flights/" target="_blank">
                             <div className="main__conteiner__s3__piensa__parrafo">
-                                <p>Vuelos</p>
+                                <p>{props.vuelos}</p>
                             </div>
                         </a>
                     </div>
                     <div className="col-md-4 col-xs-12 tres__s3 hover__s3">
                         <a href="https://www.multitravel.com/packages/" target="_blank">
                             <div className="main__conteiner__s3__piensa__parrafo">
-                                <p>Paquetes</p>
+                                <p>{props.paquetes}</p>
                             </div>
                         </a>
                     </div>
---
+++ General/secciones.js
@@ -0,0 +1,6 @@
+export const props = {
+    titulo: "Multitravel.com piensa en todo",
+    alojamiento: "Alojamientos",
+    vuelos: "Vuelos",
+    paquetes: "Paquetes"
+  };
