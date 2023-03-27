cb6f754127567c9cfde0b6f5af524c76b9520a22
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-26 21:25:53 -0300

    banner general

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
     let bookingData = document.querySelector('.booking-data__data');
-    let insurances = bookingData.querySelector('.confirm-booking__insurances');
+    // let insurances = bookingData.querySelector('.confirm-booking__insurances');
     let payment = bookingData.querySelector('.booking-payment');
     let newDiv = document.createElement('div');
     newDiv.innerHTML = `
