b3e5335af29685502319407e3670a3648826629c
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-27 20:28:45 -0300

    react test

--- General/app.js
+++ General/app.js
@@ -1,16 +1,12 @@
+import { Seccions } from 'https://raw.githubusercontent.com/MultitravelCom/style/main/General/componentSeccion.js';
+
 const App = () => {
     return (
         <>
             <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
             <div className="main__conteiner">
                 <div className="main__conteiner__s3__piensa row-cols-3">
-                    <div className="col-md-4 col-xs-12 uno__s3 hover__s3">
-                        <a href="https://www.multitravel.com/hotels/" target="_blank">
-                            <div className="main__conteiner__s3__piensa__parrafo">
-                                <p>Alojamientos</p>
-                            </div>
-                        </a>
-                    </div>
+                    <Seccions />
                     <div className="col-md-4 col-xs-12 dos__s3 hover__s3">
                         <a href="https://www.multitravel.com/flights/" target="_blank">
                             <div className="main__conteiner__s3__piensa__parrafo">
