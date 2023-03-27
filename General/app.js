e81ac54d87ba67854e3513133bf498a6c6d1ef27
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-27 20:36:17 -0300

    react test

--- General/app.js
+++ General/app.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Seccions } from 'https://raw.githubusercontent.com/MultitravelCom/style/main/General/componentSeccion.js';
 
 const App = () => {
