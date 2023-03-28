ceb9ba089fbd8e5902d41480807b0f62d005699c
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-27 23:20:50 -0300

    react test

--- General/app.js
+++ General/app.js
@@ -51,7 +51,7 @@ const App = () => {
                 <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
                 <div className="main__conteiner">
                     <div className="main__conteiner__s3__piensa row-cols-3">
-                        <Secciones seccion="Paquetes" url="https://www.multitravel.com/hotels/" className="uno__s3" />
+                    <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3" />
                         <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
                         <Secciones seccion="Asistencias" url="https://www.multitravel.com/insurances/" className="tres__s3" />
                     </div>
