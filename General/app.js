e0ee25dbdfb6944cd73cc431a1344afafa69d9e2
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-27 22:27:58 -0300

    react test

--- General/app.js
+++ General/app.js
@@ -1,3 +1,6 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
 const Secciones = (props) => {
     const { className } = props;
     return (
@@ -14,18 +17,48 @@ const Secciones = (props) => {
 };
 
 const App = () => {
-    return (
-        <>
-            <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
-            <div className="main__conteiner">
-                <div className="main__conteiner__s3__piensa row-cols-3">
-                    <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3"/>
-                    <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
-                    <Secciones seccion="Paquetes" url="https://www.multitravel.com/packages/flighthotel/" className="tres__s3"/>
-                </div>
-            </div>
-        </>
-    );
-};
+    const path = window.location.pathname;
 
-ReactDOM.render(<App />, document.getElementById('root__s3'));
+    if (path === '/flights') {
+        return (
+            <>
+                <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
+                <div className="main__conteiner">
+                    <div className="main__conteiner__s3__piensa row-cols-3">
+                        <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3" />
+                        <Secciones seccion="Paquetes" url="https://www.multitravel.com/packages/flighthotel/" className="dos__s3" />
+                        <Secciones seccion="Asistencias" url="https://www.multitravel.com/insurances/" className="tres__s3" />
+                    </div>
+                </div>
+            </>
+        );
+    } else if (path === '/hotels') {
+        return (
+            <>
+                <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
+                <div className="main__conteiner">
+                    <div className="main__conteiner__s3__piensa row-cols-3">
+                        <Secciones seccion="Paquetes" url="https://www.multitravel.com/hotels/" className="uno__s3" />
+                        <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
+                        <Secciones seccion="Asistencias" url="https://www.multitravel.com/insurances/" className="tres__s3" />
+                    </div>
+                </div>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
+                <div className="main__conteiner">
+                    <div className="main__conteiner__s3__piensa row-cols-3">
+                        <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3" />
+                        <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
+                        <Secciones seccion="Paquetes" url="https://www.multitravel.com/packages/flighthotel/" className="tres__s3" />
+                    </div>
+                </div>
+            </>
+        );
+    };
+}
+
+    ReactDOM.render(<App />, document.getElementById('root__s3'));
