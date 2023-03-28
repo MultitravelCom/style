7fd22d6ae2a92102665474362d3882a6651b90fc
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-27 21:17:07 -0300

    react test

--- General/componentSeccion.js
+++ General/componentSeccion.js
@@ -1,11 +1,5 @@
 export const Seccions = () => {
     return (
-            <div className="col-md-4 col-xs-12 uno__s3 hover__s3">
-                <a href="https://www.multitravel.com/hotels/" target="_blank">
-                    <div className="main__conteiner__s3__piensa__parrafo">
-                        <p>Alojamientos</p>
-                    </div>
-                </a>
-            </div>
+            <h2>Test</h2>
     );
 };
