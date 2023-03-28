5de8b756b137b4879fecba858180a50c3de699e5
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-27 21:42:50 -0300

    react test

--- General/app.js
+++ General/app.js
@@ -6,7 +6,7 @@ const App = () => {
             <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
             <div className="main__conteiner">
                 <div className="main__conteiner__s3__piensa row-cols-3">
-                <div className="col-md-4 col-xs-12 dos__s3 hover__s3">
+                    <div className="col-md-4 col-xs-12 dos__s3 hover__s3">
                         <a href="https://www.multitravel.com/flights/" target="_blank">
                             <div className="main__conteiner__s3__piensa__parrafo">
                                 <p>Alojamientos</p>
