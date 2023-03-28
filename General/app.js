1c9dc2d3294d3a5c4d657b9bb2b8d04bb3ed69ea
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-27 22:12:17 -0300

    react test

--- General/app.js
+++ General/app.js
@@ -1,7 +1,8 @@
 const Secciones = (props) => {
+    const { className } = props;
     return (
         <>
-            <div className="col-md-4 col-xs-12 hover__s3">
+            <div className={`col-md-4 col-xs-12 hover__s3 ${className}`}>
                 <a href={props.url} target="_blank">
                     <div className="main__conteiner__s3__piensa__parrafo">
                         <p>{props.seccion}</p>
@@ -18,9 +19,9 @@ const App = () => {
             <h2 className="main__conteiner__titulo">Multitravel.com piensa en todo</h2>
             <div className="main__conteiner">
                 <div className="main__conteiner__s3__piensa row-cols-3">
-                    <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/"/>
-                    <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/"/>
-                    <Secciones seccion="Paquetes" url="https://www.multitravel.com/packages/flighthotel/"/>
+                    <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3"/>
+                    <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
+                    <Secciones seccion="Paquetes" url="https://www.multitravel.com/packages/flighthotel/" className="tres__s3"/>
                 </div>
             </div>
         </>
