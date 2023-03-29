56fff4df97823e3b6b6bbd0183a111d1312ddf98
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-28 21:59:05 -0300

    agregar div booking

--- General/app.js
+++ General/app.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Secciones from "./componentSeccion";
+const Secciones = (props) => {
+    const { className } = props;
+    return (
+        <>
+            <div className={`col-md-4 col-xs-12 hover__s3 ${className}`}>
+                <a href={props.url} target="_blank">
+                    <div className="main__conteiner__s3__piensa__parrafo">
+                        <p>{props.seccion}</p>
+                    </div>
+                </a>
+            </div>
+        </>
+    );
+};
+
 
 const App = () => {
     const path = window.location.pathname;
