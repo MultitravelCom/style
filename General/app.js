564b478b068f7d9cb278bdbfb3e915de363c14d3
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-29 17:19:37 -0300

    arreglo seccion loMejorDe

--- General/app.js
+++ General/app.js
@@ -1,39 +1,62 @@
 // Componente BennerTop
 const BannerTop = () => {
-  return(
-    <>
-        <div className="main_conteiner__s1_medio bannerTop">
-            <div className="main_conteiner__s1_medio__paquetes">
-                <a href="https://www.multitravel.com/venta-personalizada/travelsale" target="_blank">
-                    <picture>
-                        <source media="(min-width: 1024px)" srcset="
+    return (
+        <>
+            <div className="main_conteiner__s1_medio bannerTop">
+                <div className="main_conteiner__s1_medio__paquetes">
+                    <a href="https://www.multitravel.com/venta-personalizada/travelsale" target="_blank">
+                        <picture>
+                            <source media="(min-width: 1024px)" srcset="
                         https://multitravelcom.github.io/MT/TravelSale2023/Banners/BannerProductos%20(1).jpg
                 " />
-                        <source media="(min-width: 768px) and (max-width: 1023px)" srcset="
+                            <source media="(min-width: 768px) and (max-width: 1023px)" srcset="
                         https://multitravelcom.github.io/MT/TravelSale2023/Banners/BannerProductos%20(2).jpg
                 " />
-                        <source media="(max-width: 767px)" srcset="
+                            <source media="(max-width: 767px)" srcset="
                         https://multitravelcom.github.io/MT/TravelSale2023/Banners/BannerProductos%20(3).jpg
                 " />
-                        <img src="https://multitravelcom.github.io/MT/TravelSale2023/Banners/BannerProductos%20(1).jpg"
-                            alt="Imagen banner promociones" />
-                    </picture>
-                </a>
+                            <img src="https://multitravelcom.github.io/MT/TravelSale2023/Banners/BannerProductos%20(1).jpg"
+                                alt="Imagen banner promociones" />
+                        </picture>
+                    </a>
+                </div>
             </div>
-        </div>
-    </>
-  ); 
+        </>
+    );
 };
 
 // Lo mejor de Multitravel.com
-const Secciones = (props) => {
-    const { className } = props;
+// SECCIONES
+const ofertasData = {
+    alojamientos: {
+      nombre: "Alojamientos",
+      url: "https://www.multitravel.com/hotels/",
+      className: "uno__s3",
+    },
+    paquetes: {
+      nombre: "Paquetes",
+      url: "https://www.multitravel.com/packages/flighthotel/",
+      className: "dos__s3",
+    },
+    asistencias: {
+      nombre: "Asistencias",
+      url: "https://www.multitravel.com/insurances/",
+      className: "tres__s3",
+    },
+    vuelos: {
+      nombre: "Vuelos",
+      url: "https://www.multitravel.com/flights/",
+      className: "cuatro__s3",
+    },
+  };
+
+const Secciones = ({ seccion, url, className }) => {
     return (
         <>
             <div className={`col-md-4 col-xs-12 hover__s3 ${className}`}>
-                <a href={props.url} target="_blank">
+                <a href={url} target="_blank">
                     <div className="main__conteiner__s3__piensa__parrafo">
-                        <p>{props.seccion}</p>
+                        <p>{seccion}</p>
                     </div>
                 </a>
             </div>
@@ -42,59 +65,98 @@ const Secciones = (props) => {
 };
 
 
+const Ofertas = ({ seccion1, seccion2, seccion3 }) => {
+    return (
+      <>
+        <h2 className="main__conteiner__titulo">Más ofertas en TravelSale</h2>
+        <div className="main__conteiner">
+          <div className="main__conteiner__s3__piensa row-cols-3">
+            <Secciones
+              seccion={ofertasData[seccion1].nombre}
+              url={ofertasData[seccion1].url}
+              className={ofertasData[seccion1].className}
+            />
+            <Secciones
+              seccion={ofertasData[seccion2].nombre}
+              url={ofertasData[seccion2].url}
+              className={ofertasData[seccion2].className}
+            />
+            <Secciones
+              seccion={ofertasData[seccion3].nombre}
+              url={ofertasData[seccion3].url}
+              className={ofertasData[seccion3].className}
+            />
+          </div>
+        </div>
+      </>
+    );
+  };
+
 const LoMejorDe = () => {
     const path = window.location.pathname;
 
     if (path === '/flights/') {
         return (
             <>
-                <h2 className="main__conteiner__titulo">Más ofertas en TravelSale</h2>
-                <div className="main__conteiner">
-                    <div className="main__conteiner__s3__piensa row-cols-3">
-                        <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3" />
-                        <Secciones seccion="Paquetes" url="https://www.multitravel.com/packages/flighthotel/" className="dos__s3" />
-                        <Secciones seccion="Asistencias" url="https://www.multitravel.com/insurances/" className="tres__s3" />
-                    </div>
-                </div>
+                <Ofertas
+                    seccion1="Alojamientos"
+                    url1="https://www.multitravel.com/hotels/"
+                    className1="uno__s3"
+                    seccion2="Paquetes"
+                    url2="https://www.multitravel.com/packages/flighthotel/"
+                    className2="dos__s3"
+                    seccion3="Asistencias"
+                    url3="https://www.multitravel.com/insurances/"
+                    className3="tres__s3"
+                />
             </>
         );
     } else if (path === '/hotels/') {
         return (
             <>
-                <h2 className="main__conteiner__titulo">Más ofertas en TravelSale</h2>
-                <div className="main__conteiner">
-                    <div className="main__conteiner__s3__piensa row-cols-3">
-                        <Secciones seccion="Paquetes" url="https://www.multitravel.com/hotels/" className="uno__s3" />
-                        <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
-                        <Secciones seccion="Asistencias" url="https://www.multitravel.com/insurances/" className="tres__s3" />
-                    </div>
-                </div>
+                <Ofertas
+                    seccion1="Paquetes"
+                    url1="https://www.multitravel.com/hotels/"
+                    className1="uno__s3"
+                    seccion2="Vuelos"
+                    url2="https://www.multitravel.com/flights/"
+                    className2="dos__s3"
+                    seccion3="Asistencias"
+                    url3="https://www.multitravel.com/insurances/"
+                    className3="tres__s3"
+                />
             </>
         );
-    }else if (path === '/packages/flighthotel/') {
+    } else if (path === '/packages/flighthotel/') {
         return (
             <>
-                <h2 className="main__conteiner__titulo">Más ofertas en TravelSale</h2>
-                <div className="main__conteiner">
-                    <div className="main__conteiner__s3__piensa row-cols-3">
-                    <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3" />
-                        <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
-                        <Secciones seccion="Asistencias" url="https://www.multitravel.com/insurances/" className="tres__s3" />
-                    </div>
-                </div>
+                <Ofertas
+                    seccion1="Alojamientos"
+                    url1="https://www.multitravel.com/hotels/"
+                    className1="uno__s3"
+                    seccion2="Vuelos"
+                    url2="https://www.multitravel.com/flights/"
+                    className2="dos__s3"
+                    seccion3="Asistencias"
+                    url3="https://www.multitravel.com/insurances/"
+                    className3="tres__s3"
+                />
             </>
         );
     } else {
         return (
             <>
-                <h2 className="main__conteiner__titulo">Más ofertas en TravelSale</h2>
-                <div className="main__conteiner">
-                    <div className="main__conteiner__s3__piensa row-cols-3">
-                        <Secciones seccion="Alojamientos" url="https://www.multitravel.com/hotels/" className="uno__s3" />
-                        <Secciones seccion="Vuelos" url="https://www.multitravel.com/flights/" className="dos__s3" />
-                        <Secciones seccion="Paquetes" url="https://www.multitravel.com/packages/flighthotel/" className="tres__s3" />
-                    </div>
-                </div>
+                <Ofertas
+                    seccion1="Alojamientos"
+                    url1="https://www.multitravel.com/hotels/"
+                    className1="uno__s3"
+                    seccion2="Vuelos"
+                    url2="https://www.multitravel.com/flights/"
+                    className2="dos__s3"
+                    seccion3="Paquetes"
+                    url3="https://www.multitravel.com/packages/flighthotel/"
+                    className3="tres__s3"
+                />
             </>
         );
     };
