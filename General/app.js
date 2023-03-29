a6e03709f05f65715c254c6988d63bb39bcf91e7
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-03-28 21:28:14 -0300

    react test

--- General/app.js
+++ General/app.js
@@ -1,17 +1,4 @@
-const Secciones = (props) => {
-    const { className } = props;
-    return (
-        <>
-            <div className={`col-md-4 col-xs-12 hover__s3 ${className}`}>
-                <a href={props.url} target="_blank">
-                    <div className="main__conteiner__s3__piensa__parrafo">
-                        <p>{props.seccion}</p>
-                    </div>
-                </a>
-            </div>
-        </>
-    );
-};
+import Secciones from "./componentSeccion";
 
 const App = () => {
     const path = window.location.pathname;
--- General/componentSeccion.js
+++ General/componentSeccion.js
@@ -1,11 +1,16 @@
-class Seccions extends React.Component {
-    render(){
-        return (
-                <>
-                <h2>Test</h2>
-                </>
-        );
-    }
+const Secciones = (props) => {
+    const { className } = props;
+    return (
+        <>
+            <div className={`col-md-4 col-xs-12 hover__s3 ${className}`}>
+                <a href={props.url} target="_blank">
+                    <div className="main__conteiner__s3__piensa__parrafo">
+                        <p>{props.seccion}</p>
+                    </div>
+                </a>
+            </div>
+        </>
+    );
 };
 
-export default Seccions;
+export default Secciones;
