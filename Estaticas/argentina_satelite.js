de98d9f946dc95bb70e77fa9ffd55d3a091eeaab
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-31 18:02:26 -0300

    carrusel

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -22,16 +22,33 @@ const destinos = [
 const Card = ({ linkWa, destino, img, className }) => {
     return (
         <>
-            <div className={`main__conteiner__s1__destacado__card ${className}`} style={{ height: '100%', width: '100%' }}>
-                    <a href={linkWa} target="_blank">
-                        <picture>
-                            <source media="(min-width: 1024px)" srcSet={`${img}`} />
-                            <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${img}`} />
-                            <source media="(max-width: 767px)" srcSet={`${img}`} />
-                            <img src={`${img}`} alt={`Imagen banner ${destino}`} />
-                        </picture>
-                    </a>
+            <div className="main__conteiner__s1">
+                <div className="main__conteiner__s1__titulo">
+                    <h3><strong>Descuentos Imbatibles por Argentina</strong></h3>
                 </div>
+            </div>
+            <div className="carrusel__contenedor">
+                <button aria-label="Anterior" className="carrusel__anterior btnLeft">
+                    <i className="fa fa-chevron-left" aria-hidden="true"></i>
+                </button>
+                <div className="carrusel__lista">
+                    <div className="carrusel__elemento">
+                        <div className={`main__conteiner__s1__destacado__card ${className}`} style={{ height: '100%', width: '100%' }}>
+                            <a href={linkWa} target="_blank">
+                                <picture>
+                                    <source media="(min-width: 1024px)" srcSet={`${img}`} />
+                                    <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${img}`} />
+                                    <source media="(max-width: 767px)" srcSet={`${img}`} />
+                                    <img src={`${img}`} alt={`Imagen banner ${destino}`} />
+                                </picture>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <button aria-label="Siguiente" className="carrusel__siguiente btnRight">
+                    <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                </button>
+            </div>
         </>
     )
 }
