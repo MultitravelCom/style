1e7ccb4f98838832b2ac212d696f8769359668bc
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-31 12:18:14 -0300

    carrusel

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -3,24 +3,27 @@ const destinos = [
         id: 1,
         destino: "Mendoza",
         img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/mendoza%20(1).jpg",
+        className: "uno",
     },
     {
-        id: 1,
+        id: 2,
         destino: "Bariloche",
         img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/barilo-1.jpg",
+        className: "dos",
     },
     {
-        id: 1,
+        id: 3,
         destino: "El calafate",
         img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/calafate%20(1).jpg",
+        className: "tres",
     },
 ];
 
-const Card = ({ linkWa, destino, img }) => {
+const Card = ({ linkWa, destino, img, className }) => {
     return (
         <>
             <div className="carrusel__elemento">
-                <div className="main__conteiner__s1__destacado__card" style={{ height: '100%', width: '100%'}}>
+                <div className={`main__conteiner__s1__destacado__card ${className}`} style={{ height: '100%', width: '100%'}} >
                     <a href={linkWa} target="_blank">
                         <picture>
                             <source media="(min-width: 1024px)" srcSet={`${img}`} />
@@ -44,6 +47,7 @@ const DestinosList = () => {
                     linkWa={`https://wa.link/${destino.destino}`}
                     destino={destino.destino}
                     img={destino.img}
+                    className={destino.className}
                 />
             ))}
         </>
