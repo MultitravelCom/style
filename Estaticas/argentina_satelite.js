07d1410aa17322b2924006c47964ec05c2718462
Multitravel.com <ignacio.peralta@multitravel.com>
2023-03-31 17:25:39 -0300

    carrusel

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -39,6 +39,57 @@ const Card = ({ linkWa, destino, img, className }) => {
 }
 
 const DestinosList = () => {
+
+    useEffect(() => {
+        const items = [
+          { btnLeft: '.btnLeft', btnRight: '.btnRight', carrusel: '.carrusel__lista' },
+          { btnLeft: '.btnLeft2', btnRight: '.btnRight2', carrusel: '.carrusel__lista2' },
+        ];
+    
+        items.forEach((item) => {
+          const btnLeft = document.querySelector(item.btnLeft);
+          const btnRight = document.querySelector(item.btnRight);
+    
+          btnLeft.addEventListener('click', (event) => {
+            event.preventDefault();
+          });
+    
+          btnRight.addEventListener('click', (event) => {
+            event.preventDefault();
+          });
+    
+          new Glider(document.querySelector(item.carrusel), {
+            slidesToShow: 1.2,
+            slidesToScroll: 0.5,
+            draggable: true,
+            arrows: {
+              prev: item.btnLeft,
+              next: item.btnRight,
+            },
+            responsive: [
+              {
+                // screens greater than >= 775px
+                breakpoint: 450,
+                settings: {
+                  // Set to `auto` and provide item width to adjust to viewport
+                  slidesToShow: '2.2',
+                  slidesToScroll: '1',
+                },
+              },
+              {
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 1,
+                },
+              },
+            ],
+            rewind: true,
+          });
+        });
+      }, []);
+
     return (
         <>
             {destinos.map((destino) => (
