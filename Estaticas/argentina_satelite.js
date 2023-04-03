6c39d3ce3c415add29b0518be1b06011f7264e24
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-03 16:09:45 -0300

    cambio de titulo seccion

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -23,87 +23,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const Card = ({ linkWa, destino, img, className }) => {
         return (
-            <>
+            <div class="carrusel__elemento">
                 {
-                destinos.map((destino) => (
-                    <div className="carrusel__elemento">
-                         <div className={`main__conteiner__s1__destacado__card ${className}`} style={{ height: '100%', width: '100%' }}>
-                    <a href={linkWa} target="_blank">
-                        <picture>
-                            <source media="(min-width: 1024px)" srcSet={`${img}`} />
-                            <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${img}`} />
-                            <source media="(max-width: 767px)" srcSet={`${img}`} />
-                            <img src={`${img}`} alt={`Imagen banner ${destino}`} />
-                        </picture>
-                    </a>
-                </div>
-                    </div>
-                ))
-            }
-            </>
+                    destinos.map((destino) => (
+                        <div className="carrusel__elemento">
+                            <div className={`main__conteiner__s1__destacado__card ${className}`} style={{ height: '100%', width: '100%' }}>
+                                <a href={linkWa} target="_blank">
+                                    <picture>
+                                        <source media="(min-width: 1024px)" srcSet={`${img}`} />
+                                        <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${img}`} />
+                                        <source media="(max-width: 767px)" srcSet={`${img}`} />
+                                        <img src={`${img}`} alt={`Imagen banner ${destino}`} />
+                                    </picture>
+                                </a>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         )
     }
 
     const DestinosList = () => {
-        const [gliderInitialized, setGliderInitialized] = React.useState(false);
-
-        React.useEffect(() => {
-            if (!gliderInitialized) {
-                const items = [
-                    {
-                        btnLeft: ".btnLeft",
-                        btnRight: ".btnRight",
-                        carrusel: ".carrusel__lista",
-                    },
-                ];
-
-                items.forEach(function (item) {
-                    const btnLeft = document.querySelector(item.btnLeft);
-                    const btnRight = document.querySelector(item.btnRight);
-
-                    btnLeft.addEventListener("click", function (event) {
-                        event.preventDefault();
-                    });
-
-                    btnRight.addEventListener("click", function (event) {
-                        event.preventDefault();
-                    });
-
-                    new Glider(document.querySelector(item.carrusel), {
-                        slidesToShow: 1.2,
-                        slidesToScroll: 0.5,
-                        draggable: true,
-                        arrows: {
-                            prev: item.btnLeft,
-                            next: item.btnRight,
-                        },
-                        responsive: [
-                            {
-                                // screens greater than >= 775px
-                                breakpoint: 450,
-                                settings: {
-                                    // Set to `auto` and provide item width to adjust to viewport
-                                    slidesToShow: "2.2",
-                                    slidesToScroll: "1",
-                                },
-                            },
-                            {
-                                // screens greater than >= 1024px
-                                breakpoint: 1024,
-                                settings: {
-                                    slidesToShow: 4,
-                                    slidesToScroll: 1,
-                                },
-                            },
-                        ],
-                        rewind: true,
-                    });
-                });
-
-                setGliderInitialized(true);
-            }
-        }, [gliderInitialized]);
-
         return (
             <>
                 <Card destinos={destinos} />
