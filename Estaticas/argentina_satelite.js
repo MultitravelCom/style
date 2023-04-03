63cc91208de9eeefedbec19a4b8cef85b2bd4e81
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-03 16:15:47 -0300

    cambio de titulo seccion

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -21,36 +21,38 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     ];
 
-    const Card = ({ linkWa, destino, img, className }) => {
+    const Card = ({ destinos }) => {
         return (
-            <div class="carrusel__elemento">
-                {
-                    destinos.map((destino) => (
-                        <div className="carrusel__elemento">
-                            <div className={`main__conteiner__s1__destacado__card ${className}`} style={{ height: '100%', width: '100%' }}>
-                                <a href={linkWa} target="_blank">
-                                    <picture>
-                                        <source media="(min-width: 1024px)" srcSet={`${img}`} />
-                                        <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${img}`} />
-                                        <source media="(max-width: 767px)" srcSet={`${img}`} />
-                                        <img src={`${img}`} alt={`Imagen banner ${destino}`} />
-                                    </picture>
-                                </a>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-        )
-    }
-
-    const DestinosList = () => {
-        return (
-            <>
-                <Card destinos={destinos} />
-            </>
+          <div class="carrusel__elemento">
+            {destinos.map((destino) => (
+              <div className="carrusel__elemento">
+                <div
+                  className={`main__conteiner__s1__destacado__card ${destino.className}`}
+                  style={{ height: "100%", width: "100%" }}
+                >
+                  <a href={destino.linkWa} target="_blank">
+                    <picture>
+                      <source media="(min-width: 1024px)" srcSet={`${destino.img}`} />
+                      <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${destino.img}`} />
+                      <source media="(max-width: 767px)" srcSet={`${destino.img}`} />
+                      <img src={`${destino.img}`} alt={`Imagen banner ${destino.destino}`} />
+                    </picture>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
         );
-    };
-
-    ReactDOM.render(<DestinosList />, document.getElementById("carrusel__lista_uno"));
+      };
+      
+      const DestinosList = () => {
+        return (
+          <>
+            <Card destinos={destinos} />
+          </>
+        );
+      };
+      
+      ReactDOM.render(<DestinosList />, document.getElementById("carrusel__lista_uno"));
+      
 });
