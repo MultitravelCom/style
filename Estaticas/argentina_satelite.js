6d192b57d401bf41f05096c5d57a086323c30ec6
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-03 10:52:05 -0300

    cambio de titulo seccion

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -19,6 +19,11 @@ const destinos = [
     },
 ];
 
+const items = [
+    { btnLeft: '.btnLeft', btnRight: '.btnRight', carrusel: '.carrusel__lista' },
+    { btnLeft: '.btnLeft2', btnRight: '.btnRight2', carrusel: '.carrusel__lista2' }
+];
+
 const Card = ({ linkWa, destino, img, className }) => {
     return (
         <>
@@ -36,11 +41,55 @@ const Card = ({ linkWa, destino, img, className }) => {
     )
 }
 
+
+const initGlider = () => {
+
+    items.forEach(function (item) {
+        const btnLeft = document.querySelector(item.btnLeft);
+        const btnRight = document.querySelector(item.btnRight);
+
+        btnLeft.addEventListener('click', function (event) {
+            event.preventDefault();
+        });
+
+        btnRight.addEventListener('click', function (event) {
+            event.preventDefault();
+        });
+
+        new Glider(document.querySelector(item.carrusel), {
+            slidesToShow: 1.20,
+            slidesToScroll: 0.50,
+            draggable: true,
+            arrows: {
+                prev: item.btnLeft,
+                next: item.btnRight
+            },
+            responsive: [
+                {
+                    // screens greater than >= 775px
+                    breakpoint: 450,
+                    settings: {
+                        // Set to `auto` and provide item width to adjust to viewport
+                        slidesToShow: '2.20',
+                        slidesToScroll: '1',
+                    }
+                }, {
+                    // screens greater than >= 1024px
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+
+                    }
+                }
+            ],
+            rewind: true
+        });
+    });
+}
+
+
 const DestinosList = () => {
-
-
-
-
     return (
         <>
             <div className="main__conteiner__s1">
