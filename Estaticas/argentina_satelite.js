a1053d27892bd0ca5b608fd3c4b1be94493665d7
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-03 17:19:59 -0300

    cambio de titulo seccion

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -1,114 +1,114 @@
-    const destinos = [
-        {
-            id: 1,
-            destino: "Mendoza",
-            img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/mendoza%20(1).jpg",
-            className: "uno",
-        },
-        {
-            id: 2,
-            destino: "Bariloche",
-            img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/barilo-1.jpg",
-            className: "dos",
-        },
-        {
-            id: 3,
-            destino: "El calafate",
-            img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/calafate%20(1).jpg",
-            className: "tres",
-        },
-    ];
+const destinos = [
+    {
+        id: 1,
+        destino: "Mendoza",
+        img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/mendoza%20(1).jpg",
+        className: "uno",
+    },
+    {
+        id: 2,
+        destino: "Bariloche",
+        img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/barilo-1.jpg",
+        className: "dos",
+    },
+    {
+        id: 3,
+        destino: "El calafate",
+        img: "https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/calafate%20(1).jpg",
+        className: "tres",
+    },
+];
 
-    const Card = ({ destinos }) => {
-        return (
-          <div class="carrusel__elemento">
+const Card = ({ destinos }) => {
+    return (
+        <div className="carrusel__elemento">
             {destinos.map((destino) => (
-              <div className="carrusel__elemento">
-                <div
-                  className={`main__conteiner__s1__destacado__card ${destino.className}`}
-                  style={{ height: "100%", width: "100%" }}
-                >
-                  <a href={destino.linkWa} target="_blank">
-                    <picture>
-                      <source media="(min-width: 1024px)" srcSet={`${destino.img}`} />
-                      <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${destino.img}`} />
-                      <source media="(max-width: 767px)" srcSet={`${destino.img}`} />
-                      <img src={`${destino.img}`} alt={`Imagen banner ${destino.destino}`} />
-                    </picture>
-                  </a>
+                <div className="carrusel__elemento">
+                    <div
+                        className={`main__conteiner__s1__destacado__card ${destino.className}`}
+                        style={{ height: "100%", width: "100%" }}
+                    >
+                        <a href={destino.linkWa} target="_blank">
+                            <picture>
+                                <source media="(min-width: 1024px)" srcSet={`${destino.img}`} />
+                                <source media="(min-width: 768px) and (max-width: 1023px)" srcSet={`${destino.img}`} />
+                                <source media="(max-width: 767px)" srcSet={`${destino.img}`} />
+                                <img src={`${destino.img}`} alt={`Imagen banner ${destino.destino}`} />
+                            </picture>
+                        </a>
+                    </div>
                 </div>
-              </div>
             ))}
-          </div>
-        );
-      };
-      
-      const DestinosList = () => {
+        </div>
+    );
+};
 
- const [gliderInitialized, setGliderInitialized] = React.useState(false);
+const DestinosList = () => {
 
-        React.useEffect(() => {
-            if (!gliderInitialized) {
-                const items = [
-                    {
-                        btnLeft: ".btnLeft",
-                        btnRight: ".btnRight",
-                        carrusel: ".carrusel__lista",
-                    },
-                ];
+    const [gliderInitialized, setGliderInitialized] = React.useState(false);
 
-                items.forEach(function (item) {
-                    const btnLeft = document.querySelector(item.btnLeft);
-                    const btnRight = document.querySelector(item.btnRight);
+    React.useEffect(() => {
+        if (!gliderInitialized) {
+            const items = [
+                {
+                    btnLeft: ".btnLeft",
+                    btnRight: ".btnRight",
+                    carrusel: ".carrusel__lista",
+                },
+            ];
 
-                    btnLeft.addEventListener("click", function (event) {
-                        event.preventDefault();
-                    });
+            items.forEach(function (item) {
+                const btnLeft = document.querySelector(item.btnLeft);
+                const btnRight = document.querySelector(item.btnRight);
 
-                    btnRight.addEventListener("click", function (event) {
-                        event.preventDefault();
-                    });
-
-                    new Glider(document.querySelector(item.carrusel), {
-                        slidesToShow: 1.2,
-                        slidesToScroll: 0.5,
-                        draggable: true,
-                        arrows: {
-                            prev: item.btnLeft,
-                            next: item.btnRight,
-                        },
-                        responsive: [
-                            {
-                                // screens greater than >= 775px
-                                breakpoint: 450,
-                                settings: {
-                                    // Set to `auto` and provide item width to adjust to viewport
-                                    slidesToShow: "2.2",
-                                    slidesToScroll: "1",
-                                },
-                            },
-                            {
-                                // screens greater than >= 1024px
-                                breakpoint: 1024,
-                                settings: {
-                                    slidesToShow: 4,
-                                    slidesToScroll: 1,
-                                },
-                            },
-                        ],
-                        rewind: true,
-                    });
+                btnLeft.addEventListener("click", function (event) {
+                    event.preventDefault();
                 });
 
-                setGliderInitialized(true);
-            }
-        }, [gliderInitialized]);
+                btnRight.addEventListener("click", function (event) {
+                    event.preventDefault();
+                });
 
-        return (
-          <>
+                new Glider(document.querySelector(item.carrusel), {
+                    slidesToShow: 1.2,
+                    slidesToScroll: 0.5,
+                    draggable: true,
+                    arrows: {
+                        prev: item.btnLeft,
+                        next: item.btnRight,
+                    },
+                    responsive: [
+                        {
+                            // screens greater than >= 775px
+                            breakpoint: 450,
+                            settings: {
+                                // Set to `auto` and provide item width to adjust to viewport
+                                slidesToShow: "2.2",
+                                slidesToScroll: "1",
+                            },
+                        },
+                        {
+                            // screens greater than >= 1024px
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                            },
+                        },
+                    ],
+                    rewind: true,
+                });
+            });
+
+            setGliderInitialized(true);
+        }
+    }, [gliderInitialized]);
+
+    return (
+        <>
             <Card destinos={destinos} />
-          </>
-        );
-      };
-      
-      ReactDOM.render(<DestinosList />, document.getElementById("carrusel__lista_uno"));
+        </>
+    );
+};
+
+ReactDOM.render(<DestinosList />, document.getElementById("carrusel__lista_uno"));
