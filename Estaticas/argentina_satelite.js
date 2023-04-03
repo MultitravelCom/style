2badb294532c7a56be2d68c863ab63a38e2e6fac
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-03 15:13:20 -0300

    cambio de titulo seccion

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -39,92 +39,98 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     const DestinosList = () => {
-
-        React.useLayoutEffect(() => {
-            const initGlider = () => {
-
-                const items = [
-                    { btnLeft: '.btnLeft', btnRight: '.btnRight', carrusel: '.carrusel__lista' },
-                    // { btnLeft: '.btnLeft2', btnRight: '.btnRight2', carrusel: '.carrusel__lista2' }
-                ];
-            
-
-                items.forEach(function (item) {
-                    const btnLeft = document.querySelector(item.btnLeft);
-                    const btnRight = document.querySelector(item.btnRight);
-
-                    btnLeft.addEventListener('click', function (event) {
-                        event.preventDefault();
-                    });
-
-                    btnRight.addEventListener('click', function (event) {
-                        event.preventDefault();
-                    });
-
-                    new Glider(document.querySelector(item.carrusel), {
-                        slidesToShow: 1.20,
-                        slidesToScroll: 0.50,
-                        draggable: true,
-                        arrows: {
-                            prev: item.btnLeft,
-                            next: item.btnRight
-                        },
-                        responsive: [
-                            {
-                                // screens greater than >= 775px
-                                breakpoint: 450,
-                                settings: {
-                                    // Set to `auto` and provide item width to adjust to viewport
-                                    slidesToShow: '2.20',
-                                    slidesToScroll: '1',
-                                }
-                            }, {
-                                // screens greater than >= 1024px
-                                breakpoint: 1024,
-                                settings: {
-                                    slidesToShow: 4,
-                                    slidesToScroll: 1,
-                                }
-                            }
-                        ],
-                        rewind: true
-                    });
-                });
-            };
-            initGlider();
-        }, []);
-
+        const [gliderInitialized, setGliderInitialized] = useState(false);
+      
+        useEffect(() => {
+          if (!gliderInitialized) {
+            const items = [
+              {
+                btnLeft: ".btnLeft",
+                btnRight: ".btnRight",
+                carrusel: ".carrusel__lista",
+              },
+            ];
+      
+            items.forEach(function (item) {
+              const btnLeft = document.querySelector(item.btnLeft);
+              const btnRight = document.querySelector(item.btnRight);
+      
+              btnLeft.addEventListener("click", function (event) {
+                event.preventDefault();
+              });
+      
+              btnRight.addEventListener("click", function (event) {
+                event.preventDefault();
+              });
+      
+              new Glider(document.querySelector(item.carrusel), {
+                slidesToShow: 1.2,
+                slidesToScroll: 0.5,
+                draggable: true,
+                arrows: {
+                  prev: item.btnLeft,
+                  next: item.btnRight,
+                },
+                responsive: [
+                  {
+                    // screens greater than >= 775px
+                    breakpoint: 450,
+                    settings: {
+                      // Set to `auto` and provide item width to adjust to viewport
+                      slidesToShow: "2.2",
+                      slidesToScroll: "1",
+                    },
+                  },
+                  {
+                    // screens greater than >= 1024px
+                    breakpoint: 1024,
+                    settings: {
+                      slidesToShow: 4,
+                      slidesToScroll: 1,
+                    },
+                  },
+                ],
+                rewind: true,
+              });
+            });
+      
+            setGliderInitialized(true);
+          }
+        }, [gliderInitialized]);
+      
         return (
-            <>
-                <div className="main__conteiner__s1">
-                    <div className="main__conteiner__s1__titulo">
-                        <h3><strong>Descuentos Imbatibles por Argentina</strong></h3>
-                    </div>
+          <>
+            <div className="main__conteiner__s1">
+              <div className="main__conteiner__s1__titulo">
+                <h3>
+                  <strong>Descuentos Imbatibles por Argentina</strong>
+                </h3>
+              </div>
+            </div>
+            <div className="carrusel__contenedor">
+              <button aria-label="Anterior" className="carrusel__anterior btnLeft">
+                <i className="fa fa-chevron-left" aria-hidden="true"></i>
+              </button>
+              <div className="carrusel__lista">
+                <div className="carrusel__elemento">
+                  {destinos.map((destino) => (
+                    <Card
+                      key={destino.id}
+                      linkWa={`https://wa.link/${destino.destino}`}
+                      destino={destino.destino}
+                      img={destino.img}
+                      className={destino.className}
+                    />
+                  ))}
                 </div>
-                <div className="carrusel__contenedor">
-                    <button aria-label="Anterior" className="carrusel__anterior btnLeft">
-                        <i className="fa fa-chevron-left" aria-hidden="true"></i>
-                    </button>
-                    <div className="carrusel__lista">
-                        <div className="carrusel__elemento">
-                            {destinos.map((destino) => (
-                                <Card
-                                    key={destino.id}
-                                    linkWa={`https://wa.link/${destino.destino}`}
-                                    destino={destino.destino}
-                                    img={destino.img}
-                                    className={destino.className}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                    <button aria-label="Siguiente" className="carrusel__siguiente btnRight">
-                        <i className="fa fa-chevron-right" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </>
+              </div>
+              <button aria-label="Siguiente" className="carrusel__siguiente btnRight">
+                <i className="fa fa-chevron-right" aria-hidden="true"></i>
+              </button>
+            </div>
+          </>
         );
-    };
+      };
 
     ReactDOM.createRoot(document.getElementById('carrusel__lista_uno')).render(<DestinosList />);
 });
