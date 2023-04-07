9fb3eb8af3354ea046491b1bfd4f94ae85d3abb3
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-06 22:55:13 -0300

    lading argentina react

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -130,52 +130,54 @@ const App = () => {
   
     React.useEffect(() => {
       if (!gliderInitialized) {
-        items.forEach(function (item) {
-          const btnLeft = document.querySelector(item.btnLeft);
-          const btnRight = document.querySelector(item.btnRight);
+        window.addEventListener("load", function() {
+          items.forEach(function (item) {
+            const btnLeft = document.querySelector(item.btnLeft);
+            const btnRight = document.querySelector(item.btnRight);
   
-          btnLeft.addEventListener("click", function (event) {
-            event.preventDefault();
-          });
+            btnLeft.addEventListener("click", function (event) {
+              event.preventDefault();
+            });
   
-          btnRight.addEventListener("click", function (event) {
-            event.preventDefault();
-          });
+            btnRight.addEventListener("click", function (event) {
+              event.preventDefault();
+            });
   
-          new Glider(document.querySelector(item.carrusel), {
-            slidesToShow: 1.2,
-            slidesToScroll: 0.5,
-            draggable: true,
-            arrows: {
-              prev: item.btnLeft,
-              next: item.btnRight,
-            },
-            responsive: [
-              {
-                // screens greater than >= 775px
-                breakpoint: 450,
-                settings: {
-                  // Set to `auto` and provide item width to adjust to viewport
-                  slidesToShow: "2.2",
-                  slidesToScroll: "1",
-                },
+            new Glider(document.querySelector(item.carrusel), {
+              slidesToShow: 1.2,
+              slidesToScroll: 0.5,
+              draggable: true,
+              arrows: {
+                prev: item.btnLeft,
+                next: item.btnRight,
               },
-              {
-                // screens greater than >= 1024px
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 4,
-                  slidesToScroll: 1,
+              responsive: [
+                {
+                  // screens greater than >= 775px
+                  breakpoint: 450,
+                  settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: "2.2",
+                    slidesToScroll: "1",
+                  },
                 },
-              },
-            ],
-            rewind: true,
+                {
+                  // screens greater than >= 1024px
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                  },
+                },
+              ],
+              rewind: true,
+            });
           });
+  
+          setGliderInitialized(true);
         });
-  
-        setGliderInitialized(true);
       }
-    }, [items]);
+    }, [gliderInitialized]);
   
     return (
       <>
