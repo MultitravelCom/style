8ef7bcfb19db0a0d2cc8329d50c84bf90dd4afd3
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-06 22:20:40 -0300

    lading argentina react

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -125,17 +125,60 @@ const Card = ({ destinos }) => {
         )));
 };
 
-// const MainContainer = ({ titles, items }) => {
-//     return (
-//         <div>
-//             <TitleContainer title={titles} />
-//             <CardContainer items={items} />
-//         </div>
-//     );
-// };
-
 const App = () => {
-
+    const [gliderInitialized, setGliderInitialized] = React.useState(false);
+    
+    React.useEffect(() => {
+        if (!gliderInitialized) {
+    
+    
+            items.forEach(function (item) {
+                const btnLeft = document.querySelector(item.btnLeft);
+                const btnRight = document.querySelector(item.btnRight);
+    
+                btnLeft.addEventListener("click", function (event) {
+                    event.preventDefault();
+                });
+    
+                btnRight.addEventListener("click", function (event) {
+                    event.preventDefault();
+                });
+    
+                new Glider(document.querySelector(item.carrusel), {
+                    slidesToShow: 1.2,
+                    slidesToScroll: 0.5,
+                    draggable: true,
+                    arrows: {
+                        prev: item.btnLeft,
+                        next: item.btnRight,
+                    },
+                    responsive: [
+                        {
+                            // screens greater than >= 775px
+                            breakpoint: 450,
+                            settings: {
+                                // Set to `auto` and provide item width to adjust to viewport
+                                slidesToShow: "2.2",
+                                slidesToScroll: "1",
+                            },
+                        },
+                        {
+                            // screens greater than >= 1024px
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                            },
+                        },
+                    ],
+                    rewind: true,
+                });
+            });
+    
+            setGliderInitialized(true);
+        }
+    }, [gliderInitialized]);
+    
     return (
         <>
             <TitleContainer titles={titles} />
@@ -147,55 +190,3 @@ const App = () => {
 ReactDOM.render(<App />, document.getElementById("containerCarrusel"));
 
 
-// const [gliderInitialized, setGliderInitialized] = React.useState(false);
-
-// React.useEffect(() => {
-//     if (!gliderInitialized) {
-
-
-//         items.forEach(function (item) {
-//             const btnLeft = document.querySelector(item.btnLeft);
-//             const btnRight = document.querySelector(item.btnRight);
-
-//             btnLeft.addEventListener("click", function (event) {
-//                 event.preventDefault();
-//             });
-
-//             btnRight.addEventListener("click", function (event) {
-//                 event.preventDefault();
-//             });
-
-//             new Glider(document.querySelector(item.carrusel), {
-//                 slidesToShow: 1.2,
-//                 slidesToScroll: 0.5,
-//                 draggable: true,
-//                 arrows: {
-//                     prev: item.btnLeft,
-//                     next: item.btnRight,
-//                 },
-//                 responsive: [
-//                     {
-//                         // screens greater than >= 775px
-//                         breakpoint: 450,
-//                         settings: {
-//                             // Set to `auto` and provide item width to adjust to viewport
-//                             slidesToShow: "2.2",
-//                             slidesToScroll: "1",
-//                         },
-//                     },
-//                     {
-//                         // screens greater than >= 1024px
-//                         breakpoint: 1024,
-//                         settings: {
-//                             slidesToShow: 4,
-//                             slidesToScroll: 1,
-//                         },
-//                     },
-//                 ],
-//                 rewind: true,
-//             });
-//         });
-
-//         setGliderInitialized(true);
-//     }
-// }, [gliderInitialized]);
