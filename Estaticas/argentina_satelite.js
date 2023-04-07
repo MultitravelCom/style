d399ff9a229801ce2934151fafd9f7ec35e2f398
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-07 12:10:33 -0300

    lading argentina react

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -73,56 +73,56 @@ const titles = [
     { title: 'Descuentos Imbatibles por Argentina' },
 ];
 
-const items = [
-    { btnLeft: ".btnLeft", btnRight: ".btnRight", carrusel: ".carrusel__lista" },
-    { btnLeft: ".btnLeft2", btnRight: ".btnRight2", carrusel: ".carrusel__lista2" },
-    { btnLeft: ".btnLeft3", btnRight: ".btnRight3", carrusel: ".carrusel__lista3" },
-  ]
+// const items = [
+//     { btnLeft: ".btnLeft", btnRight: ".btnRight", carrusel: ".carrusel__lista" },
+//     { btnLeft: ".btnLeft2", btnRight: ".btnRight2", carrusel: ".carrusel__lista2" },
+//     { btnLeft: ".btnLeft3", btnRight: ".btnRight3", carrusel: ".carrusel__lista3" },
+//   ]
 
-const initializeGlider = (items) => {
-    items.forEach(function (item) {
-      const btnLeft = document.querySelector(item.btnLeft);
-      const btnRight = document.querySelector(item.btnRight);
+// const initializeGlider = (items) => {
+//     items.forEach(function (item) {
+//       const btnLeft = document.querySelector(item.btnLeft);
+//       const btnRight = document.querySelector(item.btnRight);
 
-      btnLeft.addEventListener("click", function (event) {
-        event.preventDefault();
-      });
+//       btnLeft.addEventListener("click", function (event) {
+//         event.preventDefault();
+//       });
 
-      btnRight.addEventListener("click", function (event) {
-        event.preventDefault();
-      });
+//       btnRight.addEventListener("click", function (event) {
+//         event.preventDefault();
+//       });
 
-      new Glider(document.querySelector(item.carrusel), {
-        slidesToShow: 1.2,
-        slidesToScroll: 0.5,
-        draggable: true,
-        arrows: {
-          prev: item.btnLeft,
-          next: item.btnRight,
-        },
-        responsive: [
-          {
-            // screens greater than >= 775px
-            breakpoint: 450,
-            settings: {
-              // Set to `auto` and provide item width to adjust to viewport
-              slidesToShow: "2.2",
-              slidesToScroll: "1",
-            },
-          },
-          {
-            // screens greater than >= 1024px
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 1,
-            },
-          },
-        ],
-        rewind: true,
-      });
-    });
-  };
+//       new Glider(document.querySelector(item.carrusel), {
+//         slidesToShow: 1.2,
+//         slidesToScroll: 0.5,
+//         draggable: true,
+//         arrows: {
+//           prev: item.btnLeft,
+//           next: item.btnRight,
+//         },
+//         responsive: [
+//           {
+//             // screens greater than >= 775px
+//             breakpoint: 450,
+//             settings: {
+//               // Set to `auto` and provide item width to adjust to viewport
+//               slidesToShow: "2.2",
+//               slidesToScroll: "1",
+//             },
+//           },
+//           {
+//             // screens greater than >= 1024px
+//             breakpoint: 1024,
+//             settings: {
+//               slidesToShow: 4,
+//               slidesToScroll: 1,
+//             },
+//           },
+//         ],
+//         rewind: true,
+//       });
+//     });
+//   };
 
 const TitleContainer = ({ titles }) => {
     return (
@@ -137,9 +137,9 @@ const TitleContainer = ({ titles }) => {
   };
 
   const CardContainer = ({ items }) => {
-      React.useEffect(() => {
-        initializeGlider(items);
-      }, []);
+    //   React.useEffect(() => {
+    //     initializeGlider(items);
+    //   }, []);
   
     return (
       <div className="carrusel__contenedor">
@@ -186,12 +186,11 @@ const App = () => {
     return (
       <>
         <TitleContainer titles={titles} />
-        <CardContainer  />
+        <CardContainer  btnLeft={btnLeft} btnRight={btnRight} carrusel={carrusel__lista}/>
       </>
     );
   }
 
-  
 ReactDOM.render(<App />, document.getElementById("containerCarrusel"));
 
 
