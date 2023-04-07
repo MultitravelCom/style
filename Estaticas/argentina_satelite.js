341ddcf87c2161f62e4d652030f28568f83bcd8b
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-07 17:15:14 -0300

    lading argentina react

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -78,6 +78,52 @@ const items = [
     { btnLeft: "btnLeft3", btnRight: "btnRight3", carrusel: "carrusel__lista3" },
 ]
 
+const useGlider = (items) => {
+    useEffect(() => {
+      items.forEach((item) => {
+        const btnLeft = document.querySelector(item.btnLeft);
+        const btnRight = document.querySelector(item.btnRight);
+  
+        btnLeft.addEventListener('click', function (event) {
+          event.preventDefault();
+        });
+  
+        btnRight.addEventListener('click', function (event) {
+          event.preventDefault();
+        });
+  
+        new Glider(document.querySelector(item.carrusel), {
+          slidesToShow: 1.20,
+          slidesToScroll: 0.50,
+          draggable: true,
+          arrows: {
+            prev: item.btnLeft,
+            next: item.btnRight
+          },
+          responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 450,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: '2.20',
+                slidesToScroll: '1',
+              }
+            }, {
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+              }
+            }
+          ],
+          rewind: true 
+        });
+      });
+    }, [items]);
+  };
+
 const TitleContainer = ({ titles }) => {
     return (
         <div className="main__conteiner__s1">
@@ -91,6 +137,9 @@ const TitleContainer = ({ titles }) => {
 };
 
 const CardContainer = ({ items }) => {
+
+    useGlider(items);
+
     return (
         <div className="carrusel__contenedor">
             <button
