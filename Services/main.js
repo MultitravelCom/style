891cc400be3f4dedb1ee55915ebadb6f41f33033
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-11 18:28:59 -0300

    arreglos general B1

--- Services/main.js
+++ Services/main.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    // Acomoda #bannerCentral al top y cambia la clase de none a inline.
-    let moverA = document.querySelector('#home-content-1');
-    let moverEn = document.querySelector('#banner__central')
-    let primerHijo = moverA.firstChild;
+    // // Acomoda #bannerCentral al top y cambia la clase de none a inline.
+    // let moverA = document.querySelector('#home-content-1');
+    // let moverEn = document.querySelector('#banner__central')
+    // let primerHijo = moverA.firstChild;
     
-    moverA.insertBefore(moverEn, primerHijo);
-    moverEn.style.display = 'inline-block';
+    // moverA.insertBefore(moverEn, primerHijo);
+    // moverEn.style.display = 'inline-block';
 
 // Cambiar Copy B1
-const textBtn = document.querySelector('.booking-data__section-subtitle');
-textBtn.textContent = 'Indica el detalle de tu vuelo y hotel';
+const sectionSubtitle = document.querySelector('.booking-data__section-subtitle');
+sectionSubtitle.textContent = 'Indica el detalle de tu vuelo y hotel';
 
 document.querySelector('.booking-data__section-subtitle').style.display = 'inline-block';
 
