14d1ff93a3fd19e5e833d379053ffca7898cffaa
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-13 18:49:21 -0300

    agregar copy tax

--- General/main.js
+++ General/main.js
@@ -1,21 +1,13 @@
-// Seleccionar el elemento padre que contiene todos los elementos 'bestprice'
-let parentElement = document.body; // por ejemplo, aquí se está seleccionando el body, pero podría ser cualquier otro elemento
 
-// Crear un observer para detectar cambios en el DOM
-let observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    // Verificar si se agregó un nuevo elemento con la clase 'bestprice'
-    let newDivBestprice = mutation.target.querySelector('.bestprice');
-    if (newDivBestprice) {
-      // Agregar el nuevo elemento al elemento 'bestprice'
-      let titleElement = document.createElement('div');
-      titleElement.className = 'bestprice__title';
-      titleElement.innerHTML = '<span>Impuestos incluidos</span>';
-      newDivBestprice.appendChild(titleElement);
-    }
-  });
+document.addEventListener('DOMContentLoaded', () => {
+    let divBestprices = document.querySelectorAll('.bestprice');
+    
+    divBestprices.forEach((divBestprice) => {
+
+        let newDivBestprice = document.createElement('div');
+        newDivBestprice.className = 'bestprice__title';
+        newDivBestprice.innerHTML = `<span>Impuestos incluidos</span>`;
+
+        divBestprice.appendChild(newDivBestprice);
+    })
 });
-
-// Configurar el observer para observar cambios en el elemento padre y sus hijos
-let observerConfig = { childList: true, subtree: true };
-observer.observe(parentElement, observerConfig);
