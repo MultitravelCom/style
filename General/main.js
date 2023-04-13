4e4f2455f8c1d039ea800028652c1b4cf961734a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-13 19:01:24 -0300

    agregar copy tax

--- General/main.js
+++ General/main.js
@@ -1,20 +1,22 @@
 
-const bodyNode = document.querySelector('body');
-const observerConfig = { childList: true, subtree: true };
-
-const handleMutation = (mutationList) => {
-  mutationList.forEach((mutation) => {
-    const addedNodes = mutation.addedNodes;
-    addedNodes.forEach((node) => {
-      if (node.classList && node.classList.contains('bestprice')) {
-        const newDivBestprice = document.createElement('div');
-        newDivBestprice.className = 'bestprice__title';
-        newDivBestprice.innerHTML = `<span>Impuestos incluidos</span>`;
-        node.appendChild(newDivBestprice);
-      }
-    });
-  });
-};
-
-const observer = new MutationObserver(handleMutation);
-observer.observe(bodyNode, observerConfig);
+document.addEventListener('DOMContentLoaded', () => {
+    const bodyNode = document.querySelector('body');
+    const observerConfig = { childList: true, subtree: true };
+    
+    const handleMutation = (mutationList) => {
+      mutationList.forEach((mutation) => {
+        const addedNodes = mutation.addedNodes;
+        addedNodes.forEach((node) => {
+          if (node.classList && node.classList.contains('bestprice')) {
+            const newDivBestprice = document.createElement('div');
+            newDivBestprice.className = 'bestprice__title';
+            newDivBestprice.innerHTML = `<span>Impuestos incluidos</span>`;
+            node.appendChild(newDivBestprice);
+          }
+        });
+      });
+    };
+    
+    const observer = new MutationObserver(handleMutation);
+    observer.observe(bodyNode, observerConfig);
+});
