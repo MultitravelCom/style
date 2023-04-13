2921d2dff70ee14de292d7eae12f82d24c354338
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-13 19:13:02 -0300

    agregar copy tax

--- General/main.js
+++ General/main.js
@@ -1,27 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-   // Selecciona el elemento que quieres observar
-const resultsWrapper = document.querySelector('.js-results-wrapper');
+    let divBestprices = document.querySelectorAll('.bestprice');
+    
+    divBestprices.forEach((divBestprice) => {
 
-// Crea un nuevo observador de mutaciones
-const observer = new MutationObserver((mutationsList) => {
-  for(let mutation of mutationsList) {
-    if (mutation.type === 'childList' && mutation.addedNodes.length) {
-      mutation.addedNodes.forEach((node) => {
-        // Si el nodo agregado tiene la clase 'bestprice'
-        if (node.classList && node.classList.contains('bestprice')) {
-          // Agrega el contenido deseado
-          const newDivBestprice = document.createElement('div');
-          newDivBestprice.className = 'bestprice__title';
-          newDivBestprice.innerHTML = '<span>Impuestos incluidos</span>';
+        let newDivBestprice = document.createElement('div');
+        newDivBestprice.className = 'bestprice__title';
+        newDivBestprice.innerHTML = `<span>Impuestos incluidos</span>`;
 
-          node.appendChild(newDivBestprice);
-        }
-      });
-    }
-  }
-});
-
-// Configura el observador para que observe cambios en el DOM
-observer.observe(resultsWrapper, { childList: true, subtree: true });
+        divBestprice.appendChild(newDivBestprice);
+    })
 });
