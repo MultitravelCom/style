5224de29b057598d2f8a5e4f95730a8075d61d5a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-13 17:02:01 -0300

    agregar copy tax

--- General/main.js
+++ General/main.js
@@ -1,5 +1,6 @@
 let divBestprice = document.querySelector('.bestprice');
 let newDivBestprice = document.createElement('div');
 newDivBestprice.className = 'bestprice__title';
-newDivBestprice.innerHTML = `<span>Impuestos incluidos</span>`
+newDivBestprice.innerHTML = `<span>Impuestos incluidos</span>`;
+
 divBestprice.appendChild(newDivBestprice);
