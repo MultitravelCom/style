d5784cd2e96be13436cea1643e1205cd9ea1f4d1
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-14 11:52:42 -0300

    web satelite Argentina React

--- General/main.js
+++ General/main.js
@@ -22,23 +22,26 @@ async function waitForElement() {
 waitForElement();
 });
 
-async function waitForElementFlight() {
-    while (true) {
-      const divTaxes = document.querySelectorAll('.results-list__item');
-      if (divTaxes.length > 0) {
-        divTaxes.forEach((divTax) => {
-          const divCopyTaxes = divTax.querySelector('.bestprice__amount');
-          const newDivTax = document.createElement('span');
-          newDivTax.className = 'renderPriceTag';
-          newDivTax.innerHTML = '<span>Precio Final</span>';
-          divCopyTaxes.appendChild(newDivTax);
-        });
-        break;
+document.addEventListener('DOMContentLoaded', () => {
+    async function waitForElementFlight() {
+      while (true) {
+        const divTaxes = document.querySelectorAll('.results-list__item');
+        const divCopyTaxes = document.querySelectorAll('.bestprice__amount')
+        if (divTaxes.length > 0) {
+            divTaxes.forEach((divTax) => {
+            const newDivTax = document.createElement('span');
+            newDivTax.className = 'renderPriceTag';
+            newDivTax.innerHTML = '<span>Precio Final</span>';
+            const firstChild = divCopyTaxes[0].firstElementChild;
+            divCopyTaxes[0].insertBefore(newDivTax, firstChild);
+          });
+          break;
+        }
+        // Esperar 100ms y volver a intentar
+        await new Promise(resolve => setTimeout(resolve, 100));
       }
-      // Esperar 100ms y volver a intentar
-      await new Promise(resolve => setTimeout(resolve, 100));
     }
-  }
   
-  // Llamar a la función para esperar a que se cargue la clase .bestprice__amount
-  waitForElementFlight();
+    // Llamar a la función para esperar a que se cargue la clase .bestprice
+    waitForElementFlight();
+  });
