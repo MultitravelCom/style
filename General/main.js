5d11e5b6b5eeb6b6c89284732c973a806990d2fb
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-14 10:31:16 -0300

    web satelite Argentina React

--- General/main.js
+++ General/main.js
@@ -1,22 +1,34 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Seleccionar el div que contiene la clase .bestprice
-const resultsWrapper = document.querySelector('.bestprice');
+// Seleccionar los divs que contienen las clases .bestprice y .product-price
+const bestPriceDivs = document.querySelectorAll('.bestprice');
+const productPriceDivs = document.querySelectorAll('.details-card');
 
 // Crear un observador de mutación
 const observer = new MutationObserver(() => {
-  // Seleccionar todos los divs que contienen la clase .bestprice
-  const divBestprices = document.querySelectorAll('.bestprice');
-
-  // Recorrer todos los divs y agregar el nuevo div con el texto "Impuestos incluidos"
-  divBestprices.forEach((divBestprice) => {
+  // Recorrer todos los divs que contienen la clase .bestprice y agregar el nuevo div con el texto "Impuestos incluidos"
+  bestPriceDivs.forEach((divBestprice) => {
     const newDivBestprice = document.createElement('div');
     newDivBestprice.className = 'bestprice__title';
     newDivBestprice.innerHTML = '<span>Impuestos incluidos</span>';
     divBestprice.appendChild(newDivBestprice);
   });
+
+  // Recorrer todos los divs que contienen la clase .product-price y agregar el nuevo div con el texto "Impuestos incluidos"
+  productPriceDivs.forEach((divProductPrice) => {
+    const newDivProductPrice = document.createElement('div');
+    newDivProductPrice.className = 'product-price__title';
+    newDivProductPrice.innerHTML = '<span>Impuestos incluidos</span>';
+    divProductPrice.appendChild(newDivProductPrice);
+  });
 });
 
-// Configurar el observador para observar cambios en el nodo de resultados y sus hijos
-observer.observe(resultsWrapper, { childList: true, subtree: true });
+// Configurar el observador para observar cambios en los nodos de las clases y sus hijos
+bestPriceDivs.forEach((divBestprice) => {
+  observer.observe(divBestprice, { childList: true, subtree: true });
+});
+
+productPriceDivs.forEach((divProductPrice) => {
+  observer.observe(divProductPrice, { childList: true, subtree: true });
+});
 });
