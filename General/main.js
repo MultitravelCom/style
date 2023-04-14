4c2023bb5dd51049afbd47cb79ac0f8d1102530d
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-14 11:04:39 -0300

    web satelite Argentina React

--- General/main.js
+++ General/main.js
@@ -1,23 +1,22 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    async function waitForElement() {
-        while (true) {
-          const divBestprices = document.querySelectorAll('.bestprice');
-          if (divBestprices.length > 0) {
-            // La clase .bestprice existe, entonces ejecutar tu función aquí
-            divBestprices.forEach((divBestprice) => {
-              const newDivBestprice = document.createElement('div');
-              newDivBestprice.className = 'bestprice__title';
-              newDivBestprice.innerHTML = '<span>Impuestos incluidos</span>';
-              divBestprice.appendChild(newDivBestprice);
-            });
-            break;
-          }
-          // Esperar 100ms y volver a intentar
-          await new Promise(resolve => setTimeout(resolve, 100));
-        }
-      }
-      
-      // Llamar a la función para esperar a que se cargue la clase .bestprice
-      waitForElement();
+    // Función para agregar el nuevo div con el texto "Impuestos incluidos"
+function addTaxes() {
+    const divBestprices = document.querySelectorAll('.bestprice');
+    divBestprices.forEach((divBestprice) => {
+      const newDivBestprice = document.createElement('div');
+      newDivBestprice.className = 'bestprice__title';
+      newDivBestprice.innerHTML = '<span>Impuestos incluidos</span>';
+      divBestprice.appendChild(newDivBestprice);
+    });
+  }
+  
+  // Intervalo para comprobar si el elemento ya está presente en el DOM
+  const interval = setInterval(() => {
+    const divBestprices = document.querySelectorAll('.bestprice');
+    if (divBestprices.length > 0) {
+      clearInterval(interval);
+      addTaxes();
+    }
+  }, 1000);
 });
