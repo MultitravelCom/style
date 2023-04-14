d83013eb511bb629be08a1d521bcb876edc727f4
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-14 11:45:17 -0300

    web satelite Argentina React

--- General/main.js
+++ General/main.js
@@ -23,16 +23,17 @@ waitForElement();
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    async function waitForElement() {
+    async function waitForElementFlight() {
       while (true) {
         const divTaxes = document.querySelectorAll('.results-list__item');
         const divCopyTaxes = document.querySelectorAll('.bestprice__amount')
         if (divTaxes.length > 0) {
-            divTaxes.forEach((divTax) => {
+          divTaxes.forEach((divTax, index) => {
             const newDivTax = document.createElement('span');
             newDivTax.className = 'renderPriceTag';
             newDivTax.innerHTML = '<span>Precio Final</span>';
-            divTax.appendChild(divCopyTaxes);
+            divTax.appendChild(newDivTax);
+            divTax.appendChild(divCopyTaxes[index]);
           });
           break;
         }
@@ -40,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
         await new Promise(resolve => setTimeout(resolve, 100));
       }
     }
-    
+  
     // Llamar a la función para esperar a que se cargue la clase .bestprice
-    waitForElement();
-    });
+    waitForElementFlight();
+  });
