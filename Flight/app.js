3f493b7d05ac6c9fef791cdcd869650a264aee78
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-17 17:38:09 -0300

    bannersearch

--- Flight/app.js
+++ Flight/app.js
@@ -1,26 +1,27 @@
 const BannerSearchResult = () => {
     return (
-        <div className='container'>
-            <h1>Test</h1>
-        </div>
+        <>
+            <div className='container'>
+                <h1>Test</h1>
+            </div>
+        </>
     );
 };
-
 
 document.addEventListener('DOMContentLoaded', () => {
     async function crearDivYRenderizarComponente(componente) {
         let placeholder = document.querySelector('.results-list__body.js-results-list-placeholder');
-      
+
         while (!placeholder) {
-          await new Promise(resolve => setTimeout(resolve, 500));
-          placeholder = document.querySelector('.results-list__body.js-results-list-placeholder');
+            await new Promise(resolve => setTimeout(resolve, 500));
+            placeholder = document.querySelector('.results-list__body.js-results-list-placeholder');
         }
-      
+
         const newDiv = document.createElement('div');
         placeholder.insertBefore(newDiv, placeholder.childNodes[1]);
-      
-        ReactDOM.render(componente, newDiv);
-      }
 
-      crearDivYRenderizarComponente(<BannerSearchResult />);
+        ReactDOM.render(componente, newDiv);
+    }
+
+    crearDivYRenderizarComponente(<BannerSearchResult />);
 });
