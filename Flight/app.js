754425e6e268632aff00111a3febb72733916559
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-17 17:01:05 -0300

    bannersearch

--- Flight/app.js
+++ Flight/app.js
@@ -18,6 +18,20 @@
 //     );
 // };
 
+{/* <div>
+<span>¿Necesitas ayuda con tu reserva?</span>
+<p>Contactate con nosotros a través de WhatsApp y recibí atención personalizada</p>
+</div>
+<div>
+<picture>
+    <source media="(min-width: 1024px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-01.webp" />
+    <source media="(min-width: 768px) and (max-width: 1023px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-02.webp" />
+    <source media="(max-width: 767px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-03.webp" />
+    <img alt={`Imagen banner Ahora12`} src="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-01.webp" />
+</picture>
+</div>
+<button>contactarme</button> */}
+
 function RenderizarBannerSearchResult() {
     React.useEffect(() => {
         const placeholder = document.querySelector('.results-list__body.js-results-list-placeholder');
@@ -31,19 +45,7 @@ function RenderizarBannerSearchResult() {
 
     return (
         <div className='container'>
-            <div>
-                <span>¿Necesitas ayuda con tu reserva?</span>
-                <p>Contactate con nosotros a través de WhatsApp y recibí atención personalizada</p>
-            </div>
-            <div>
-                <picture>
-                    <source media="(min-width: 1024px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-01.webp" />
-                    <source media="(min-width: 768px) and (max-width: 1023px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-02.webp" />
-                    <source media="(max-width: 767px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-03.webp" />
-                    <img alt={`Imagen banner Ahora12`} src="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-01.webp" />
-                </picture>
-            </div>
-            <button>contactarme</button>
+            <p>test</p>
         </div>
     );
 };
