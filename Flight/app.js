04d3da88304d2fd94d2bbbf40e31ebd65935945d
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-17 17:34:59 -0300

    bannersearch

--- Flight/app.js
+++ Flight/app.js
@@ -1,19 +1,7 @@
 const BannerSearchResult = () => {
     return (
         <div className='container'>
-            <div>
-                <span>¿Necesitas ayuda con tu reserva?</span>
-                <p>Contactate con nosotros a través de WhatsApp y recibí atención personalizada</p>
-            </div>
-            <div>
-                <picture>
-                    <source media="(min-width: 1024px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-01.webp" />
-                    <source media="(min-width: 768px) and (max-width: 1023px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-02.webp" />
-                    <source media="(max-width: 767px)" srcSet="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-03.webp" />
-                    <img alt={`Imagen banner Ahora12`} src="https://multitravelcom.github.io/MT/Secciones/MediosPagos/ahora12/ahora12-01.webp" />
-                </picture>
-            </div>
-            <button>contactarme</button>
+            <h1>Test</h1>
         </div>
     );
 };
