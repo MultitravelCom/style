f005c6c462003911e1629975a50cd5f48e7fa8dd
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-17 17:55:45 -0300

    bannersearch

--- Flight/app.js
+++ Flight/app.js
@@ -8,11 +8,11 @@ const BannerSearchResult = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     async function crearDivYRenderizarComponente(componente) {
-        let placeholder = document.querySelector('.results-list__body.js-results-list-placeholder');
+        let placeholder = document.querySelector('.js-results-list-placeholder');
 
         while (!placeholder) {
             await new Promise(resolve => setTimeout(resolve, 500));
-            placeholder = document.querySelector('.results-list__body.js-results-list-placeholder');
+            placeholder = document.querySelector('.js-results-list-placeholder');
         }
 
         const newDiv = document.createElement('div');
