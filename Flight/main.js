dcd88b1b3bfef4f069e53b68c59b601d4f1f8638
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-18 18:34:35 -0300

    bannerSearchResult

--- Flight/main.js
+++ Flight/main.js
@@ -28,23 +28,6 @@ window.addEventListener('load', () => {
     aplicarEstilos();
 
     window.addEventListener('resize', aplicarEstilos);
-    
-    const insertNewDivSearchResultFlight = async () => {
-        while (true) {
-            const parentDiv = document.querySelector('.results-list__body');
-            const firstChdtDiv = document.querySelector('.results-list__item');
-    
-            if (parentDiv && firstChdtDiv) {
-                const newDiv = document.createElement('div');
-                firstChdtDiv.after(newDiv);
-                ReactDOM.render(<BannerSearchResult />, newDiv);
-                break;
-            }
-    
-            await new Promise(resolve => setTimeout(resolve, 100));
-        }
-    }
-    
-    insertNewDivSearchResultFlight();
+
 });
 
--- General/bannerComponent.js
+++ General/bannerComponent.js
@@ -1,3 +1,21 @@
+const insertNewDivSearchResultFlight = async () => {
+    while (true) {
+        const parentDiv = document.querySelector('.results-list__body');
+        const firstChdtDiv = document.querySelector('.results-list__item');
+
+        if (parentDiv && firstChdtDiv) {
+            const newDiv = document.createElement('div');
+            firstChdtDiv.after(newDiv);
+            ReactDOM.render(<BannerSearchResult />, newDiv);
+            break;
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+}
+
+insertNewDivSearchResultFlight();
+
 function Button(props) {
 
     const handleClick = (event) => {
