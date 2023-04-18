f4ae3a744a412fb672167754504ccd7de8d51bb5
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-18 11:45:56 -0300

    bannerSearchResult

--- Flight/app.js
+++ Flight/app.js
@@ -1,25 +1,39 @@
 const BannerSearchResult = () => {
     return (
-      <div>
-        <h1>Test</h1>
-      </div>
+        <>
+            <div className="container">
+                <div>
+                    <h2>¿Necesitas ayuda con tu reserva?</h2>
+                    <p>Contactate con nosotros a través de WhatsApp y recibí atención personalizada</p>
+                </div>
+                <div>
+                    <picture>
+                        <source media="(min-width: 1024px)" srcset="https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/barilo-1.jpg" />
+                        <source media="(min-width: 768px) and (max-width: 1023px)" srcset="https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/barilo-2.jpg" />
+                        <source media="(max-width: 767px)" srcset="https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/barilo-3.jpg" />
+                        <img id="a1" src="https://multitravelcom.github.io/MT/TravelSale2023/LandingOFF/Nacional/barilo-1.jpg" alt="Imagen banner Bariloche promo" />
+                    </picture>
+                </div>
+                <button className="btnStyleBannerSearch">Contactarme</button>
+            </div>
+        </>
     );
-  };
-  
-  const waitForElement = async () => {
+};
+
+const insertNewDivSearchResult = async () => {
     while (true) {
-      const parentDiv = document.querySelector('.results-list__body');
-      const targetDiv = document.querySelector('.results-list__item');
-  
-      if (parentDiv && targetDiv) {
-        const newDiv = document.createElement('div');
-        targetDiv.after(newDiv);
-        ReactDOM.render(<BannerSearchResult />, newDiv);
-        break;
-      }
-  
-      await new Promise(resolve => setTimeout(resolve, 100));
+        const parentDiv = document.querySelector('.results-list__body');
+        const firstChdtDiv = document.querySelector('.results-list__item');
+
+        if (parentDiv && firstChdtDiv) {
+            const newDiv = document.createElement('div');
+            firstChdtDiv.after(newDiv);
+            ReactDOM.render(<BannerSearchResult />, newDiv);
+            break;
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 100));
     }
-  }
-  
-  waitForElement();
+}
+
+insertNewDivSearchResult();
