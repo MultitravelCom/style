a437f400fc1535737b5a07304b8cce42624256a4
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-17 21:24:24 -0300

    bannerSearchResult

--- Flight/app.js
+++ Flight/app.js
@@ -6,21 +6,22 @@ const BannerSearchResult = () => {
     );
   };
   
-  const App = () => {
-    useEffect(() => {
-      const placeholder = document.querySelector('.results-list__body.js-results-list-placeholder');
-  
-      if (placeholder) {
-        const newDiv = document.createElement('div');
-        placeholder.insertBefore(newDiv, placeholder.childNodes[1]);
-        ReactDOM.render(<BannerSearchResult />, newDiv);
+  const waitForElement = async () => {
+    while (true) {
+      const totalLineDiv = document.querySelector('.results-list__body');
+      const conceptDiv = document.querySelector('.results-list__item');
+      if (totalLineDiv && conceptDiv) {
+        const newSpan = document.createElement('span');
+        newSpan.className = 'flight-selection__breakdown-concept';
+        newSpan.textContent = 'Precio Final';
+        totalLineDiv.insertBefore(newSpan, totalLineDiv.firstChild);
+        conceptDiv.style.display = 'none';
+        break;
       }
-    }, []);
+      // Esperar 100ms y volver a intentar
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+  }
   
-    return (
-      <div>
-        <div className="results-list__body js-results-list-placeholder"></div>
-      </div>
-    );
-  };
+  waitForElement();
   
