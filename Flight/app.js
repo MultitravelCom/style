fa303683c21a2f2d74899dd8f9458dc3d5f07319
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-17 21:53:00 -0300

    bannerSearchResult

--- Flight/app.js
+++ Flight/app.js
@@ -1,21 +1,23 @@
-// const BannerSearchResult = () => {
-//     return (
-//       <div>
-//         <h1>Test</h1>
-//       </div>
-//     );
-//   };
+const BannerSearchResult = () => {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  };
   
   const waitForElement = async () => {
     while (true) {
       const parentDiv = document.querySelector('.results-list__body');
-      const firstItemDiv = parentDiv.querySelector('.results-list__item');
-      if (parentDiv && firstItemDiv) {
+      const targetDiv = document.querySelector('.results-list__item');
+  
+      if (parentDiv && targetDiv) {
         const newDiv = document.createElement('div');
-        newDiv.textContent = 'Nuevo div creado';
-        parentDiv.insertBefore(newDiv, firstItemDiv.nextSibling);
+        targetDiv.after(newDiv);
+        ReactDOM.render(<BannerSearchResult />, newDiv);
         break;
       }
+  
       await new Promise(resolve => setTimeout(resolve, 100));
     }
   }
