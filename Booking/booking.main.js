1b99c806a4c32384ca45694837aff695caa45e86
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-19 17:04:14 -0300

    bannersearch

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -1,26 +1,26 @@
-window.addEventListener('load', () => {
-    let bookingData = document.querySelector('.booking-data__data');
-    // let insurances = bookingData.querySelector('.confirm-booking__insurances');
-    let payment = bookingData.querySelector('.booking-payment');
-    let newDiv = document.createElement('div');
-    newDiv.innerHTML = `
-    <div class="confirm-booking__section banner__promo">    
-        <div class="promo__ar">
-            <div class="promo__ar__icon">
-                <i class="glyphicon glyphicon-agent" aria-hidden="true"></i>
-            </div>
-            <div class="promo__ar_parrafo">
-                <h3>¿Sabías que tenemos 6 cuotas sin interés con Aerolíneas Argentinas?</h3>
-                <p>Estas son ofertas exclusivas offline en vuelos por Argentina</p>
-            </div>
-                <button class="btn btn-primary promo__ar__btn">
-                    <a style="color: white; text-decoration: none" target="_blank" href="https://wa.link/v9ejqn">Contactarme</a>
-                </button>
-        </div>
-    </div>
-    `;
-    bookingData.insertBefore(newDiv, payment);
-});
+// window.addEventListener('load', () => {
+//     let bookingData = document.querySelector('.booking-data__data');
+//     // let insurances = bookingData.querySelector('.confirm-booking__insurances');
+//     let payment = bookingData.querySelector('.booking-payment');
+//     let newDiv = document.createElement('div');
+//     newDiv.innerHTML = `
+//     <div class="confirm-booking__section banner__promo">    
+//         <div class="promo__ar">
+//             <div class="promo__ar__icon">
+//                 <i class="glyphicon glyphicon-agent" aria-hidden="true"></i>
+//             </div>
+//             <div class="promo__ar_parrafo">
+//                 <h3>¿Sabías que tenemos 6 cuotas sin interés con Aerolíneas Argentinas?</h3>
+//                 <p>Estas son ofertas exclusivas offline en vuelos por Argentina</p>
+//             </div>
+//                 <button class="btn btn-primary promo__ar__btn">
+//                     <a style="color: white; text-decoration: none" target="_blank" href="https://wa.link/v9ejqn">Contactarme</a>
+//                 </button>
+//         </div>
+//     </div>
+//     `;
+//     bookingData.insertBefore(newDiv, payment);
+// });
 
 
 
--- General/main.js
+++ General/main.js
@@ -1,26 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // async function waitForElement() {
-    //     while (true) {
-    //         const divBestprices = document.querySelectorAll('.bestprice');
-    //         if (divBestprices.length > 0) {
-    //             divBestprices.forEach((divBestprice) => {
-    //                 const newDivBestprice = document.createElement('div');
-    //                 newDivBestprice.className = 'bestprice__title';
-    //                 newDivBestprice.innerHTML = '<span>Impuestos incluidos</span>';
-    //                 divBestprice.appendChild(newDivBestprice);
-    //             });
-    //             break;
-    //         }
-    //         // Esperar 100ms y volver a intentar
-    //         await new Promise(resolve => setTimeout(resolve, 100));
-    //     }
-    // }
-
-    // // Llamar a la función para esperar a que se cargue la clase .bestprice
-    // waitForElement();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
     async function waitForElementFlight() {
         while (true) {
             const divTaxes = document.querySelectorAll('.results-list__item');
