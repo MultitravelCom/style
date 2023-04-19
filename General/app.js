327bb8fa5225b881957eb50de99e4d4516f0a8f3
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-19 10:44:42 -0300

    bannersearch

--- General/app.js
+++ General/app.js
@@ -101,7 +101,7 @@ const Secciones = ({ seccion, url, className }) => {
 const Ofertas = (props) => {
     return (
         <>
-            <h2 className="main__conteiner__titulo">Lo mejor de Multitravel.com</h2>
+            <h2 className="main__conteiner__titulo">Más ofertas con PreViaje</h2>
             <div className="main__conteiner">
                 <div className="main__conteiner__s3__piensa row-cols-3">
                     <Secciones seccion={props.seccion1} url={props.url1} className={props.className1} />
