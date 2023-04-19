3cac92f2d2d94a277d6952f26e3e83fd474c2959
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-19 15:33:18 -0300

    bannersearch

---
+++ General/contactoComponent.js
@@ -0,0 +1,15 @@
+// <!-- ICONO -->
+<link
+  href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+  rel="stylesheet"
+/>
+<details>
+    <summary>
+        <div><i class="bx bx-chevron-down"></i></div>
+        <div>Atencion personalizada</div>
+    </summary>
+    <div class="acordeonStyle">
+        <div><i class="bx bx-chevron-down"></i>11 4979 1877</div>
+        <div><i class="bx bx-chevron-down"></i>0800 348 0003</div>
+    </div>
+</details>
