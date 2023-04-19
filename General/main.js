340c9b24a02234a6613dda552aef4c1468aeb047
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-19 14:42:11 -0300

    bannersearch

--- General/main.js
+++ General/main.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    async function waitForElement() {
-        while (true) {
-            const divBestprices = document.querySelectorAll('.bestprice');
-            if (divBestprices.length > 0) {
-                divBestprices.forEach((divBestprice) => {
-                    const newDivBestprice = document.createElement('div');
-                    newDivBestprice.className = 'bestprice__title';
-                    newDivBestprice.innerHTML = '<span>Impuestos incluidos</span>';
-                    divBestprice.appendChild(newDivBestprice);
-                });
-                break;
-            }
-            // Esperar 100ms y volver a intentar
-            await new Promise(resolve => setTimeout(resolve, 100));
-        }
-    }
+    // async function waitForElement() {
+    //     while (true) {
+    //         const divBestprices = document.querySelectorAll('.bestprice');
+    //         if (divBestprices.length > 0) {
+    //             divBestprices.forEach((divBestprice) => {
+    //                 const newDivBestprice = document.createElement('div');
+    //                 newDivBestprice.className = 'bestprice__title';
+    //                 newDivBestprice.innerHTML = '<span>Impuestos incluidos</span>';
+    //                 divBestprice.appendChild(newDivBestprice);
+    //             });
+    //             break;
+    //         }
+    //         // Esperar 100ms y volver a intentar
+    //         await new Promise(resolve => setTimeout(resolve, 100));
+    //     }
+    // }
 
-    // Llamar a la función para esperar a que se cargue la clase .bestprice
-    waitForElement();
+    // // Llamar a la función para esperar a que se cargue la clase .bestprice
+    // waitForElement();
 });
 
 document.addEventListener('DOMContentLoaded', () => {
