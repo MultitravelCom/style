8cb9bf50d48771e4906d5398a37fbb2d9ace7bcd
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-21 15:08:13 -0300

    ocultar mail

--- Flight/main.js
+++ Flight/main.js
@@ -8,40 +8,28 @@ window.addEventListener('load', () => {
     document.getElementById("flight-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
     document.querySelector('.radio').style.display = 'inline-block';
 
-    // Cambiar texto al btn filtrar
+    // Cambiar texto al btn filtrar y agregar un icons.
 
     const textBtn = document.querySelector('.btn-tertiary');
     textBtn.textContent = 'Filtrar';
 
+    document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'inline-block';
+
     const breakpoint = window.matchMedia('(max-width: 992px)');
-    const startDate = document.querySelector('.date.start-date');
-    const filterToggle = document.querySelector('.results-list__filter-toggle');
-    
-    function toggleFilterToggleZIndex() {
-      filterToggle.style.zIndex = startDate.classList.contains('active') ? '-1' : '';
-    }
-    
-    function toggleFilterToggleVisibility() {
-      const display = breakpoint.matches ? 'inline-block' : 'none';
-      filterToggle.style.display = display;
-    }
-    
-    function observeStartDate() {
-      const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-          if (mutation.attributeName === 'class') {
-            toggleFilterToggleZIndex();
-          }
-        });
-      });
-      observer.observe(startDate, { attributes: true });
-    }
-    
-    toggleFilterToggleVisibility();
-    toggleFilterToggleZIndex();
-    observeStartDate();
-    window.addEventListener('resize', toggleFilterToggleVisibility);
+
+    function aplicarEstilos() {
+        if (breakpoint.matches) {
+            document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'inline-block';
+        } else {
+            document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'none';
+        }
+    };
+
+    aplicarEstilos();
+
+    window.addEventListener('resize', aplicarEstilos);
 
 });
+
 
 
