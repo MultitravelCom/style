5e71374aee2e83d126c0c82cf974d4d9161879fb
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-21 15:03:00 -0300

    ocultar mail

--- Flight/main.js
+++ Flight/main.js
@@ -8,45 +8,39 @@ window.addEventListener('load', () => {
     document.getElementById("flight-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
     document.querySelector('.radio').style.display = 'inline-block';
 
-    // Cambiar texto al btn filtrar y agregar un icons.
+    // Cambiar texto al btn filtrar
 
     const textBtn = document.querySelector('.btn-tertiary');
     textBtn.textContent = 'Filtrar';
 
-    document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'inline-block';
-
     const breakpoint = window.matchMedia('(max-width: 992px)');
-
-    function aplicarEstilos() {
-        if (breakpoint.matches) {
-            document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'inline-block';
-        } else {
-            document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'none';
-        }
-    };
-
-    aplicarEstilos();
-
-    window.addEventListener('resize', aplicarEstilos);
     const startDate = document.querySelector('.date.start-date');
     const filterToggle = document.querySelector('.results-list__filter-toggle');
     
-    // Crea una nueva instancia del observador de mutaciones
-    const observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        // Comprueba si se ha añadido o eliminado la clase 'active'
-        if (startDate.classList.contains('active')) {
-          // Añade el z-index
-          filterToggle.style.zIndex = '-1';
-        } else {
-          // Elimina el z-index
-          filterToggle.style.zIndex = '';
-        }
-      });
-    });
+    function toggleFilterToggleZIndex() {
+      filterToggle.style.zIndex = startDate.classList.contains('active') ? '-1' : '';
+    }
     
-    // Observa los cambios en el elemento con la clase 'date start-date'
-    observer.observe(startDate, { attributes: true });
+    function toggleFilterToggleVisibility() {
+      const display = breakpoint.matches ? 'inline-block' : 'none';
+      filterToggle.style.display = display;
+    }
+    
+    function observeStartDate() {
+      const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+          if (mutation.attributeName === 'class') {
+            toggleFilterToggleZIndex();
+          }
+        });
+      });
+      observer.observe(startDate, { attributes: true });
+    }
+    
+    toggleFilterToggleVisibility();
+    toggleFilterToggleZIndex();
+    observeStartDate();
+    window.addEventListener('resize', toggleFilterToggleVisibility);
 
 });
 
