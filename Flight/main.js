aad5e19c00717c6d8c6602646b09834ad0aa4d37
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-21 11:57:27 -0300

    ocultar mail

--- Flight/main.js
+++ Flight/main.js
@@ -13,7 +13,7 @@ window.addEventListener('load', () => {
     const textBtn = document.querySelector('.btn-tertiary');
     textBtn.textContent = 'Filtrar';
 
-    document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'inline-block';
+    // document.querySelector('.results-list__filter-toggle-wrapper').style.display = 'inline-block';
 
     const calendarContainer = document.querySelector('.js-calendar-container');
 const button = document.querySelector('.results-list__filter-toggle');
