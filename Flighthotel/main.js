2e990afada1f2c61ac83e656eb07296f94732198
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-25 18:42:21 -0300

    task mult-71

--- Flighthotel/main.js
+++ Flighthotel/main.js
@@ -1,10 +1,20 @@
-window.addEventListener('load', ()=>{
-    let displayMaster = document.querySelector('.master-selector');
-    let removeHidden = document.querySelectorAll(".master-selector-option");
+// window.addEventListener('load', ()=>{
+//     let displayMaster = document.querySelector('.master-selector');
+//     let removeHidden = document.querySelectorAll(".master-selector-option");
     
-    removeHidden.forEach(el =>{
-        el.classList.remove("hidden")
-    });
-        displayMaster.style.display = 'inline-block';
+//     removeHidden.forEach(el =>{
+//         el.classList.remove("hidden")
+//     });
+//         displayMaster.style.display = 'inline-block';
 
-});
+// });
+
+// /* MULTI-126 */
+
+window.addEventListener('load', () => {
+    let displayMaster = document.querySelector('.master-selector');
+    let firstOption = displayMaster.querySelector('.master-selector-option:first-child');
+    
+    firstOption.classList.remove('hidden');
+    displayMaster.style.display = 'inline-block';
+  });
