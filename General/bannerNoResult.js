46c9a937d62b8a8e55c4dc3a83e1851f6bc9b6a1
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-27 17:08:23 -0300

    mult-30

--- General/bannerNoResult.js
+++ General/bannerNoResult.js
@@ -5,18 +5,18 @@ function Button(props) {
     }
 
     return (
-        <button id={props.id} className="btn_Style_Venta_Per" onClick={handleClick}>{props.text}</button>
+        <button id={props.id} className="btn_Style_NoResult" onClick={handleClick}>{props.text}</button>
     );
 }
 
 const BannerNoResult = () => {
 return(
   <>
-    <div className ="container">
+    <div className ="container container__bannerNoResultr">
         <div className="bannerNoResultr_text">
         <h2>Lo sentimos</h2>
-            <p>En este momento no contamos con disponibilidad para esta búsqueda</p>
-            <p>Te invitamos a modificar la fecha o a comunicarte con nuestro equipo de atención personalizada</p>
+            <p className= "bannerNoResultr_text__p1">En este momento no contamos con disponibilidad para esta búsqueda</p>
+            <p className= "bannerNoResultr_text__p2">Te invitamos a modificar la fecha o a comunicarte con nuestro equipo de atención personalizada</p>
             <Button link="https://wa.link/j47nea" text="Contactarme" />
         </div>
         <div className="bannerNoResultr_img">
