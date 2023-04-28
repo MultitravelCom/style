3a20e552097166a0de17f0c11cd39fd40f40856f
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-28 12:59:21 -0300

    mult-30

--- Home/main.js
+++ Home/main.js
@@ -7,7 +7,18 @@ window.addEventListener('load', () => {
     divDestination.appendChild(divToMove);
 
     //    Cambio de copy de Multiple vuelos a multiple destinos.
-    document.getElementById("flights-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
+    // document.getElementById("flights-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
+    
+
+    // seleccionar el elemento div por su clase
+    let multiFlightDiv = $('.flights-searcher-roundtrip-multi');
+
+    // seleccionar el label que contiene el texto que deseas cambiar
+    let label = multiFlightDiv.find('label[for="flight-searcher-roundtrip-multi"]');
+
+    // cambiar el texto del label
+    label.text('Multidestino');
+
     document.querySelector('.radio').style.display = 'inline-block';
 
 });
