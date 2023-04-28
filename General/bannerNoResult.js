9d41f35dd6d9c9d24c91db8e3601585c748527d5
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-04-27 21:42:05 -0300

    mult-30

--- General/bannerNoResult.js
+++ General/bannerNoResult.js
@@ -12,12 +12,12 @@ function Button(props) {
 const BannerNoResult = () => {
 return(
   <>
-    <div className ="container container__bannerNoResultr">
+    <div className ="container-fluid container__bannerNoResultr">
         <div className="bannerNoResultr_text">
         <h2>Lo sentimos</h2>
             <p className= "bannerNoResultr_text__p1">En este momento no contamos con disponibilidad para esta búsqueda</p>
             <p className= "bannerNoResultr_text__p2">Te invitamos a modificar la fecha o a comunicarte con nuestro equipo de atención personalizada</p>
-            <Button className="btn btn_Style_NoResult" link="https://wa.link/j47nea" text="Contactarme" />
+            <Button style="btn btn_Style_NoResult" link="https://wa.link/j47nea" text="Contactarme" />
         </div>
         <div className="bannerNoResultr_img">
             <img alt="icono signo de interrogación" src="https://multitravelcom.github.io/style/General/img/iconresultado.png" />
