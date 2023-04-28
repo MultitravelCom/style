6f8ede5c5c6889cde4bc17eefa6379f7d18dfe70
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-28 14:06:22 -0300

    mult-30

--- Home/main.js
+++ Home/main.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
 
     // Mover div fuera del mas opciones.
-    let divToMove = document.getElementsByClassName("checkbox")[0];
-    let divDestination = document.getElementsByClassName("singledestination-only")[0];
+    // let divToMove = document.getElementsByClassName("checkbox")[0];
+    // let divDestination = document.getElementsByClassName("singledestination-only")[0];
 
-    divDestination.appendChild(divToMove);
+    // divDestination.appendChild(divToMove);
 
     //    Cambio de copy de Multiple vuelos a multiple destinos.
     // document.getElementById("flights-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
