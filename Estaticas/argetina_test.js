d9670ad26893ead2b9ce81b259ced115fb33fe59
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-28 11:32:39 -0300

    mult-30

--- Estaticas/argetina_test.js
+++ Estaticas/argetina_test.js
@@ -1,4 +1,4 @@
-function addGliderStylesheet() {
+function addHeaderLinks() {
     const head = document.head || document.getElementsByTagName('head')[0];
     const meta = document.createElement('meta');
 
@@ -12,10 +12,18 @@ function addGliderStylesheet() {
     link.rel = 'stylesheet';
     link.href = 'https://cdn.jsdelivr.net/npm/glider-js@1.7.8/glider.min.css';
 
+    const mailchimp = document.createElement('script');
+    mailchimp.id = 'mcjs';
+    mailchimp.async = true;
+    mailchimp.src = 'https://chimpstatic.com/mcjs-connected/js/users/d09ee86703b1761e8337397e9/6e305f08d149ab3c55d2d9573.js';
+    mailchimp.textContent = '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/d09ee86703b1761e8337397e9/6e305f08d149ab3c55d2d9573.js");';
+
+
     head.appendChild(meta);
     head.appendChild(link);
+    head.appendChild(mailchimp);
 }
-addGliderStylesheet();
+addHeaderLinks();
 
 const linkWaHeader = document.querySelector('.btn-group.upper-menu__phone-wrapper a');
 const linkWaFixed = document.querySelector('.whatsAppFixes a');
@@ -37,7 +45,7 @@ function mostrarSeccion() {
         setTimeout(mostrarSeccion, 500); // Intentar nuevamente después de 500 milisegundos
     }
 }
-mostrarSeccion(); 
+mostrarSeccion();
 // ************************************************
 // ***************************  Conexion a BD ***************************************
 const fetchDestinos = async () => {
@@ -130,107 +138,107 @@ const Card = () => {
 };
 const CardContainer = ({ btnStyles, destinos }) => {
     const { title, btnRight, btnLeft, carrusel, destino } = btnStyles;
-  
+
     const setupGlider = () => {
-      const btnLeftElement = document.querySelector(`.${btnLeft}`);
-      const btnRightElement = document.querySelector(`.${btnRight}`);
-  
-      btnLeftElement.addEventListener('click', function (event) {
-        event.preventDefault();
-      });
-  
-      btnRightElement.addEventListener('click', function (event) {
-        event.preventDefault();
-      });
-  
-      new Glider(document.querySelector(`.${carrusel}`), {
-        slidesToShow: 1.2,
-        slidesToScroll: 0.5,
-        draggable: true,
-        arrows: {
-          prev: btnLeftElement,
-          next: btnRightElement,
-        },
-        responsive: [
-          {
-            // screens greater than >= 775px
-            breakpoint: 450,
-            settings: {
-              // Set to `auto` and provide item width to adjust to viewport
-              slidesToShow: "2.2",
-              slidesToScroll: "1",
-            },
-          },
-          {
-            // screens greater than >= 775px
-            breakpoint: 760,
-            settings: {
-              // Set to `auto` and provide item width to adjust to viewport
-              slidesToShow: "3.2",
-              slidesToScroll: "1",
-            },
-          },
-          {
-            // screens greater than >= 1024px
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 1,
-            },
-          },
-        ],
-        rewind: true,
-      });
-    };
-  
-    React.useEffect(() => {
-      const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-            setupGlider();
-            observer.disconnect();
-          }
+        const btnLeftElement = document.querySelector(`.${btnLeft}`);
+        const btnRightElement = document.querySelector(`.${btnRight}`);
+
+        btnLeftElement.addEventListener('click', function (event) {
+            event.preventDefault();
         });
-      });
-  
-      observer.observe(document.querySelector(`.${carrusel}`), {
-        childList: true,
-      });
-  
-      return () => {
-        observer.disconnect();
-      };
+
+        btnRightElement.addEventListener('click', function (event) {
+            event.preventDefault();
+        });
+
+        new Glider(document.querySelector(`.${carrusel}`), {
+            slidesToShow: 1.2,
+            slidesToScroll: 0.5,
+            draggable: true,
+            arrows: {
+                prev: btnLeftElement,
+                next: btnRightElement,
+            },
+            responsive: [
+                {
+                    // screens greater than >= 775px
+                    breakpoint: 450,
+                    settings: {
+                        // Set to `auto` and provide item width to adjust to viewport
+                        slidesToShow: "2.2",
+                        slidesToScroll: "1",
+                    },
+                },
+                {
+                    // screens greater than >= 775px
+                    breakpoint: 760,
+                    settings: {
+                        // Set to `auto` and provide item width to adjust to viewport
+                        slidesToShow: "3.2",
+                        slidesToScroll: "1",
+                    },
+                },
+                {
+                    // screens greater than >= 1024px
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+            rewind: true,
+        });
+    };
+
+    React.useEffect(() => {
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+                    setupGlider();
+                    observer.disconnect();
+                }
+            });
+        });
+
+        observer.observe(document.querySelector(`.${carrusel}`), {
+            childList: true,
+        });
+
+        return () => {
+            observer.disconnect();
+        };
     }, [btnLeft, btnRight, carrusel]);
-  
+
     return (
-      <>
-        <div key={title} className="main__conteiner__s1">
-          <div className="main__conteiner__s1__titulo" id={`seccion${destino}`}>
-            <h2 key={title}>
-              <strong>{title}</strong>
-            </h2>
-          </div>
-          <div className="carrusel__contenedor">
-            <button
-              aria-label="Anterior"
-              className={`carrusel__anterior ${btnLeft}`}
-            >
-              <i className="fa fa-chevron-left" aria-hidden="true"></i>
-            </button>
-            <div className={carrusel} id={destinos.title}>
-              <Card destinos={destinos} />
+        <>
+            <div key={title} className="main__conteiner__s1">
+                <div className="main__conteiner__s1__titulo" id={`seccion${destino}`}>
+                    <h2 key={title}>
+                        <strong>{title}</strong>
+                    </h2>
+                </div>
+                <div className="carrusel__contenedor">
+                    <button
+                        aria-label="Anterior"
+                        className={`carrusel__anterior ${btnLeft}`}
+                    >
+                        <i className="fa fa-chevron-left" aria-hidden="true"></i>
+                    </button>
+                    <div className={carrusel} id={destinos.title}>
+                        <Card destinos={destinos} />
+                    </div>
+                    <button
+                        aria-label="Siguiente"
+                        className={`carrusel__siguiente ${btnRight}`}
+                    >
+                        <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                    </button>
+                </div>
             </div>
-            <button
-              aria-label="Siguiente"
-              className={`carrusel__siguiente ${btnRight}`}
-            >
-              <i className="fa fa-chevron-right" aria-hidden="true"></i>
-            </button>
-          </div>
-        </div>
-      </>
+        </>
     );
-  };
+};
 
 const Loader = () => {
     return (
