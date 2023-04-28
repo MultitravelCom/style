35ec1ba6d92bec16835746943ddd6c64d6cabf40
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-28 18:25:57 -0300

    mult-30

--- General/addHeaderLinks.js
+++ General/addHeaderLinks.js
@@ -1,6 +1,6 @@
 function addHeaderLinks() {
     const head = document.head || document.getElementsByTagName('head')[0];
-  
+
     const bannerNoResult = document.createElement('script');
 
     bannerNoResult.src = 'https://multitravelcom.github.io/style/General/bannerNoResult.js';
@@ -9,5 +9,5 @@ function addHeaderLinks() {
 
     head.appendChild(bannerNoResult);
 
-  }
-  addHeaderLinks();
+}
+addHeaderLinks();
