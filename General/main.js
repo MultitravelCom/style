9eff21a42cd3a762200d68e01593c26c5c9a0a22
Multitravel.com <ignacio.peralta@multitravel.com>
2023-04-28 17:54:04 -0300

    mult-30

--- General/main.js
+++ General/main.js
@@ -1,3 +1,15 @@
+function addHeaderLinks() {
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const meta = document.createElement('meta');
+
+    const bannerNoResult = document.createElement('script');
+    bannerNoResult.src = 'https://multitravelcom.github.io/style/General/bannerNoResult.js';
+    bannerNoResult.textContent = "text/babel";
+
+    head.appendChild(bannerNoResult);
+}
+addHeaderLinks();
+
 document.addEventListener('DOMContentLoaded', () => {
     async function waitForElementFlight() {
         while (true) {
