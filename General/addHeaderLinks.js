d00ff9eb3e533f839dd9cfbdc1d43b09b1d35ee2
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-01 11:44:51 -0300

    mult-30

--- General/addHeaderLinks.js
+++ General/addHeaderLinks.js
@@ -1,12 +1,16 @@
 function addHeaderLinks() {
     const head = document.head || document.getElementsByTagName('head')[0];
 
+    const bannerNoResultStyle = document.createElement('link');
     const bannerNoResult = document.createElement('script');
 
     bannerNoResult.src = 'https://multitravelcom.github.io/style/General/bannerNoResult.js';
     bannerNoResult.type = "text/babel";
 
+    bannerNoResultStyle.href ="https://multitravelcom.github.io/style/General/noResultStyle.css";
+    bannerNoResultStyle.rel = "stylesheet";
 
+    head.appendChild(bannerNoResultStyle);
     head.appendChild(bannerNoResult);
 
 }
