07e6bccece7c593acac36674aa4d73bf750de9fc
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-03 14:32:44 -0300

    mult-199

--- General/components/BannerTop.js
+++ General/components/BannerTop.js
@@ -1,13 +1,16 @@
-const BannerTop = () =>{
-    return(
-        <div className="container-fluid main__container__bannerTop scroll-mobile">
-            <div className="main__container_left">
+const BannerTop = () => {
+    return (
+        <div className="container-fluid main__container__bannerTop">
+            <div className="scroll-mobile">
+                <div id="bannerTop__left" className="main__container_left">
 
-            </div>
-            <div className="main__container_center">
+                </div>
+                <div id="bannerTop__center" className="main__container_center">
 
-            </div>
-            <div className="main__container_right">
+                </div>
+                <div id="bannerTop__rigth" className="main__container_right">
+
+                </div>
 
             </div>
         </div>
