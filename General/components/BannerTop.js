a4e9f7b9189e7ebc54d161d500e031d713512665
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-03 12:17:18 -0300

    mult-199

---
+++ General/components/BannerTop.js
@@ -0,0 +1,17 @@
+const BannerTop = () =>{
+    return(
+        <div className="container-fluid">
+            <div className="main__container_left">
+
+            </div>
+            <div className="main__container_center">
+
+            </div>
+            <div className="main__container_right">
+
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('home-content-5')).render(<BannerTop />);
