5f5c9acd403c8a14c3d70be26fbf607ab7449bc9
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-04 19:37:59 -0300

    mul-127

--- Home/main.js
+++ Home/main.js
@@ -14,6 +14,16 @@ window.addEventListener('load', () => {
 
     // Mostrar el botón una vez que se cargue el DOM
     btn.style.display = 'inline-block';
+
+
+    // Mover div fuera del mas opciones.
+    let divToMove = document.getElementsByClassName("checkbox")[0];
+    let divDestination = document.getElementsByClassName("singledestination-only")[0];
+
+    divDestination.appendChild(divToMove);
+    //    Cambio de copy de Multiple vuelos a multiple destinos.
+    document.getElementById("flights-searcher-roundtrip-multi").nextSibling.nodeValue = "Multidestino";
+    document.querySelector('.radio').style.display = 'inline-block';
     // ***************************** **********************
 
     // seleccionar el elemento div por su clase
