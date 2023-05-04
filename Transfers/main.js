8cf3cfaf4fe42945c175aded6916c46f4f98d61a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-04 17:53:35 -0300

    lading brasil

--- Transfers/main.js
+++ Transfers/main.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-// Acomoda #bannerCentral al top y cambia la clase de none a inline.
-let moverA = document.querySelector('#home-content-1');
-let moverEn = document.querySelector('#banner__central')
-let primerHijo = moverA.firstChild;
+// document.addEventListener('DOMContentLoaded', ()=>{
+// // Acomoda #bannerCentral al top y cambia la clase de none a inline.
+// let moverA = document.querySelector('#home-content-1');
+// let moverEn = document.querySelector('#banner__central')
+// let primerHijo = moverA.firstChild;
 
-moverA.insertBefore(moverEn, primerHijo);
-moverEn.style.display = 'inline-block';
-});
+// moverA.insertBefore(moverEn, primerHijo);
+// moverEn.style.display = 'inline-block';
+// });
