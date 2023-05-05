b9928ea205031e6a3731e7f397b4f381e0ce2f0f
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-05 11:30:41 -0300

    mult-199

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function(event) {
 
     const padre = document.querySelector('.booking-data__data');
     const nuevoDiv = document.createElement('div');
