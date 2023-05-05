011f9951317f92a929228f3be09645c1f0e8a0fe
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-05 11:02:17 -0300

    mult-199

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -1,4 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const padre = document.querySelector('.booking-data__data');
+    const nuevoDiv = document.createElement('div');
 
+    nuevoDiv.classList.add('booking-data__data__text');
+    nuevoDiv.textContent =
+        `<div class="booking-data__data__text__h2">
+    <h2>Aprovecha nuestro cupón 10% OFF en vuelos y paquetes</h2>
+</div>
+<div class="booking-data__data__text__p">
+    <p>En paquetes, el descuento se aplica solo al aéreo.
+    Tope de descuento $25.000. Cantidad limitada.</p>
+</div>
+<div class="booking-data__data__text__span">
+    <span>Código</span>
+    <p>MULTITRAVELHOTSALE10</p>
+</div>
+`;
 
-
-
+    padre.appendChild(nuevoDiv);
+});
