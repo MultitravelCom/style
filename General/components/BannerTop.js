455e92ae40e6169bb7bc0bd7f553e09a5a39a2c0
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-05 11:57:10 -0300

    mult-199

--- General/components/BannerTop.js
+++ General/components/BannerTop.js
@@ -14,13 +14,64 @@ const BannerTop = () => {
     return (
         <div className="container-fluid main__container__bannerTop scroll-mobile">
             <div id="bannerTop__left" className="main__container_left">
-
+                <picture>
+                    <source
+                        media="(min-width: 1024px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-1.webp"
+                    />
+                    <source
+                        media="(min-width: 768px) and (max-width: 1023px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-1.webp"
+                    />
+                    <source
+                        media="(max-width: 767px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerMobile-1.webp"
+                    />
+                    <img
+                        alt=""
+                        src="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-1.webp"
+                    />
+                </picture>
             </div>
             <div id="bannerTop__center" className="main__container_center">
-
+            <picture>
+                    <source
+                        media="(min-width: 1024px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-2.webp"
+                    />
+                    <source
+                        media="(min-width: 768px) and (max-width: 1023px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-2.webp"
+                    />
+                    <source
+                        media="(max-width: 767px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerMobile-2.webp"
+                    />
+                    <img
+                        alt=""
+                        src="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-2.webp"
+                    />
+                </picture>
             </div>
             <div id="bannerTop__rigth" className="main__container_right">
-
+            <picture>
+                    <source
+                        media="(min-width: 1024px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-3.webp"
+                    />
+                    <source
+                        media="(min-width: 768px) and (max-width: 1023px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-3.webp"
+                    />
+                    <source
+                        media="(max-width: 767px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerMobile-3.webp"
+                    />
+                    <img
+                        alt=""
+                        src="https://multitravelcom.github.io/MT/Secciones/BannerPrincipal/bannerDesktop-3.webp"
+                    />
+                </picture>
             </div>
         </div>
     )
