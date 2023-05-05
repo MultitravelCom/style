fef56e309a8794020cbc2f19b0f40f41eca36734
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-05 18:31:17 -0300

    mult-199

--- Home/main.js
+++ Home/main.js
@@ -11,16 +11,16 @@ window.addEventListener('load', () => {
     if (!btnMoreOptions.classList.contains('collapsed')) {
         btnMoreOptions.setAttribute('data-toggle-text', 'Cabina y equipaje');
     }
-    // Obtener el label por su etiqueta "for"
-    const labelFlights = document.querySelector('label[for="flights-searcher-flightclass"]');
+    // // Obtener el label por su etiqueta "for"
+    // const labelFlights = document.querySelector('label[for="flights-searcher-flightclass"]');
 
-    // Reemplazar el texto "Clase" por "Cabina"
-    labelFlights.textContent = labelFlights.textContent.replace('Clase', 'Cabina');
-    // Obtener el elemento span que contiene el texto "clase"
-    const labelFlightsClassSpan = document.querySelector('#select2-flights-searcher-flightclass-container');
+    // // Reemplazar el texto "Clase" por "Cabina"
+    // labelFlights.textContent = labelFlights.textContent.replace('Clase', 'Cabina');
+    // // Obtener el elemento span que contiene el texto "clase"
+    // const labelFlightsClassSpan = document.querySelector('#select2-flights-searcher-flightclass-container');
 
-    // Reemplazar el texto "clase" por "cabina"
-    labelFlightsClassSpan.textContent = labelFlightsClassSpan.textContent.replace('clase', 'cabina');
+    // // // Reemplazar el texto "clase" por "cabina"
+    // // labelFlightsClassSpan.textContent = labelFlightsClassSpan.textContent.replace('clase', 'cabina');
 
     // Mostrar el botón una vez que se cargue el DOM
     btnMoreOptions.style.display = 'inline-block';
