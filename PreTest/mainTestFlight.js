7dca6210c9fc0fa2aa83091e7a9267f83cb494a7
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-06 11:26:17 -0300

    mult-209

--- PreTest/mainTestFlight.js
+++ PreTest/mainTestFlight.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
     const referenceNode = document.querySelector('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
 
     const newElement = document.createElement('div');
-    newElement.classList.add('booking-breakdown__table main__warningPrice');
+    newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
     newElement.innerHTML = `
             <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
             <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
