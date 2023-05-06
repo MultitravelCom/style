fa36c53e33a2864169b03d783dadd557af71f3e9
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-06 10:31:07 -0300

    actulizacion precios bdBrasil

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -266,7 +266,7 @@ const Card = ({ destinos }) => {
         destinos.map((destino) => (
             <div key={destino.id} className="carrusel__elemento">
                 <div className="main__conteiner__s1__destacado__card uno" style={{ height: "100%", width: "100%" }}>
-                    {destino.events === "si" && <EventImg style="eventImg" />}
+                    {/* {destino.events === "si" && <EventImg style="eventImg" />} */}
                     <picture>
                         <map name={destino.id}>
                             <area target="_blank" alt={destino.title} title={destino.title} href={destino.linkWa} coords={destino.coords} shape="rect" />
