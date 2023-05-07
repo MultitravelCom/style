9d75fb1573e57fb595d609984482cab9729e9f6a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-06 21:52:54 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRg.js
+++ General/components/CopyTaxMult209/copyTaxRg.js
@@ -1,14 +1,18 @@
 window.addEventListener('load', () => {
     //*************************************** MULT-209 *************************** */
-    const referenceNode = document.querySelector('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
+    function agregarElemento(referenceSelector) {
+        const referenceNode = document.querySelector(referenceSelector);
+      
+        const newElement = document.createElement('div');
+        newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
+        newElement.innerHTML = `
+          <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+          <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
+        `;
+      
+        referenceNode.insertAdjacentElement('afterend', newElement);
+      }
 
-    const newElement = document.createElement('div');
-    newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
-    newElement.innerHTML = `
-            <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-            <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
-`;
-
-    referenceNode.insertAdjacentElement('afterend', newElement);
+      agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
     //****************************************************************** */
 });
