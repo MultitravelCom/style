ab9814c0fb62496e4b396b71987a0558c94e95a8
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-07 11:27:11 -0300

    mult-209

--- General/addHeaderLinks.js
+++ General/addHeaderLinks.js
@@ -16,7 +16,7 @@ function addHeaderLinks() {
 
     const path = window.location.pathname;
     const copyTaxRg = path.includes('/flights');
-    const priceFinalFlight = path.includes('onefiveb2b.juniper.es/flights/results.aspx');
+    const priceFinalFlight = path.includes('onefiveb2b.juniper.es/flights/results');
 
     if(copyTaxRg){
         const copyTaxRegStyle = document.createElement('link');
