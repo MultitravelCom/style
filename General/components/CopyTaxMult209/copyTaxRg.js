f93f05cc451b7da5f67b5d27df649e5cf5f408a2
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-07 10:44:38 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRg.js
+++ General/components/CopyTaxMult209/copyTaxRg.js
@@ -1,6 +1,19 @@
 window.addEventListener('load', () => {
     //*************************************** MULT-209 *************************** */
-    function agregarElemento(referenceSelector) {
+    // function agregarElemento(referenceSelector) {
+    //     const referenceNode = document.querySelector(referenceSelector);
+      
+    //     const newElement = document.createElement('div');
+    //     newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
+    //     newElement.innerHTML = `
+    //       <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+    //       <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
+    //     `;
+      
+    //     referenceNode.insertAdjacentElement('afterend', newElement);
+    //   }
+
+    function agregarElemento(referenceSelector, index = -1) {
         const referenceNode = document.querySelector(referenceSelector);
       
         const newElement = document.createElement('div');
@@ -10,10 +23,16 @@ window.addEventListener('load', () => {
           <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
         `;
       
-        referenceNode.insertAdjacentElement('afterend', newElement);
+        if (index === -1 || index >= referenceNode.children.length) {
+          referenceNode.appendChild(newElement);
+        } else {
+          referenceNode.insertBefore(newElement, referenceNode.children[index]);
+        }
       }
+      
 
       agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
-      agregarElemento('.flight-selection__box > :nth-child(3) ~ :nth-child(4)');
+      
+      agregarElemento('.flight-selection__box > :nth-child(3) + :nth-child(1)');
       //****************************************************************** */
 });
