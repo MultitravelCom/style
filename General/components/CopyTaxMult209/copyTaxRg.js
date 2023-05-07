abb04af3ad854514e15645ece6ee11c2f8e2e0cc
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-07 14:49:23 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRg.js
+++ General/components/CopyTaxMult209/copyTaxRg.js
@@ -38,7 +38,7 @@ window.addEventListener('load', () => {
     }
 
     onElementAddedToDOM('.js-results-list-selection-placeholder', () => {
-        agregarElemento('.js-results-list-selection-placeholder > .flight-selection > .flight-selection__box div:nth-child(3)');
+        agregarElemento('.js-results-list-selection-placeholder > .flight-selection');
       });
 
     agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
