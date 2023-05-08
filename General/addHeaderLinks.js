6fb428e76396d7ceae37f03ec5c500b59d73e18b
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-08 12:12:00 -0300

    mult-199

--- General/addHeaderLinks.js
+++ General/addHeaderLinks.js
@@ -18,6 +18,7 @@ function addHeaderLinks() {
     const copyTaxRg = path.includes('/flights') || path.includes('/booking/');
     const priceFinalFlight = path.includes('/results');
     const bannerTop = path.includes('/results') || path.includes('/booking/');
+    const cuponSeccion = path.includes('/booking/');
 
     if(copyTaxRg){
         const copyTaxRegStyle = document.createElement('link');
@@ -55,20 +56,20 @@ function addHeaderLinks() {
         head.appendChild(bannerTopScript);
     }
 
+    // 
+    if(cuponSeccion){
+        const cuponSeccionScript = document.createElement('script');
+        const cuponSeccionStyle = document.createElement('link');
+
+        cuponSeccionScript.src = 'https://multitravelcom.github.io/style/General/components/CuponSeccion/cuponSeccion.js';
+
     
-    // if(cuponSeccion){
-    //     const cuponSeccionScript = document.createElement('script');
-    //     const cuponSeccionStyle = document.createElement('link');
+        cuponSeccionStyle.href = "https://multitravelcom.github.io/style/General/components/CuponSeccion/cuponSeccion.css";
+        cuponSeccionStyle.rel = "stylesheet";
 
-    //     cuponSeccionScript.src = 'https://multitravelcom.github.io/style/General/components/CuponSeccion/cuponSeccion.js';
-
-    
-    //     cuponSeccionStyle.href = "https://multitravelcom.github.io/style/General/components/CuponSeccion/cuponSeccion.css";
-    //     cuponSeccionStyle.rel = "stylesheet";
-
-    //     head.appendChild(cuponSeccionStyle);
-    //     head.appendChild(cuponSeccionScript);
-    // }
+        head.appendChild(cuponSeccionStyle);
+        head.appendChild(cuponSeccionScript);
+    }
 
 }
 addHeaderLinks();
