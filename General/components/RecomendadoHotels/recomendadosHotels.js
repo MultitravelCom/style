7387330089537e3cdd8f9be335a4677679a07f13
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 14:16:37 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -1,45 +1,44 @@
 // ***************************** MULT-209 *********************************************
-function addTaxCopyToPriceResults(priceResults) {
-    if (priceResults) {
-      let newDivTaxCopy = document.createElement('div');
-      newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-      newDivTaxCopy.classList.add("DivTaxCopyStyle");
-      priceResults.appendChild(newDivTaxCopy);
-    }
-  }
-  
-  function observeResultsList() {
-    const resultsListHotels = document.querySelector('.js-results-list-placeholder');
+function observeResultsList() {
+    const resultsListHotels = document.querySelector('.results-list__item');
     if (!resultsListHotels) {
-      return; // si el selector no existe, no hacemos nada
+      console.log('No se encontró el selector ".results-list__item"');
+      return;
     }
   
     const itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
+    console.log(`Se encontraron ${itemsResults.length} elementos con clase "results-list__item"`);
+  
     if (itemsResults.length > 0) {
       itemsResults.forEach(function(item) {
         const priceResults = item.querySelector('.info-card__price');
+        console.log('Agregando copia de impuestos a', priceResults);
         addTaxCopyToPriceResults(priceResults);
       });
-    }
-  
-    const itemsResultsObserver = new MutationObserver(function(mutationsList, observer) {
-        console.log('Mutation detected');
-      for (const mutation of mutationsList) {
-        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-          const addedNodes = mutation.addedNodes;
-          for (const node of addedNodes) {
-            if (node.classList && node.classList.contains('results-list__item')) {
-              const priceResults = node.querySelector('.info-card__price');
-              addTaxCopyToPriceResults(priceResults);
+    } else {
+      const itemsResultsObserver = new MutationObserver(function(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+          if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            const addedNodes = mutation.addedNodes;
+            console.log(`Se detectaron ${addedNodes.length} nodos añadidos`);
+            for (const node of addedNodes) {
+              if (node.classList && node.classList.contains('results-list__item')) {
+                console.log('Se agregó un elemento con clase "results-list__item"');
+                const priceResults = node.querySelector('.info-card__price');
+                console.log('Agregando copia de impuestos a', priceResults);
+                addTaxCopyToPriceResults(priceResults);
+              }
             }
           }
         }
-      }
-    });
-    itemsResultsObserver.observe(resultsListHotels, { childList: true, subtree: true });
+      });
+      itemsResultsObserver.observe(resultsListHotels, { childList: true, subtree: true });
+      console.log('Observando cambios en "results-list__item"');
+    }
   }
   
   document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOMContentLoaded!');
     observeResultsList();
   });
 // **************************************************************************
