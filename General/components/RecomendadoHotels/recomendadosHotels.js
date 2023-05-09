23197beb3135f57626b527cad718e7faf770ab4f
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 13:14:31 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -1,28 +1,30 @@
-async function observeMutation(elementToObserve, callback) {
-    while (!document.querySelector(elementToObserve)) {
-        // Esperar hasta que el elemento esté disponible en el DOM
-        await new Promise(resolve => requestAnimationFrame(resolve));
+window.onload = function () {
+
+    async function observeMutation(elementToObserve, callback) {
+        while (!document.querySelector(elementToObserve)) {
+            // Esperar hasta que el elemento esté disponible en el DOM
+            await new Promise(resolve => requestAnimationFrame(resolve));
+        }
+
+        const observer = new MutationObserver(callback);
+        observer.observe(document.querySelector(elementToObserve), { childList: true, subtree: true });
+        return observer;
     }
 
-    const observer = new MutationObserver(callback);
-    observer.observe(document.querySelector(elementToObserve), { childList: true, subtree: true });
-    return observer;
-}
+    // ***************************** MULT-209 *********************************************
+    let resultsListHotels = document.querySelector('.js-results-list-placeholder');
 
-// ***************************** MULT-209 *********************************************
-let resultsListHotels = document.querySelector('.js-results-list-placeholder');
-const itemsResults = document.querySelectorAll('.results-list__item');
+    observeMutation('.js-results-list-placeholder', function (mutationsList, observer) {
 
-observeMutation('.js-results-list-placeholder', function (mutationsList, observer) {
-   
-    itemsResults.forEach(function (item) {
-        const priceResults = item.querySelector('.info-card__price');
-        const newDivTaxCopy = document.createElement('div');
-        newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-        newDivTaxCopy.classList.add("DivTaxCopyStyle");
+        const itemsResults = document.querySelectorAll('.results-list__item');
+        itemsResults.forEach(function (item) {
+            const priceResults = item.querySelector('.info-card__price');
+            const newDivTaxCopy = document.createElement('div');
+            newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
+            newDivTaxCopy.classList.add("DivTaxCopyStyle");
 
-        priceResults.appendChild(newDivTaxCopy);
+            priceResults.appendChild(newDivTaxCopy);
+        });
     });
-});
-
+}
 // **************************************************************************
