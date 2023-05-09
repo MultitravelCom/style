4ed292b423c84e67e2870a0f5e6ac5778c87de55
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 13:47:20 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -1,33 +1,35 @@
 // ***************************** MULT-209 *********************************************
+document.addEventListener('DOMContentLoaded', function () {
+    function addTaxCopyToResultsList() {
+        let resultsListHotels = document.querySelector('.js-results-list-placeholder');
 
-function addTaxCopyToResultsList() {
-    let resultsListHotels = document.querySelector('.js-results-list-placeholder');
-  
-    if (resultsListHotels) {
-      let itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
-  
-      itemsResults.forEach(function(item) {
-        let priceResults = item.querySelector('.info-card__price');
-        let newDivTaxCopy = document.createElement('div');
-        newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-        newDivTaxCopy.classList.add("DivTaxCopyStyle");
-  
-        priceResults.appendChild(newDivTaxCopy);
-      });
+        if (resultsListHotels) {
+            let itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
+
+            itemsResults.forEach(function (item) {
+                let priceResults = item.querySelector('.info-card__price');
+                let newDivTaxCopy = document.createElement('div');
+                newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
+                newDivTaxCopy.classList.add("DivTaxCopyStyle");
+
+                priceResults.appendChild(newDivTaxCopy);
+            });
+        }
     }
-  }
-  
-  let resultsListObserver = new MutationObserver(function(mutationsList) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === 'childList' && mutation.addedNodes.length) {
-        mutation.addedNodes.forEach(function(node) {
-          if (node.classList && node.classList.contains('js-results-list-placeholder')) {
-            addTaxCopyToResultsList();
-          }
-        });
-      }
-    }
-  });
-  
-  resultsListObserver.observe(document.body, { childList: true, subtree: true });
+
+    let resultsListObserver = new MutationObserver(function (mutationsList) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                mutation.addedNodes.forEach(function (node) {
+                    if (node.classList && node.classList.contains('js-results-list-placeholder')) {
+                        addTaxCopyToResultsList();
+                    }
+                });
+            }
+        }
+    });
+
+    resultsListObserver.observe(document.body, { childList: true, subtree: true });
+
+});
 // **************************************************************************
