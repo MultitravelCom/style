1cf7a6a3be4e420a8e5839086d530f34c077c643
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 12:57:28 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -7,17 +7,19 @@ function observeMutation(elementToObserve, callback) {
 
 // ***************************** MULT-209 *********************************************
 let resultsListHotels = document.querySelector('.js-results-list-placeholder');
-let itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
 
-observeMutation(resultsListHotels, function(mutationsList, observer) {
-  itemsResults.forEach(function(item) {
-    let priceResults = item.querySelector('.info-card__price');
-    let newDivTaxCopy = document.createElement('div');
-    newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-    newDivTaxCopy.classList.add("DivTaxCopyStyle");
+observeMutation(resultsListHotels, function (mutationsList, observer) {
 
-    priceResults.appendChild(newDivTaxCopy);
-  });
+    let itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
+
+    itemsResults.forEach(function (item) {
+        let priceResults = item.querySelector('.info-card__price');
+        let newDivTaxCopy = document.createElement('div');
+        newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
+        newDivTaxCopy.classList.add("DivTaxCopyStyle");
+
+        priceResults.appendChild(newDivTaxCopy);
+    });
 });
 
 // **************************************************************************
