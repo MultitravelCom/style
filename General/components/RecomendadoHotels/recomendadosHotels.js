c88fd3acaf67ce80953ec4ef2294e566eed2aa63
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 15:22:28 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -1,26 +1,12 @@
 // ***************************** MULT-209 *********************************************
 
-async function addTaxCopyToPriceResults() {
-    let resultsListHotels = null;
-  
-    // Espera a que se cargue el selector donde debe ir el nuevo div
-    while (!resultsListHotels) {
-      await new Promise(resolve => setTimeout(resolve, 100));
-      resultsListHotels = document.querySelector('.js-results-list-placeholder');
-    }
-  
-    // Agrega el nuevo div a cada elemento con la clase results-list__item
-    const itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
-    itemsResults.forEach(item => {
-      const priceResults = item.querySelector('.info-card__price');
-      const newDivTaxCopy = document.createElement('div');
-      newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-      newDivTaxCopy.classList.add("DivTaxCopyStyle");
-      priceResults.appendChild(newDivTaxCopy);
-    });
+function addTaxCopyToPriceResults(priceResults) {
+    let newDivTaxCopy = document.createElement('div');
+    newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
+    newDivTaxCopy.classList.add("DivTaxCopyStyle");
+    priceResults.appendChild(newDivTaxCopy);
   }
   
-  // Llama a la función cada vez que se agregue un nuevo elemento con la clase results-list__item
   function observeResultsList() {
     const resultsListHotels = document.querySelector('.js-results-list-placeholder');
     if (!resultsListHotels) {
@@ -28,35 +14,31 @@ async function addTaxCopyToPriceResults() {
     }
   
     const itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
-    itemsResults.forEach(item => {
-      const priceResults = item.querySelector('.info-card__price');
-      const newDivTaxCopy = document.createElement('div');
-      newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-      newDivTaxCopy.classList.add("DivTaxCopyStyle");
-      priceResults.appendChild(newDivTaxCopy);
-    });
+    if (itemsResults.length > 0) {
+      itemsResults.forEach(function(item) {
+        const priceResults = item.querySelector('.info-card__price');
+        addTaxCopyToPriceResults(priceResults);
+      });
+    }
   
-    const itemsResultsObserver = new MutationObserver((mutationsList, observer) => {
+    const itemsResultsObserver = new MutationObserver(function(mutationsList, observer) {
       for (const mutation of mutationsList) {
         if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
           const addedNodes = mutation.addedNodes;
           for (const node of addedNodes) {
             if (node.classList && node.classList.contains('results-list__item')) {
               const priceResults = node.querySelector('.info-card__price');
-              const newDivTaxCopy = document.createElement('div');
-              newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-              newDivTaxCopy.classList.add("DivTaxCopyStyle");
-              priceResults.appendChild(newDivTaxCopy);
+              addTaxCopyToPriceResults(priceResults);
             }
           }
         }
       }
     });
+  
     itemsResultsObserver.observe(resultsListHotels, { childList: true, subtree: true });
   }
   
-  document.addEventListener('DOMContentLoaded', () => {
-    addTaxCopyToPriceResults();
+  window.addEventListener('load', function() {
     observeResultsList();
   });
 // **************************************************************************
