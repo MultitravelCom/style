ed24b004715145770cd2c9ab43f73409e3bf4526
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 12:25:26 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -1,14 +1,14 @@
 // ***************************** MULT-209 *********************************************
 
 let resultsListHotels = document.querySelector('.js-results-list-placeholder');
-let itemsResults = resultsList.querySelectorAll('.results-list__item');
+let itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
 
 itemsResults.forEach(function(item) {
   let priceResults = item.querySelector('.info-card__price');
   let newDivTaxCopy = document.createElement('div');
   newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
   newDivTaxCopy.classList.add("bestprice__title");
-  
+
   priceResults.appendChild(newDivTaxCopy);
 });
 // **************************************************************************
