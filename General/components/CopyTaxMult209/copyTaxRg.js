79d5ada4b9e091333c073d50a19d149c92e47a48
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 20:25:14 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRg.js
+++ General/components/CopyTaxMult209/copyTaxRg.js
@@ -1,3 +1,23 @@
+function agregarElemento(referenceSelector, index = -1) {
+    const referenceNode = document.querySelector(referenceSelector);
+    if (!referenceNode) {
+        return; // si no se encuentra el elemento de referencia, se sale de la función
+    }
+
+    const newElement = document.createElement('div');
+    newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
+    newElement.innerHTML = `
+    <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+    <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
+  `;
+
+    if (index === -1 || index >= referenceNode.children.length) {
+        referenceNode.appendChild(newElement);
+    } else {
+        referenceNode.insertBefore(newElement, referenceNode.children[index]);
+    }
+}
+
 function observarCambiosEnDOM() {
     const targetNode = document.querySelector('.js-results-list-placeholder');
   
@@ -16,7 +36,7 @@ function observarCambiosEnDOM() {
             const currentItem = node.querySelector('.results-list__item--current-flight');
             if (currentItem) {
               const flightSelectionBox = currentItem.closest('.flight-selection__box');
-              agregarElemento(flightSelectionBox, -1);
+              agregarElemento('.flight-selection__box');
             }
           });
         }
@@ -29,27 +49,28 @@ function observarCambiosEnDOM() {
   observarCambiosEnDOM();
 
 
+
 // window.addEventListener('load', () => {
 
-//     function agregarElemento(referenceSelector, index = -1) {
-//         const referenceNode = document.querySelector(referenceSelector);
-//         if (!referenceNode) {
-//             return; // si no se encuentra el elemento de referencia, se sale de la función
-//         }
+    // function agregarElemento(referenceSelector, index = -1) {
+    //     const referenceNode = document.querySelector(referenceSelector);
+    //     if (!referenceNode) {
+    //         return; // si no se encuentra el elemento de referencia, se sale de la función
+    //     }
 
-//         const newElement = document.createElement('div');
-//         newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
-//         newElement.innerHTML = `
-//         <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-//         <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
-//       `;
+    //     const newElement = document.createElement('div');
+    //     newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
+    //     newElement.innerHTML = `
+    //     <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+    //     <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
+    //   `;
 
-//         if (index === -1 || index >= referenceNode.children.length) {
-//             referenceNode.appendChild(newElement);
-//         } else {
-//             referenceNode.insertBefore(newElement, referenceNode.children[index]);
-//         }
-//     }
+    //     if (index === -1 || index >= referenceNode.children.length) {
+    //         referenceNode.appendChild(newElement);
+    //     } else {
+    //         referenceNode.insertBefore(newElement, referenceNode.children[index]);
+    //     }
+    // }
 
 //     agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
 
