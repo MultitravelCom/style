786b4f2e0aebac3702797eaa0ca510e4736b8894
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 13:08:42 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -1,19 +1,23 @@
-function observeMutation(elementToObserve, callback) {
+async function observeMutation(elementToObserve, callback) {
+    while (!document.querySelector(elementToObserve)) {
+        // Esperar hasta que el elemento esté disponible en el DOM
+        await new Promise(resolve => requestAnimationFrame(resolve));
+    }
+
     const observer = new MutationObserver(callback);
-    observer.observe(elementToObserve, { childList: true, subtree: true });
+    observer.observe(document.querySelector(elementToObserve), { childList: true, subtree: true });
     return observer;
 }
 
 // ***************************** MULT-209 *********************************************
 let resultsListHotels = document.querySelector('.js-results-list-placeholder');
-let itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
 
-observeMutation(resultsListHotels, function (mutationsList, observer) {
-
-
+observeMutation('.js-results-list-placeholder', function (mutationsList, observer) {
+    const itemsResults = document.querySelectorAll('.results-list__item');
+   
     itemsResults.forEach(function (item) {
-        let priceResults = item.querySelector('.info-card__price');
-        let newDivTaxCopy = document.createElement('div');
+        const priceResults = item.querySelector('.info-card__price');
+        const newDivTaxCopy = document.createElement('div');
         newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
         newDivTaxCopy.classList.add("DivTaxCopyStyle");
 
