84fcff1fb48af955f6451a39fe66bb192c84a10a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 13:58:33 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -6,10 +6,37 @@ function addTaxCopyToPriceResults(priceResults) {
     priceResults.appendChild(newDivTaxCopy);
   }
   
-  let itemsResults = document.querySelectorAll('.results-list__item');
+  function observeResultsList() {
+    const resultsListHotels = document.querySelector('.js-results-list-placeholder');
+    if (!resultsListHotels) {
+      return; // si el selector no existe, no hacemos nada
+    }
   
-  itemsResults.forEach(function(item) {
-    let priceResults = item.querySelector('.info-card__price');
-    addTaxCopyToPriceResults(priceResults);
+    const itemsResults = resultsListHotels.querySelectorAll('.results-list__item');
+    if (itemsResults.length > 0) {
+      itemsResults.forEach(function(item) {
+        const priceResults = item.querySelector('.info-card__price');
+        addTaxCopyToPriceResults(priceResults);
+      });
+    } else {
+      const observer = new MutationObserver(function(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+          if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            const addedNodes = mutation.addedNodes;
+            for (const node of addedNodes) {
+              if (node.classList && node.classList.contains('results-list__item')) {
+                const priceResults = node.querySelector('.info-card__price');
+                addTaxCopyToPriceResults(priceResults);
+              }
+            }
+          }
+        }
+      });
+      observer.observe(resultsListHotels, { childList: true, subtree: true });
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    observeResultsList();
   });
 // **************************************************************************
