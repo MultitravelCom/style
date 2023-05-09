d8791efb1930570a4dcc6f13f389d86775e1a40c
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 17:12:47 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRg.js
+++ General/components/CopyTaxMult209/copyTaxRg.js
@@ -1,49 +1,57 @@
 window.addEventListener('load', () => {
 
     function agregarElemento(referenceSelector, index = -1) {
-        const referenceNode = document.querySelector(referenceSelector);
-        if (!referenceNode) {
-            return;
-        }
-
-        const newElement = document.createElement('div');
-        newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
-        newElement.innerHTML = `
-          <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-          <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
-        `;
-
-        if (index === -1 || index >= referenceNode.children.length) {
-            referenceNode.appendChild(newElement);
-        } else {
-            referenceNode.insertBefore(newElement, referenceNode.children[index]);
-        }
+      const referenceNode = document.querySelector(referenceSelector);
+      if (!referenceNode) {
+        return;
+      }
+  
+      const newElement = document.createElement('div');
+      newElement.classList.add('booking-breakdown__table', 'main__warningPrice');
+      newElement.innerHTML = `
+        <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+        <p>En caso de producto internacional, los precios siempre incluyen el impuesto país y las percepciones RG4815 Y RG5272.</p>
+      `;
+  
+      if (index === -1 || index >= referenceNode.children.length) {
+        referenceNode.appendChild(newElement);
+      } else {
+        referenceNode.insertBefore(newElement, referenceNode.children[index]);
+      }
     }
-
+  
     function onElementAddedToDOM(selector, callback) {
-        const observer = new MutationObserver(mutationsList => {
-            for (const mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                    for (const node of mutation.addedNodes) {
-                        if (node.nodeType === Node.ELEMENT_NODE && node.matches(selector)) {
-                            callback();
-                            console.log('Element added to DOM:', node);
-                        }
-                    }
+      const observer = new MutationObserver(mutationsList => {
+        for (const mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+            for (const node of mutation.addedNodes) {
+              if (node.nodeType === Node.ELEMENT_NODE && node.matches(selector)) {
+                if (!node.querySelector('.main__warningPrice')) {
+                  agregarElemento('.results-list__item.results-list__item--current-flight > .js-results-list-selection-placeholder > .flight-selection__box');
+                  console.log('Element added to DOM:', node);
                 }
+                callback();
+              }
             }
-        });
-
-        observer.observe(document.documentElement, { childList: true, subtree: true });
+          }
+        }
+      });
+  
+      observer.observe(document.documentElement, { childList: true, subtree: true });
     }
-
-    onElementAddedToDOM('.js-results-list-selection-placeholder', () => {
-        agregarElemento('.results-list__item.results-list__item--current-flight > .js-results-list-selection-placeholder > .flight-selection__box');
-        console.log("tesT")
-    });
-
-    agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
-    console.log("tesT")
-
-    agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web:nth-of-type(2)');
-});
+  
+    function addWarningElements() {
+      if (!document.querySelector('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web .main__warningPrice')) {
+        agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web');
+      }
+  
+      if (!document.querySelector('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web:nth-of-type(2) .main__warningPrice')) {
+        agregarElemento('.booking-breakdown__item.booking-breakdown__item--total.booking-breakdown__item--total-price.booking-breakdown__item--is-pay-web:nth-of-type(2)');
+      }
+    }
+  
+    onElementAddedToDOM('.js-results-list-selection-placeholder', addWarningElements);
+    addWarningElements();
+  
+  });
+  
