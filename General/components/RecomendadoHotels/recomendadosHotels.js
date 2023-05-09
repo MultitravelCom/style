95ffb940a45c933c1531963e208fc9bbdcec1971
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 13:29:51 -0300

    mult-209

--- General/components/RecomendadoHotels/recomendadosHotels.js
+++ General/components/RecomendadoHotels/recomendadosHotels.js
@@ -1,30 +1,36 @@
-window.onload = function () {
-
-    async function observeMutation(elementToObserve, callback) {
-        while (!document.querySelector(elementToObserve)) {
-            // Esperar hasta que el elemento esté disponible en el DOM
-            await new Promise(resolve => requestAnimationFrame(resolve));
-        }
-
-        const observer = new MutationObserver(callback);
-        observer.observe(document.querySelector(elementToObserve), { childList: true, subtree: true });
-        return observer;
+async function observeMutation(elementToObserve, callback) {
+    while (!document.querySelector(elementToObserve)) {
+      // Esperar hasta que el elemento esté disponible en el DOM
+      await new Promise(resolve => requestAnimationFrame(resolve));
     }
-
-    // ***************************** MULT-209 *********************************************
+  
+    const observer = new MutationObserver(callback);
+    observer.observe(document.querySelector(elementToObserve), { childList: true, subtree: true });
+    return observer;
+  }
+  
+  function addTaxCopyToPriceResults() {
     const itemsResults = document.querySelectorAll('.results-list__item');
-    // let resultsListHotels = document.querySelector('.js-results-list-placeholder');
-    
-    observeMutation(resultsListHotels, function (mutationsList, observer) {
-        
-        itemsResults.forEach(function (item) {
-            const priceResults = item.querySelector('.info-card__price');
-            const newDivTaxCopy = document.createElement('div');
-            newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
-            newDivTaxCopy.classList.add("DivTaxCopyStyle");
-
-            priceResults.appendChild(newDivTaxCopy);
-        });
+  
+    itemsResults.forEach(function (item) {
+      const priceResults = item.querySelector('.info-card__price');
+      const existingTaxCopy = item.querySelector('.DivTaxCopyStyle');
+  
+      if (!existingTaxCopy) {
+        const newDivTaxCopy = document.createElement('div');
+        newDivTaxCopy.textContent = 'Incluyen el impuesto país y las percepciones';
+        newDivTaxCopy.classList.add('DivTaxCopyStyle');
+        priceResults.appendChild(newDivTaxCopy);
+      }
     });
-}
+  }
+  
+  setInterval(() => {
+    const resultsListHotels = document.querySelector('.js-results-list-placeholder');
+    if (resultsListHotels) {
+      observeMutation(resultsListHotels, function(mutationsList, observer) {
+        addTaxCopyToPriceResults();
+      });
+    }
+  }, 1000);
 // **************************************************************************
