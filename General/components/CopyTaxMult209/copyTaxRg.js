52fe99066428bb33c59d34462c90992835907856
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 17:29:46 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRg.js
+++ General/components/CopyTaxMult209/copyTaxRg.js
@@ -27,7 +27,7 @@ window.addEventListener('load', () => {
             for (const node of mutation.addedNodes) {
               if (node.nodeType === Node.ELEMENT_NODE && node.matches(selector)) {
                 if (!node.querySelector('.main__warningPrice')) {
-                  agregarElemento('.js-results-list-selection-placeholder>.flight-selection');
+                  agregarElemento('.js-results-list-selection-placeholder');
                   console.log('Element added to DOM:', node);
                 }
                 callback();
