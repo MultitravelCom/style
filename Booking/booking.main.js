6a8496d7444dc2abc7a2ff7873a4e14f706974bb
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-10 12:16:11 -0300

    taxes

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -10,32 +10,51 @@ async function changeText() {
 }
 
 async function agreeBtn() {
-    const confirmBooking = document.querySelector('.confirm-booking__promocodes');
-
     const newElementBookingCupons = document.createElement('div');
+    newElementBookingCupons.id = 'openModalBtnContainer'; // Agrega un id al nuevo div
     newElementBookingCupons.classList.add('container__conocer__cupones');
-    newElementBookingCupons.innerHTML =
-        `<div class="container__conocer__cupones__p">
-            <p>Conoce y elegí de nuestros diferentes cupones el que más te convenga</p>
-        </div>
-        <div class="container__conocer__cupones__btn">
-            <button class="cupones__btn__style">Conocer cupones</button>
+    newElementBookingCupons.innerHTML = `
+    <div class="container__conocer__cupones__p">
+        <p>Conoce y elegí de nuestros diferentes cupones el que más te convenga</p>
+    </div>
+    <div class="container__conocer__cupones__btn">
+        <button id="openModalBtn" class="cupones__btn__style">Conocer cupones</button>
+    </div>
+`;
+
+    // ...
+
+    const openModalBtn = newElementBookingCupons.querySelector('#openModalBtn');
+    const openModalBtnContainer = newElementBookingCupons.querySelector('#openModalBtnContainer'); // Obtén el div que contiene el botón
+
+    openModalBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const modalCupones = document.createElement('div');
+        modalCupones.id = 'modalCuponera';
+        modalCupones.innerHTML = `
+        <div class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>Aquí puedes agregar cualquier contenido que desees mostrar en el modal.</p>
+            </div>
         </div>
     `;
-    
-    // Espera a que el elemento padre exista antes de agregar el nuevo elemento
-    while (!confirmBooking) {
-        await new Promise(resolve => setTimeout(resolve, 100));
-    }
-    
-    confirmBooking.appendChild(newElementBookingCupons);
+        document.body.appendChild(modalCupones);
 
-    const modal = document.getElementById("modalCuponera");
-    const openModalBtn = newElementBookingCupons.querySelector('.cupones__btn__style');
-    openModalBtn.addEventListener("click", function() {
-      modal.style.display = "block";
+        const modal = document.getElementById("modalCuponera");
+        const closeModalBtn = modal.querySelector('.close');
+        closeModalBtn.addEventListener("click", function () {
+            modal.style.display = "none";
+        });
+
+        modal.style.display = "block";
     });
+
+    // Agrega el div que contiene el botón al elemento padre
+    confirmBooking.appendChild(openModalBtnContainer);
 }
+
 
 async function showPromocodesDiv() {
     // Espera a que el elemento exista antes de intentar mostrarlo
@@ -47,18 +66,6 @@ async function showPromocodesDiv() {
         }
         await new Promise(resolve => setTimeout(resolve, 100));
     }
-}
-
-let modalCupones = document.getElementById('modalCuponera');
-if (modalCupones) {
-  modalCupones.innerHTML = `
-    <div class="modal">
-      <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Aquí puedes agregar cualquier contenido que desees mostrar en el modal.</p>
-      </div>
-    </div>
-  `;
 }
 
 // Ejecuta las funciones en orden y asegúrate de que se completen antes de continuar con la siguiente
