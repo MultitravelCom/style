fdcd60f33b800b3835099883aae339c9f0dced8a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-09 21:48:07 -0300

    mult-209

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -3,14 +3,11 @@ async function changeText() {
 
     if (!span) {
         await new Promise(resolve => setTimeout(resolve, 100));
-        changeText();
-        return;
+        await changeText();
+    } else {
+        span.textContent = 'Ingresá tu código promocional';
     }
-
-    span.textContent = 'Ingresá tu código promocional';
 }
-
-changeText();
 
 async function agreeBtn() {
     const confirmBooking = document.querySelector('.confirm-booking__promocodes');
@@ -18,24 +15,39 @@ async function agreeBtn() {
     const newElementBookingCupons = document.createElement('div');
     newElementBookingCupons.classList.add('container__conocer__cupones');
     newElementBookingCupons.innerHTML =
-            `<div class="container__conocer cupones__p">
-                    <p>Conoce y elegí de nuestros diferentes cupones el que más te convenga</p>
-            </div>
-            <div class="container__conocer cupones__btn">
-                <button class="cupones__btn__style">Conocer cupones</button>
-            </div>
+        `<div class="container__conocer cupones__p">
+            <p>Conoce y elegí de nuestros diferentes cupones el que más te convenga</p>
+        </div>
+        <div class="container__conocer cupones__btn">
+            <button class="cupones__btn__style">Conocer cupones</button>
+        </div>
     `;
+    
+    // Espera a que el elemento padre exista antes de agregar el nuevo elemento
+    while (!confirmBooking) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+    
     confirmBooking.appendChild(newElementBookingCupons);
-
 }
 
-agreeBtn();
-
-function showPromocodesDiv() {
-    setTimeout(function () {
+async function showPromocodesDiv() {
+    // Espera a que el elemento exista antes de intentar mostrarlo
+    while (true) {
         const promocodesDiv = document.querySelector('.confirm-booking__promocodes');
         if (promocodesDiv) {
             promocodesDiv.style.display = 'block';
+            break;
         }
-    }, 5000);
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
 }
+
+// Ejecuta las funciones en orden y asegúrate de que se completen antes de continuar con la siguiente
+async function run() {
+    await changeText();
+    await agreeBtn();
+    await showPromocodesDiv();
+}
+
+run();
