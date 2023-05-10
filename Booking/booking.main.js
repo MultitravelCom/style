37963ec40734005f1a026efd5810a2b3e3078544
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-10 14:34:45 -0300

    taxes

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -10,62 +10,29 @@ async function changeText() {
 }
 
 async function agreeBtn() {
-    const newElementBookingCupons = document.createElement('div');
-    newElementBookingCupons.id = 'openModalBtnContainer';
-    newElementBookingCupons.classList.add('container__conocer__cupones');
-    newElementBookingCupons.innerHTML = `
-      <div class="container__conocer__cupones__p">
-        <p>Conoce y elegí de nuestros diferentes cupones el que más te convenga</p>
-      </div>
-      <div class="container__conocer__cupones__btn">
-        <button id="openModalBtn" class="cupones__btn__style">Conocer cupones</button>
-      </div>
-    `;
+    const confirmBooking = document.querySelector('.confirm-booking__promocodes');
 
-    let confirmBooking;
+    const newElementBookingCupons = document.createElement('div');
+    newElementBookingCupons.classList.add('container__conocer__cupones');
+    newElementBookingCupons.innerHTML =
+        `<div class="container__conocer__cupones__p">
+            <p>Conoce y elegí de nuestros diferentes cupones el que más te convenga</p>
+        </div>
+        <div class="container__conocer__cupones__btn">
+            <button class="cupones__btn__style">Conocer cupones</button>
+        </div>
+    `;
+    
+    // Espera a que el elemento padre exista antes de agregar el nuevo elemento
     while (!confirmBooking) {
-        confirmBooking = document.querySelector('.container__conocer__cupones__btn');
         await new Promise(resolve => setTimeout(resolve, 100));
     }
-
-    const openModalBtnContainer = newElementBookingCupons.querySelector('#openModalBtnContainer');
-    const openModalBtn = newElementBookingCupons.querySelector('#openModalBtn');
-
-    openModalBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-        showModal();
-    });
-
-    confirmBooking.appendChild(openModalBtnContainer);
-}
-
-async function showModal() {
-    const modalCupones = document.createElement('div');
-    modalCupones.id = 'modalCuponera';
-    modalCupones.innerHTML = `
-      <div class="modal">
-        <div class="modal-content">
-          <span class="close">X</span>
-          <div class="row">
-            <div class="col-md-12 col-lg-4">Contenido del primer div</div>
-            <div class="col-md-12 col-lg-4">Contenido del segundo div</div>
-            <div class="col-md-12 col-lg-4">Contenido del tercer div</div>
-          </div>
-        </div>
-      </div>
-    `;
-    document.body.appendChild(modalCupones);
-
-    const modal = document.getElementById('modalCuponera');
-    const closeModalBtn = modal.querySelector('.close');
-    closeModalBtn.addEventListener('click', function () {
-        modal.style.display = 'none';
-    });
-
-    modal.style.display = 'block';
+    
+    confirmBooking.appendChild(newElementBookingCupons);
 }
 
 async function showPromocodesDiv() {
+    // Espera a que el elemento exista antes de intentar mostrarlo
     while (true) {
         const promocodesDiv = document.querySelector('.confirm-booking__promocodes');
         if (promocodesDiv) {
@@ -76,11 +43,11 @@ async function showPromocodesDiv() {
     }
 }
 
+// Ejecuta las funciones en orden y asegúrate de que se completen antes de continuar con la siguiente
 async function run() {
     await changeText();
     await agreeBtn();
     await showPromocodesDiv();
 }
 
-    run();
-
+run();
