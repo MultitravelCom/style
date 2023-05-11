508b82a0cd46c70c131695e6fe68853de67115c8
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-11 15:10:34 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRgComponent.js
+++ General/components/CopyTaxMult209/copyTaxRgComponent.js
@@ -1,4 +1,40 @@
+const observeFlightSelectionBox = () => {
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.addedNodes) {
+                mutation.addedNodes.forEach((node) => {
+                    if (node.classList && node.classList.contains('flight-selection__box')) {
+                        renderCopyTaxFlight();
+                    }
+                });
+            }
+            if (mutation.removedNodes) {
+                mutation.removedNodes.forEach((node) => {
+                    if (node.classList && node.classList.contains('flight-selection__box')) {
+                        unmountCopyTaxFlight();
+                    }
+                });
+            }
+        });
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+};
+
+
 const CopyTaxFlight = () => {
+
+    useEffect(() => {
+        observeFlightSelectionBox();
+
+        return () => {
+            unmountCopyTaxFlight();
+        };
+    }, []);
+
     return (
         <div className="main_container_copyTaxFlight">
             <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
@@ -8,9 +44,19 @@ const CopyTaxFlight = () => {
 };
 
 
-const flightBestPrices = document.querySelector('.flight-bestprices');
-const ContainerCopyTaxFlight = document.createElement('div');
-ContainerCopyTaxFlight.classList.add('booking-breakdown__table');
-flightBestPrices.after(ContainerCopyTaxFlight);
+const renderCopyTaxFlight = () => {
+    const flightBestPrices = document.querySelector('.flight-selection__box');
+    const ContainerCopyTaxFlight = document.createElement('div');
+    ContainerCopyTaxFlight.classList.add('booking-breakdown__table', 'main__warningPrice');
+    flightBestPrices.before(ContainerCopyTaxFlight);
 
-ReactDOM.render(<CopyTaxFlight />, ContainerCopyTaxFlight);
+    ReactDOM.render(<CopyTaxFlight />, ContainerCopyTaxFlight);
+};
+
+const unmountCopyTaxFlight = () => {
+    const container = document.querySelector('.booking-breakdown__table.main__warningPrice');
+    if (container) {
+        ReactDOM.unmountComponentAtNode(container);
+        container.remove();
+    }
+};
