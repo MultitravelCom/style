241ae3559eba1b51ce8ecc239ff6de19ad3ee623
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-11 13:13:00 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRgComponent.js
+++ General/components/CopyTaxMult209/copyTaxRgComponent.js
@@ -1,14 +1,29 @@
 const CopyTaxFlight = () => {
+    const [flightSelectionBoxExists, setFlightSelectionBoxExists] = useState(false);
+  
+    useEffect(() => {
+      const flightSelectionBox = document.querySelector('.flight-selection__box');
+      if (flightSelectionBox) {
+        setFlightSelectionBoxExists(true);
+        flightSelectionBox.appendChild(myContainer);
+      } else {
+        setFlightSelectionBoxExists(false);
+      }
+    }, []);
+  
     return (
-        <div className="main_container_copyTaxFlight">
+      <>
+        {flightSelectionBoxExists && (
+          <div className="main_container_copyTaxFlight">
             <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
             <p>En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
-        </div>
+          </div>
+        )}
+      </>
     );
-};
+  };
 
-
-const flightBestPrices = document.querySelector('.flight-selection__breakdown');
+const flightBestPrices = document.querySelector('.flight-selection__box');
 const ContainerCopyTaxFlight = document.createElement('div');
 ContainerCopyTaxFlight.classList.add('booking-breakdown__table', 'main__warningPrice');
 flightBestPrices.after(ContainerCopyTaxFlight);
