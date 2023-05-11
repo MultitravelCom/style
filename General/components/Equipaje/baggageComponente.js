804646c1305b656bdc58f04aaee4adee6a6962ff
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-11 16:01:27 -0300

    mult-209

--- General/components/Equipaje/baggageComponente.js
+++ General/components/Equipaje/baggageComponente.js
@@ -31,7 +31,7 @@ const BaggagePax = () => {
                     <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
                     <p className="main__warningPric__icon__p">Si deseas volver atrás y ver diferentes vuelos con una diferente cantidad de equipaje, utiliza el botón a continuación y utiliza nuestros filtros para realizar tu búsqueda más precisa.</p>
                     <div>
-                        <Button id={destino.title} text="< VOLVER" />
+                        <Button text="< VOLVER" />
                     </div>
                 </div>
             }
