31933d27dadcff9bfc78bef65349cdca7ce41342
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-11 13:47:54 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRgComponent.js
+++ General/components/CopyTaxMult209/copyTaxRgComponent.js
@@ -1,33 +1,16 @@
 const CopyTaxFlight = () => {
-    const [flightSelectionBoxExists, setFlightSelectionBoxExists] = useState(false);
-  
-    useEffect(() => {
-      const flightSelectionBox = document.querySelector('.flight-selection__box');
-      if (flightSelectionBox) {
-        setFlightSelectionBoxExists(true);
-  
-        const ContainerCopyTaxFlight = document.createElement('div');
-        ContainerCopyTaxFlight.classList.add('booking-breakdown__table', 'main__warningPrice');
-        flightSelectionBox.before(ContainerCopyTaxFlight);
-  
-        ReactDOM.render(
-          <CopyTaxFlight />,
-          ContainerCopyTaxFlight
-        );
-      } else {
-        setFlightSelectionBoxExists(false);
-      }
-    }, []);
-  
     return (
-      <>
-        {flightSelectionBoxExists && (
-          <div className="main_container_copyTaxFlight">
+        <div className="main_container_copyTaxFlight">
             <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
             <p>En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
-          </div>
-        )}
-      </>
+        </div>
     );
-  };
-  
+};
+
+
+const flightBestPrices = document.querySelector('.flight-bestprices');
+const ContainerCopyTaxFlight = document.createElement('div');
+ContainerCopyTaxFlight.classList.add('booking-breakdown__table', 'main__warningPrice');
+flightBestPrices.after(ContainerCopyTaxFlight);
+
+ReactDOM.render(<CopyTaxFlight />, ContainerCopyTaxFlight);
