4720fb9c83346b2e15aeaec18957cd7fd1c48946
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-10 22:55:48 -0300

    mult-209

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -246,11 +246,9 @@ function shouldShowEvent() {
 }
 
 const EventImg = (props) => {
-    const startDate = new Date("2023-05-07T23:59:00");
-    const endDate = new Date("2023-05-10T23:59:00");
-    const now = new Date();
+    const { shouldShowEvent } = props;
 
-    if (now > endDate) {
+    if (!shouldShowEvent()) {
         return (
             <div className={props.style}>
                 <img
@@ -288,7 +286,7 @@ const Card = ({ destinos }) => {
         destinos.map((destino) => (
             <div key={destino.id} className="carrusel__elemento">
                 <div className="main__conteiner__s1__destacado__card uno" style={{ height: "100%", width: "100%" }}>
-                    {shouldShowEvent() && <EventImg style="eventImg" />}
+                    {shouldShowEvent() && <EventImg style="eventImg" shouldShowEvent={shouldShowEvent}/>}
                     <picture>
                         <map name={destino.id}>
                             <area target="_blank" alt={destino.title} title={destino.title} href={destino.linkWa} coords={destino.coords} shape="rect" />
