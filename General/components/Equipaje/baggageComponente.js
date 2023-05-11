218e7e028f1b5ad4b273b93c2f608c8cfd245e32
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-11 15:29:20 -0300

    mult-209

--- General/components/Equipaje/baggageComponente.js
+++ General/components/Equipaje/baggageComponente.js
@@ -1,3 +1,15 @@
+function Button(props) {
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        window.history.back();
+    }
+
+    return (
+        <button id={props.id} className={props.style} onClick={handleClick}>{props.text}</button>
+    );
+}
+
 const BaggagePax = () => {
     return (
         <div className="main__warningPric">
@@ -5,11 +17,11 @@ const BaggagePax = () => {
             <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
             <p className="main__warningPric__icon__p">Si deseas volver atrás y ver diferentes vuelos con una diferente cantidad de equipaje, utiliza el botón a continuación y utiliza nuestros filtros para realizar tu búsqueda más precisa.</p>
             <div>
-                <Button id={destino.title} link={destino.linkWa} text="< VOLVER" />
+                <Button id={destino.title} text="< VOLVER" />
             </div>
         </div>
     )
 }
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<BaggagePax />, document.getElementsByClassName("baggage-selection__bag-section"));
