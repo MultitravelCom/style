c69609906584f9f171a0957ed3833d09770241b3
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-11 14:56:22 -0300

    mult-209

--- General/components/CopyTaxMult209/copyTaxRgComponent.js
+++ General/components/CopyTaxMult209/copyTaxRgComponent.js
@@ -2,7 +2,7 @@ const CopyTaxFlight = () => {
     return (
         <div className="main_container_copyTaxFlight">
             <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-            <p>En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
+            <p className="main__warningPric__icon__p">En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
         </div>
     );
 };
