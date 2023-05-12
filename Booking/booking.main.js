fc79d59834553a144bc83281926942612b3c3c9a
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-12 15:44:08 -0300

    mult-199

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -65,21 +65,20 @@ function handleClick(event) {
                     </div>
                     <div class="modal__content-title-circleCalendar">
                         <div class="modal__content-title-h3">
-                            <h2>10% OFF en vuelos y paquetes</h2>
+                            <h2>15% OFF sobre la tarifa base</h2>
                         </div>
                         <div class="modal__content-title-circle">
                             <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-                                <p>En paquetes, el descuento se aplica solo al aéreo.
-                                Tope de descuento $25.000. Cantidad limitada.</p>
+                                <p>Vuelos a Miami y Nueva York con Aerolineas Argentinas.</p>
                             </div>
                         <div class="modal__content-title-calendar">
                             <div class="main__warningPric__icon glyphicon glyphicon-calendar"></div>
-                            <p>Disponi ble hasta el 10/05/2023 hasta las 23:59.</p>
+                            <p>Disponible hasta el 12/05/2023 hasta las 23:59.</p>
                         </div>
                     </div>
                 </div>
                 <div class="modal__content-cupon">
-                    <h2>MULTITRAVELHOTSALE10</h2>
+                    <h2>ARMTSALE15</h2>
                     <span>Copia el siguiente código</span>
                 </div>
             </div>
@@ -89,21 +88,20 @@ function handleClick(event) {
                     </div>
                     <div class="modal__content-title-circleCalendar">
                         <div class="modal__content-title-h3">
-                            <h2>10% OFF en vuelos y paquetes</h2>
+                            <h2>10% OFF sobre la tarifa base</h2>
                         </div>
                         <div class="modal__content-title-circle">
                             <div class="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-                                <p>En paquetes, el descuento se aplica solo al aéreo.
-                                Tope de descuento $25.000. Cantidad limitada.</p>
+                                <p>Vuelos a Madrid, Rio de Janeiro, Cancun, Punta Cana, Bogota, Havana, Lima, Roma, Santiago de Chile, Salvador de Bahia, Porto Seguro con Aerolíneas Argentinas.</p>
                             </div>
                         <div class="modal__content-title-calendar">
                             <div class="main__warningPric__icon glyphicon glyphicon-calendar"></div>
-                            <p>Disponi ble hasta el 10/05/2023 hasta las 23:59.</p>
+                            <p>Disponible hasta el 12/05/2023 hasta las 23:59.</p>
                         </div>
                     </div>
                 </div>
                 <div class="modal__content-cupon">
-                    <h2>MULTITRAVELHOTSALE10</h2>
+                    <h2>ARMTSALE10</h2>
                     <span>Copia el siguiente código</span>
                 </div>
             </div>
