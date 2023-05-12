301317e4e6f2e3f62acc5c3da0b5af19ed89bf95
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-12 11:39:46 -0300

    mult-199

---
+++ General/Transferencias/transferencia.js
@@ -0,0 +1,19 @@
+const TransferenciaComponent = () => {
+    return (
+        <>
+            <div className="main__transferencia container">
+                <picture>
+                    <source media="(min-width: 1024px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp" />
+                    <source media="(min-width: 768px) and (max-width: 1023px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenTablet.webp" />
+                    <source media="(max-width: 767px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenMobile.webp" />
+                    <img alt="" src="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp" />
+                </picture>
+            </div>
+        </>
+    )
+}
+
+ReactDOM.render(<TransferenciaComponent />, document.querySelector(".booking-payment__info prepayment"));
