89b240bda2607dee8be72bf932a24a237e98897d
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-12 16:24:10 -0300

    mult-199

--- General/addHeaderLinks.js
+++ General/addHeaderLinks.js
@@ -30,15 +30,15 @@ function addHeaderLinks() {
         head.appendChild(copyTaxRegStyle);
         head.appendChild(copyTaxRegJs);
     }
-    // const copyTaxRgComponent = path.includes('/flights/results');
+    const copyTaxRgComponent = path.includes('/flights/results');
 
-    // if (copyTaxRgComponent) {
-    //     const copyTaxRgComponentJs = document.createElement('script');
-    //     copyTaxRgComponentJs.src = 'https://multitravelcom.github.io/style/General/components/CopyTaxMult209/copyTaxRgComponent.js';
-    //     copyTaxRgComponentJs.type = "text/babel";
+    if (copyTaxRgComponent) {
+        const copyTaxRgComponentJs = document.createElement('script');
+        copyTaxRgComponentJs.src = 'https://multitravelcom.github.io/style/General/components/CopyTaxMult209/copyTaxRgComponent.js';
+        copyTaxRgComponentJs.type = "text/babel";
         
-    //     head.appendChild(copyTaxRgComponentJs);
-    // }
+        head.appendChild(copyTaxRgComponentJs);
+    }
 
     const priceFinalFlight = path.includes('/results');
     if (priceFinalFlight) {
