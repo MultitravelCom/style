30cef78d82f3ff1b4b035f1f9dedf03acf83dd0b
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-12 11:55:54 -0300

    mult-199

--- General/Transferencias/transferencia.js
+++ General/Transferencias/transferencia.js
@@ -16,4 +16,4 @@ const TransferenciaComponent = () => {
     )
 }
 
-ReactDOM.render(<TransferenciaComponent />, document.querySelector(".booking-payment__info prepayment"));
+ReactDOM.render(<TransferenciaComponent />, document.querySelector(".booking-payment__info"));
