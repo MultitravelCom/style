957a330e6b135a87518bf7b65645e68b1149306f
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-12 12:25:37 -0300

    mult-199

--- General/Transferencias/transferencia.js
+++ General/Transferencias/transferencia.js
@@ -2,33 +2,29 @@ const TransferenciaComponent = () => {
 
     return (
         <>
-            {isVisible && (
-                <div className="main__transferencia container">
-                    <picture>
-                        <source
-                            media="(min-width: 1024px)"
-                            srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp"
-                        />
-                        <source
-                            media="(min-width: 768px) and (max-width: 1023px)"
-                            srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenTablet.webp"
-                        />
-                        <source
-                            media="(max-width: 767px)"
-                            srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenMobile.webp"
-                        />
-                        <img
-                            alt=""
-                            src="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp"
-                        />
-                    </picture>
-                </div>
-            )}
+            <div className="main__transferencia container">
+                <picture>
+                    <source
+                        media="(min-width: 1024px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp"
+                    />
+                    <source
+                        media="(min-width: 768px) and (max-width: 1023px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenTablet.webp"
+                    />
+                    <source
+                        media="(max-width: 767px)"
+                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenMobile.webp"
+                    />
+                    <img
+                        alt=""
+                        src="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp"
+                    />
+                </picture>
+            </div>
         </>
     );
 };
 
-const paymentInfoContainers = document.querySelectorAll('.booking-payment__info');
-const secondPaymentInfoContainer = paymentInfoContainers[1];
-
-ReactDOM.render(<TransferenciaComponent />, secondPaymentInfoContainer);
+const paymentInfoContainers = document.querySelectorAll('booking-payment__info.prepayment');
+ReactDOM.render(<TransferenciaComponent />, paymentInfoContainers);
