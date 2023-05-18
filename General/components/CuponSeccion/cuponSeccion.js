65061d8a6076d23ad418efb668e13c5c87f8562a
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-18 10:56:53 -0300

    alojamientos

--- General/components/CuponSeccion/cuponSeccion.js
+++ General/components/CuponSeccion/cuponSeccion.js
@@ -39,7 +39,6 @@ modalCupones.innerHTML = `
   </div>
 `;
 
-<<<<<<< HEAD
 // const Cupones = () => {
 //   return (
 //     <div class="modal__content">
@@ -68,77 +67,3 @@ modalCupones.innerHTML = `
 //     </div>
 //   )
 // }
-=======
-const CuponsComponente = () => {
-  const [cupones, setCupones] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchCuponesData();
-      setCupones(data);
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <div className="main__cupon">
-        <div className="main__cupon__img"></div>
-        <div className="main__cupon__text">
-          <p>{cupon.titple}</p>
-        </div>
-        {cupones.map((cupon) => (
-          <React.Fragment key={cupon.id}>
-            <CuponsCondicion cupon={cupon} />
-          </React.Fragment>
-        ))}
-      </div>
-    </>
-  );
-};
-
-const CuponsCondicion = ({ cupon }) => {
-  return (
-    <div>
-      <div className="main__cupon__text--condiciones">
-        <div className="modal__content-title-circle">
-          <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-          <p>{cupon.destino}</p>
-        </div>
-        <div className="modal__content-title-calendar">
-          <div className="main__warningPric__icon glyphicon glyphicon-calendar"></div>
-          <p>{cupon.fecha}</p>
-        </div>
-      </div>
-      <div className="modal__content-cupon">
-        <h2>{cupon.cupon}</h2>
-        <span>Copia el siguiente código</span>
-      </div>
-    </div>
-  );
-};
-
-
-const ModalComponent = () => {
-  return (
-    <>
-      <div class="overlay__cupones" id="overlay__cupones">
-        <div class="modal__cupones">
-          <div class="modal-content">
-            <div class="modal__content-title">
-              <h2>Te damos la bienvenida a nuestra cuponera</h2>
-              <span class="close">X</span>
-            </div>
-            <div>
-              <CuponsCondicion />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
-
-
-ReactDOM.render(<ModalComponent />, document.getElementById("root"));
->>>>>>> c9ce9d514335b20a6da2aea46d5a8f4f8b23b903
