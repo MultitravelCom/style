2c88cc5829d0ed9c3c4efda2462de3f27b5cccf7
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-19 13:59:15 -0300

    boton modal

--- Estaticas/argetina_test.js
+++ Estaticas/argetina_test.js
@@ -70,46 +70,47 @@ const btnStyles = [
 // ************** COMPONENTES ********************
 const Button = (props) => {
     const [modalOpen, setModalOpen] = React.useState(false);
-
+  
     const handleClick = (event) => {
-        event.preventDefault();
-
-        if (window.innerWidth <= 767) {
-            // Llamar a un número en dispositivos móviles
-            window.location.href = 'tel:08003480003';
-        } else {
-            // Abrir el formulario en dispositivos de escritorio
-            setModalOpen(true);
-        }
+      event.preventDefault();
+  
+      if (window.innerWidth <= 767) {
+        // Llamar a un número en dispositivos móviles
+        window.location.href = 'tel:08003480003';
+      } else {
+        // Abrir el formulario en dispositivos de escritorio
+        setModalOpen(true);
+      }
     };
-
+  
     React.useEffect(() => {
-        const handleFormLoad = () => {
-            setModalOpen(true);
-        };
-        const script = document.createElement('script');
-        script.src = 'https://cdn.bitrix24.com/b19657597/crm/form/loader_54.js';
-        script.async = true;
-        script.onload = handleFormLoad;
-
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
+      const handleFormLoad = () => {
+        setModalOpen(true);
+      };
+  
+      const script = document.createElement('script');
+      script.src = 'https://cdn.bitrix24.com/b19657597/crm/form/loader_54.js';
+      script.async = true;
+      script.onload = handleFormLoad;
+  
+      document.body.appendChild(script);
+  
+      return () => {
+        document.body.removeChild(script);
+      };
     }, []);
-
+  
     return (
-        <div>
-          <button id={props.id} className="btn_Style_Venta_Per" onClick={handleClick}>{props.text}</button>
-          {modalOpen && (
-            <div className="modal">
-              <button onClick={() => setModalOpen(false)}>Cerrar modal</button>
-            </div>
-          )}
-        </div>
-      );
-};
+      <div>
+        <button id={props.id} className="btn_Style_Venta_Per" onClick={handleClick}>{props.text}</button>
+        {modalOpen && (
+          <div className="modal">
+            <button onClick={() => setModalOpen(false)}>Cerrar modal</button>
+          </div>
+        )}
+      </div>
+    );
+  };
 const Loader = () => {
     return (
         <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
