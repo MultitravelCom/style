d8598d0868881f3d7333dd5f6d5f5f1a6c0f65e7
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-19 18:06:36 -0300

    boton modal

--- Estaticas/Brasil/Brasil.js
+++ Estaticas/Brasil/Brasil.js
@@ -95,8 +95,13 @@ const btnStyles = [
 function Button(props) {
     const handleClick = (event) => {
         event.preventDefault();
-        window.open(props.link, '_blank');
-    }
+
+        if (window.innerWidth <= 767) {
+            // Llamar a un número en dispositivos móviles
+            window.location.href = 'tel:08003480003';
+            return;
+        }
+    };
 
     return (
         <button id={props.id} className={props.style} onClick={handleClick}>{props.text}</button>
