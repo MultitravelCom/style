7e50917fbd56ccc0bd5238c61c805cdc037a5b07
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-22 12:15:16 -0300

    Transferencia

--- Booking/booking.main.js
+++ Booking/booking.main.js
@@ -122,12 +122,13 @@ function handleClick(event) {
 
 document.addEventListener('DOMContentLoaded', function () {
     const liElement = document.querySelector('.booking-payment__options-prepayment');
+    const bookingPaymentOptions = document.querySelector('.booking-payment__options');
 
     if (liElement) {
         liElement.childNodes.forEach(node => {
             if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() === 'Transferencia') {
                 node.textContent = 'Transferencia / Efectivo';
-                liElement.style.display = 'block';
+                bookingPaymentOptions.style.display = 'inline-block;';
             }
         });
     }
