1876ef77d6a28306bd7b4314e7b62870bfb7c417
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-29 14:49:54 -0300

    mult-271

--- General/components/CopyTaxMult209/copyTaxRgComponent.js
+++ General/components/CopyTaxMult209/copyTaxRgComponent.js
@@ -1,15 +1,40 @@
 const CopyTaxFlight = () => {
-    const shouldShowComponent = document.querySelector('.flight-selection__box') !== null;
-  
-    return shouldShowComponent ? (
-      <div className="main_container_copyTaxFlight">
-        <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-        <p className="main__warningPric__icon__p">En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
+  const [shouldShowComponent, setShouldShowComponent] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleDOMChange = () => {
+      const flightSelectionBox = document.querySelector('.flight-selection__box');
+      setShouldShowComponent(flightSelectionBox !== null);
+    };
+
+    // Observar cambios en el DOM usando MutationObserver
+    const observer = new MutationObserver(handleDOMChange);
+    const targetNode = document.documentElement;
+    const config = { childList: true, subtree: true };
+
+    observer.observe(targetNode, config);
+
+    // Detener la observación cuando el componente se desmonta
+    return () => observer.disconnect();
+  }, []);
+
+  return shouldShowComponent ? (
+    <div className="main_container_copyTaxFlight">
+      <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+      <p className="main__warningPric__icon__p">
+        En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.
+      </p>
+    </div>
+  ) : null;
+};
+
+
+const App = () => {
+    return (
+      <div>
+        <CopyTaxFlight />
       </div>
-    ) : null;
+    );
   };
   
-  // Inserta el componente CopyTaxFlight en el lugar deseado
-
-  const ContainerCopyTaxFlight = document.querySelector(".flight-selection")  
-  ReactDOM.render(<CopyTaxFlight />, ContainerCopyTaxFlight);
+  ReactDOM.render(<App />, document.getElementsByClassName('flight-selection__box'));
