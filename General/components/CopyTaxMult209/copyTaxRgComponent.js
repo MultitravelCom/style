9908ce5f06c7af8a74feb6b48e7f92b1ffabb8c8
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-29 13:34:27 -0300

    mult-271

--- General/components/CopyTaxMult209/copyTaxRgComponent.js
+++ General/components/CopyTaxMult209/copyTaxRgComponent.js
@@ -1,16 +1,18 @@
 const CopyTaxFlight = () => {
-    return (
-        <div className="main_container_copyTaxFlight">
-            <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-            <p className="main__warningPric__icon__p">En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
-        </div>
-    );
-};
-
-
-const flightBestPrices = document.querySelector('.flight-bestprices');
-const ContainerCopyTaxFlight = document.createElement('div');
-ContainerCopyTaxFlight.classList.add('booking-breakdown__table');
-flightBestPrices.after(ContainerCopyTaxFlight);
-
-ReactDOM.render(<CopyTaxFlight />, ContainerCopyTaxFlight);
+    const shouldShowComponent = document.querySelector('.flight-selection__box') !== null;
+  
+    return shouldShowComponent ? (
+      <div className="main_container_copyTaxFlight">
+        <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+        <p className="main__warningPric__icon__p">En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
+      </div>
+    ) : null;
+  };
+  
+  // Inserta el componente CopyTaxFlight en el lugar deseado
+  const flightBestPrices = document.querySelector('.flight-bestprices');
+  const ContainerCopyTaxFlight = document.createElement('div');
+  ContainerCopyTaxFlight.classList.add('booking-breakdown__table');
+  flightBestPrices.after(ContainerCopyTaxFlight);
+  
+  ReactDOM.render(<CopyTaxFlight />, ContainerCopyTaxFlight);
