323de7bb2a3f964b54927ba340799a76bc8107c0
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-29 15:19:34 -0300

    mult-271

--- General/components/CopyTaxMult209/copyTaxRgComponent.js
+++ General/components/CopyTaxMult209/copyTaxRgComponent.js
@@ -1,37 +1,16 @@
 const CopyTaxFlight = () => {
     return (
-      <div className="main_container_copyTaxFlight">
-        <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-        <p className="main__warningPric__icon__p">
-          En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.
-        </p>
-      </div>
+        <div className="main_container_copyTaxFlight">
+            <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+            <p className="main__warningPric__icon__p">En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.</p>
+        </div>
     );
-  };
-  
-  const App = () => {
-    React.useEffect(() => {
-      const observer = new MutationObserver((mutationsList) => {
-        const breakdownContainer = document.querySelector('.flight-selection__breakdown');
-        const copyTaxFlightContainer = breakdownContainer.querySelector('.main_container_copyTaxFlight');
-  
-        if (breakdownContainer && !copyTaxFlightContainer) {
-          const newCopyTaxFlightContainer = document.createElement('div');
-          newCopyTaxFlightContainer.className = 'main_container_copyTaxFlight';
-          breakdownContainer.appendChild(newCopyTaxFlightContainer);
-  
-          ReactDOM.render(<CopyTaxFlight />, newCopyTaxFlightContainer);
-        }
-      });
-  
-      const targetNode = document.body;
-      const config = { childList: true, subtree: true };
-  
-      observer.observe(targetNode, config);
-  
-      return () => observer.disconnect();
-    }, []);
-  
-    return null;
-  };
-  
+};
+
+
+const flightBestPrices = document.querySelector('.flight-bestprices');
+const ContainerCopyTaxFlight = document.createElement('div');
+ContainerCopyTaxFlight.classList.add('booking-breakdown__table');
+flightBestPrices.after(ContainerCopyTaxFlight);
+
+ReactDOM.render(<CopyTaxFlight />, ContainerCopyTaxFlight);
