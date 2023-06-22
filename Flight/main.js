0d5e585696c9e20b84b99fb6c1a1e0ed2b07930e
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-22 11:34:35 -0300

    equipaje

--- Flight/main.js
+++ Flight/main.js
@@ -1,4 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // MULT-
+
+  function changeSelectText() {
+    let selectElementNew = document.querySelector('select[name="baggageincluded"]');
+    let labelElementNew = document.querySelector('label[for="flight-searcher-baggageincluded"]');
+    let spanElementNew = document.querySelector('#select2-flight-searcher-baggageincluded-container');
+
+    if (selectElementNew && labelElementNew && spanElementNew) {
+      let optionElementsNew = selectElementNew.querySelectorAll('option');
+
+      for (let i = 0; i < optionElementsNew.length; i++) {
+        if (optionElementsNew[i].textContent.includes("Con equipaje incluido")) {
+          optionElementsNew[i].textContent = optionElementsNew[i].textContent.replace("Con equipaje incluido", "Con equipaje a despachar");
+        } else if (optionElementsNew[i].textContent.includes("Sin equipaje incluido")) {
+          optionElementsNew[i].textContent = optionElementsNew[i].textContent.replace("Sin equipaje incluido", "Sin equipaje a despachar");
+        }
+      }
+
+      if (spanElementNew.getAttribute('title') === "Con equipaje incluido") {
+        spanElementNew.textContent = "Con equipaje a despachar";
+      } else if (spanElementNew.getAttribute('title') === "Sin equipaje incluido") {
+        spanElementNew.textContent = "Sin equipaje a despachar";
+      }
+
+      labelElementNew.textContent = "Equipaje a despachar";
+    }
+  }
+
+  document.querySelector('select[name="baggageincluded"]').addEventListener('change', changeSelectText);
+
+  changeSelectText();
   // MULT-127
   const divAMover = document.querySelector('.hidden-lg.col-xs-6.col-sm-2.pull-right');
   const divObjetivo = document.getElementById('flight-searcher-more-options');
@@ -35,36 +66,6 @@ document.addEventListener('DOMContentLoaded', function () {
   btnMoreOptions.setAttribute('data-toggle-text', 'Clase y equipaje a despachar');
   btnMoreOptions.style.display = 'inline-block';
   // ********************************************************************************************
-
-  function changeSelectText() {
-    let selectElementNew = document.querySelector('select[name="baggageincluded"]');
-    let labelElementNew = document.querySelector('label[for="flight-searcher-baggageincluded"]');
-    let spanElementNew = document.querySelector('#select2-flight-searcher-baggageincluded-container');
-
-    if (selectElementNew && labelElementNew && spanElementNew) {
-      let optionElementsNew = selectElementNew.querySelectorAll('option');
-
-      for (let i = 0; i < optionElementsNew.length; i++) {
-        if (optionElementsNew[i].textContent.includes("Con equipaje incluido")) {
-          optionElementsNew[i].textContent = optionElementsNew[i].textContent.replace("Con equipaje incluido", "Con equipaje a despachar");
-        } else if (optionElementsNew[i].textContent.includes("Sin equipaje incluido")) {
-          optionElementsNew[i].textContent = optionElementsNew[i].textContent.replace("Sin equipaje incluido", "Sin equipaje a despachar");
-        }
-      }
-
-      if (spanElementNew.getAttribute('title') === "Con equipaje incluido") {
-        spanElementNew.textContent = "Con equipaje a despachar";
-      } else if (spanElementNew.getAttribute('title') === "Sin equipaje incluido") {
-        spanElementNew.textContent = "Sin equipaje a despachar";
-      }
-
-      labelElementNew.textContent = "Equipaje a despachar";
-    }
-  }
-
-  document.querySelector('select[name="baggageincluded"]').addEventListener('change', changeSelectText);
-
-  changeSelectText();
 
 });
 
