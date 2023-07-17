29db386fcdbe80affae0b27d5efc7d1a9bb52a10
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-17 13:35:54 -0300

    banner

--- General/components/BannerTop.js
+++ General/components/BannerTop.js
@@ -32,17 +32,17 @@ const BannerTop = () => {
     return (
         <>
             <div className="container-fluid main__container__bannerTop scroll-mobile">
-            <div id="bannerTop__left" className="main__container_left">
+                <div id="bannerTop__left" className="main__container_left">
                     {isMobile ? (
                         // <a href="https://www.example.com/mobile-link" target="_blank">
-                            <img className="bannerTop__img"
-                                alt=""
-                                srcSet={
-                                    showPackageImages
-                                        ? `${UrlImgPaquetes}/Paquetes/bannerMobile-1.webp`
-                                        : `${UrlImg}/bannerMobile-2.webp`
-                                }
-                            />
+                        <img className="bannerTop__img"
+                            alt=""
+                            srcSet={
+                                showPackageImages
+                                    ? `${UrlImgPaquetes}/Paquetes/bannerMobile-1.webp`
+                                    : `${UrlImg}/bannerMobile-2.webp`
+                            }
+                        />
                         // </a>
                     ) : (
                         <a href="https://www.multitravel.com/venta-personalizada/argentina" target="_blank">
@@ -79,7 +79,19 @@ const BannerTop = () => {
                     )}
                 </div>
                 <div id="bannerTop__center" className="main__container_center">
-                    <a href="https://www.multitravel.com/venta-personalizada/argentina" target="_blank">
+                    {isMobile ? (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/argentina" : "https://www.example.com/mobile-link"} target="_blank">
+                            <img className="bannerTop__img"
+                                alt=""
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/Paquetes/bannerMobile-2.webp`
+                                        : `${UrlImg}/bannerMobile-1.webp`
+                                }
+                            />
+                        </a>
+                    ) : (
+                        // <a href="https://www.multitravel.com/venta-personalizada/argentina" target="_blank">
                         <picture>
                             <source
                                 media="(min-width: 1024px)"
@@ -97,15 +109,6 @@ const BannerTop = () => {
                                         : `${UrlImg}/bannerDesktop-2.webp`
                                 }
                             />
-                            <source
-                                media="(max-width: 767px)"
-                                srcSet={
-                                    showPackageImages
-
-                                        ? `${UrlImgPaquetes}/Paquetes/bannerMobile-2.webp`
-                                        : `${UrlImg}/bannerMobile-1.webp`
-                                }
-                            />
                             <img className="bannerTop__img"
                                 alt=""
                                 srcSet={
@@ -115,49 +118,59 @@ const BannerTop = () => {
                                 }
                             />
                         </picture>
-                    </a>
-                    {showPackageImages && (<Button style="btn btnStyleBannerPre" link="https://www.multitravel.com/venta-personalizada/argentina" text="Ver Más" />)}
-
+                        // </a>
+                    )}
+                    {showPackageImages && (
+                        <Button style="btn btnStyleBannerPre" link={showPackageImages ? "https://www.multitravel.com/venta-personalizada/argentina" : "https://www.multitravel.com/venta-personalizada/argentina"} text="Ver Más" />
+                    )}
                 </div>
-                <div id="bannerTop__rigth" className="main__container_right">
-                    <picture>
-                        <source
-                            media="(min-width: 1024px)"
-                            srcSet={
-                                showPackageImages
-                                    ? `${UrlImgPaquetes}/Paquetes/bannerDesktop-3.webp`
-                                    : `${UrlImg}/bannerDesktop-3.webp`
-                            }
-                        />
-                        <source
-                            media="(min-width: 768px) and (max-width: 1023px)"
-                            srcSet={
-                                showPackageImages
-                                    ? `${UrlImgPaquetes}/Paquetes/bannerDesktop-3.webp`
-                                    : `${UrlImg}/bannerDesktop-3.webp`
-                            }
-                        />
-                        <source
-                            media="(max-width: 767px)"
-                            srcSet={
-                                showPackageImages
-                                    ? `${UrlImgPaquetes}/Paquetes/bannerMobile-3.webp`
-                                    : `${UrlImg}/bannerMobile-3.webp`
-                            }
-                        />
-                        <img className="bannerTop__img"
-                            alt=""
-                            srcSet={
-                                showPackageImages
-                                    ? `${UrlImgPaquetes}/Paquetes/bannerDesktop-3.webp`
-                                    : `${UrlImg}/bannerDesktop-3.webp`
-                            }
-                        />
-                    </picture>
-                    {showPackageImages && (<Button style="btn btnStyleBannerPre" link="https://www.multitravel.com/venta-personalizada/brasil" text="Ver Más" />)};
+                <div id="bannerTop__right" className="main__container_right">
+                    {isMobile ? (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/brasil" : "https://www.multitravel.com/venta-personalizada/brasil"} target="_blank">
+                            <img className="bannerTop__img"
+                                alt=""
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/Paquetes/bannerMobile-3.webp`
+                                        : `${UrlImg}/bannerMobile-3.webp`
+                                }
+                            />
+                        </a>
+                    ) : (
+                        // <a href="https://www.multitravel.com/venta-personalizada/argentina" target="_blank">
+                        <picture>
+                            <source
+                                media="(min-width: 1024px)"
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/Paquetes/bannerDesktop-3.webp`
+                                        : `${UrlImg}/bannerDesktop-3.webp`
+                                }
+                            />
+                            <source
+                                media="(min-width: 768px) and (max-width: 1023px)"
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/Paquetes/bannerDesktop-3.webp`
+                                        : `${UrlImg}/bannerDesktop-3.webp`
+                                }
+                            />
+                            <img className="bannerTop__img"
+                                alt=""
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/Paquetes/bannerDesktop-3.webp`
+                                        : `${UrlImg}/bannerDesktop-3.webp`
+                                }
+                            />
+                        </picture>
+                        // </a>
+                    )}
+                    {showPackageImages && (
+                        <Button style="btn btnStyleBannerPre" link={showPackageImages ? "https://www.multitravel.com/venta-personalizada/brasil" : "https://www.multitravel.com/venta-personalizada/brasil"} text="Ver Más" />
+                    )}
                 </div>
             </div>
-            
         </>
     )
 }
