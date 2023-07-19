22a530db4ea12350412d24c6458a4901ba26f738
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-19 14:43:18 -0300

    mult287

--- General/addHeaderLinks.js
+++ General/addHeaderLinks.js
@@ -106,6 +106,22 @@ function addHeaderLinks() {
 
 }
 addHeaderLinks();
+// ***********************************MULT-287 /Ocultar nacionalidad*************************
+function ocultarNacionalidad() {
+    const element = document.querySelector('label[for="hotel-searcher-nationality"]');
+    const parent = element.closest('.col-xs-12.col-sm-12.col-md-3');
+    parent.style.display = 'none';
+
+    if (window.innerWidth >= 768) {
+        const elementFixedProduct = document.querySelector('.searcher__fixed-product');
+        const parentFixedProduct = elementFixedProduct.closest('.col-xs-12.col-sm-12.col-md-3');
+        parentFixedProduct.style.width = '50%';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+    ocultarNacionalidad()
+});
 
 // **************** MULT-317 **********************************************************
 
