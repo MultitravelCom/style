07501758ff3a21f53012eae5d2369cef491e66b1
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-15 16:07:17 -0300

    lading argentina

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -254,9 +254,9 @@ function Button(props) {
 
     const [scriptExecuted, setScriptExecuted] = React.useState(false);
 
-    const bitrixScript = `
-        (function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn.bitrix24.com/b19657597/crm/form/loader_56.js');
-    `;
+    // const bitrixScript = `
+    //     (function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn.bitrix24.com/b19657597/crm/form/loader_56.js');
+    // `;
 
     const handleClick = (event) => {
         if (!scriptExecuted) {
