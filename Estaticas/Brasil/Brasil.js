c04f1072877f84e66562aedfbcded02e0e91956d
Multitravel.com <ignacio.peralta@multitravel.com>
2023-08-18 18:17:01 -0300

    bitrix

--- Estaticas/Brasil/Brasil.js
+++ Estaticas/Brasil/Brasil.js
@@ -490,7 +490,7 @@ function App() {
                     {isFormVisible && (
                         <div className="modalBitrix">
                             <div className="modal-content-Bitrix">
-                                <span className="close-button" onClick={handleCloseForm}>
+                                <span className="close-button-bitrix" onClick={handleCloseForm}>
                                     &times;
                                 </span>
                                 <BitrixFormTitle />
