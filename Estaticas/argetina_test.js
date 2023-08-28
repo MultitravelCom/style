77d53bc0f1255f92514631e939f612436f2c531d
Multitravel.com <ignacio.peralta@multitravel.com>
2023-08-28 17:41:14 -0300

    doble boton lading

--- Estaticas/argetina_test.js
+++ Estaticas/argetina_test.js
@@ -419,7 +419,7 @@ const Card = ({ destinos, onContactClick }) => {
     const handleBannerClick = (event) => {
         event.preventDefault();
         setOpenModal(true);
-        console.loog("click modal")
+        console.log("click modal")
     };
 
     return (
