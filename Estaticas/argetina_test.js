43515331a4eef1cbe6ce2d7071dfd60f00a37c13
Multitravel.com <ignacio.peralta@multitravel.com>
2023-08-28 18:15:41 -0300

    doble boton lading

--- Estaticas/argetina_test.js
+++ Estaticas/argetina_test.js
@@ -432,7 +432,7 @@ const Card = ({ destinos, onContactClick }) => {
                         <div className="priceStyle right">{destino.price}</div>
                     </div>
                     <div className="main__container__buttonsCars">
-                        <Button id={destino.id} className="btn_Style_Venta_Per classOpenModal" text="Contactarme" onClick={handleBannerClick} />
+                        <Button id={destino.id} className="btn_Style_Venta_Per classOpenModal" text="Contactarme" />
                         <Button id={destino.title} className="btn_Style_Venta_Per" text="Agendar" onClick={() => onContactClick(destino.id)} />
                     </div>
                 </div>
