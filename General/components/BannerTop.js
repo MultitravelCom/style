aefc44bb2a623126bfabfdf252b1253d0f4b8ec8
Multitravel.com <ignacio.peralta@multitravel.com>
2023-09-08 18:19:21 -0300

    update tarifas

--- General/components/BannerTop.js
+++ General/components/BannerTop.js
@@ -176,164 +176,164 @@ const BannerTop = () => {
                     <Cuponera />
                 </div>
             )}
-            {!showHotelsCoupons &&
-                (<div className="container-fluid main__container__bannerTop scroll-mobile">
-                    <div id={`bannerTop__left_${bannerId}`} className="main__container_left">
-                        {isMobile ? (
-                            <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/argentina" : "https://www.multitravel.com/hotels/"} target="_blank"
-                                onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-1")}
-                            >
+
+            <div className="container-fluid main__container__bannerTop scroll-mobile">
+                <div id={`bannerTop__left_${bannerId}`} className="main__container_left">
+                    {isMobile ? (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/argentina" : "https://www.multitravel.com/hotels/"} target="_blank"
+                            onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-1")}
+                        >
+                            <img className="bannerTop__img"
+                                alt=""
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/bannerMobile-1.webp`
+                                        : `${UrlImg}/bannerMobile-2.webp`
+                                }
+                            />
+                        </a>
+                    ) : (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/argentina" : "https://www.multitravel.com/hotels/"} target="_blank"
+                            onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-1")}
+                        >
+                            <picture>
+                                <source
+                                    media="(min-width: 1024px)"
+                                    srcSet={
+                                        showPackageImages
+                                            ? `${UrlImgPaquetes}/bannerDesktop-1.webp`
+                                            : `${UrlImg}/bannerDesktop-1.webp`
+                                    }
+                                />
+                                <source
+                                    media="(min-width: 768px) and (max-width: 1023px)"
+                                    srcSet={
+                                        showPackageImages
+                                            ? `${UrlImgPaquetes}/bannerDesktop-1.webp`
+                                            : `${UrlImg}/bannerDesktop-1.webp`
+                                    }
+                                />
                                 <img className="bannerTop__img"
                                     alt=""
                                     srcSet={
                                         showPackageImages
-                                            ? `${UrlImgPaquetes}/bannerMobile-1.webp`
-                                            : `${UrlImg}/bannerMobile-2.webp`
+                                            ? `${UrlImgPaquetes}/bannerDesktop-1.webp`
+                                            : `${UrlImg}/bannerDesktop-1.webp`
                                     }
                                 />
-                            </a>
-                        ) : (
-                            <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/argentina" : "https://www.multitravel.com/hotels/"} target="_blank"
-                                onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-1")}
-                            >
-                                <picture>
-                                    <source
-                                        media="(min-width: 1024px)"
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-1.webp`
-                                                : `${UrlImg}/bannerDesktop-1.webp`
-                                        }
-                                    />
-                                    <source
-                                        media="(min-width: 768px) and (max-width: 1023px)"
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-1.webp`
-                                                : `${UrlImg}/bannerDesktop-1.webp`
-                                        }
-                                    />
-                                    <img className="bannerTop__img"
-                                        alt=""
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-1.webp`
-                                                : `${UrlImg}/bannerDesktop-1.webp`
-                                        }
-                                    />
-                                </picture>
-                            </a>
-                        )}
-                        {/* {showPackageImages && (
+                            </picture>
+                        </a>
+                    )}
+                    {/* {showPackageImages && (
                         <Button style="btn btnStyleBannerPre" link="https://www.multitravel.com/venta-personalizada/caribe" text="Ver Más" />
                     )} */}
-                    </div>
-                    <div id={`bannerTop__center_${bannerId}`} className="main__container_center">
-                        {isMobile ? (
-                            <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/brasil" : null}
-                                target="_blank"
-                                onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-2")}
-                            >
+                </div>
+                <div id={`bannerTop__center_${bannerId}`} className="main__container_center">
+                    {isMobile ? (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/brasil" : null}
+                            target="_blank"
+                            onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-2")}
+                        >
 
+                            <img className="bannerTop__img"
+                                alt=""
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/bannerMobile-2.webp`
+                                        : `${UrlImg}/bannerMobile-1.webp`
+                                }
+                            />
+                        </a>
+                    ) : (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/brasil" : null}
+                            target="_blank"
+                            onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-2")}
+                        >
+                            <picture>
+                                <source
+                                    media="(min-width: 1024px)"
+                                    srcSet={
+                                        showPackageImages
+                                            ? `${UrlImgPaquetes}/bannerDesktop-2.webp`
+                                            : `${UrlImg}/bannerDesktop-2.webp`
+                                    }
+                                />
+                                <source
+                                    media="(min-width: 768px) and (max-width: 1023px)"
+                                    srcSet={
+                                        showPackageImages
+                                            ? `${UrlImgPaquetes}/bannerDesktop-2.webp`
+                                            : `${UrlImg}/bannerDesktop-2.webp`
+                                    }
+                                />
                                 <img className="bannerTop__img"
                                     alt=""
                                     srcSet={
                                         showPackageImages
-                                            ? `${UrlImgPaquetes}/bannerMobile-2.webp`
-                                            : `${UrlImg}/bannerMobile-1.webp`
+                                            ? `${UrlImgPaquetes}/bannerDesktop-2.webp`
+                                            : `${UrlImg}/bannerDesktop-2.webp`
                                     }
                                 />
-                            </a>
-                        ) : (
-                            <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/brasil" : null}
-                                target="_blank"
-                                onClick={showPackageImages ? null : (event) => scrollAncla(event, "home-sliding-offers-2")}
-                            >
-                                <picture>
-                                    <source
-                                        media="(min-width: 1024px)"
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-2.webp`
-                                                : `${UrlImg}/bannerDesktop-2.webp`
-                                        }
-                                    />
-                                    <source
-                                        media="(min-width: 768px) and (max-width: 1023px)"
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-2.webp`
-                                                : `${UrlImg}/bannerDesktop-2.webp`
-                                        }
-                                    />
-                                    <img className="bannerTop__img"
-                                        alt=""
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-2.webp`
-                                                : `${UrlImg}/bannerDesktop-2.webp`
-                                        }
-                                    />
-                                </picture>
-                            </a>
-                        )}
-                        {/* {showPackageImages && (
+                            </picture>
+                        </a>
+                    )}
+                    {/* {showPackageImages && (
                         <Button style="btn btnStyleBannerPre" link={showPackageImages ? "https://www.multitravel.com/venta-personalizada/argentina" : "https://www.multitravel.com/venta-personalizada/argentina"} text="Ver Más" />
                     )} */}
-                    </div>
-                    <div id={`bannerTop__right_${bannerId}`} className="main__container_right">
-                        {isMobile ? (
-                            <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/caribe" : null} target="_blank"
-                                onClick={showPackageImages ? null : null}
-                            >
+                </div>
+                <div id={`bannerTop__right_${bannerId}`} className="main__container_right">
+                    {isMobile ? (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/caribe" : null} target="_blank"
+                            onClick={showPackageImages ? null : null}
+                        >
+                            <img className="bannerTop__img"
+                                alt=""
+                                srcSet={
+                                    showPackageImages
+                                        ? `${UrlImgPaquetes}/bannerMobile-3.webp`
+                                        : `${UrlImg}/bannerMobile-3.webp`
+                                }
+                            />
+                        </a>
+                    ) : (
+                        <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/caribe" : null} target="_blank"
+                            onClick={showPackageImages ? null : null}
+                        >
+                            <picture>
+                                <source
+                                    media="(min-width: 1024px)"
+                                    srcSet={
+                                        showPackageImages
+                                            ? `${UrlImgPaquetes}/bannerDesktop-3.webp`
+                                            : `${UrlImg}/bannerDesktop-3.webp`
+                                    }
+                                />
+                                <source
+                                    media="(min-width: 768px) and (max-width: 1023px)"
+                                    srcSet={
+                                        showPackageImages
+                                            ? `${UrlImgPaquetes}/bannerDesktop-3.webp`
+                                            : `${UrlImg}/bannerDesktop-3.webp`
+                                    }
+                                />
                                 <img className="bannerTop__img"
                                     alt=""
                                     srcSet={
                                         showPackageImages
-                                            ? `${UrlImgPaquetes}/bannerMobile-3.webp`
-                                            : `${UrlImg}/bannerMobile-3.webp`
+                                            ? `${UrlImgPaquetes}/bannerDesktop-3.webp`
+                                            : `${UrlImg}/bannerDesktop-3.webp`
                                     }
                                 />
-                            </a>
-                        ) : (
-                            <a href={showPackageImages ? "https://www.multitravel.com/venta-personalizada/caribe" : null} target="_blank"
-                                onClick={showPackageImages ? null : null}
-                            >
-                                <picture>
-                                    <source
-                                        media="(min-width: 1024px)"
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-3.webp`
-                                                : `${UrlImg}/bannerDesktop-3.webp`
-                                        }
-                                    />
-                                    <source
-                                        media="(min-width: 768px) and (max-width: 1023px)"
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-3.webp`
-                                                : `${UrlImg}/bannerDesktop-3.webp`
-                                        }
-                                    />
-                                    <img className="bannerTop__img"
-                                        alt=""
-                                        srcSet={
-                                            showPackageImages
-                                                ? `${UrlImgPaquetes}/bannerDesktop-3.webp`
-                                                : `${UrlImg}/bannerDesktop-3.webp`
-                                        }
-                                    />
-                                </picture>
-                            </a>
-                        )}
-                        {/* {showPackageImages && (
+                            </picture>
+                        </a>
+                    )}
+                    {/* {showPackageImages && (
                         <Button style="btn btnStyleBannerPre" link={showPackageImages ? "https://www.multitravel.com/venta-personalizada/brasil" : "https://www.multitravel.com/venta-personalizada/brasil"} text="Ver Más" />
                     )} */}
-                    </div>
-                    <Modal open={openModal} onClose={() => setOpenModal(false)} />
-                </div>)
-            }
+                </div>
+                <Modal open={openModal} onClose={() => setOpenModal(false)} />
+            </div>
+
         </>
     )
 }
