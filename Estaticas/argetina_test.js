4db7f312ef0ee846b47b19d7467973b2cc67ea07
Multitravel.com <ignacio.peralta@multitravel.com>
2023-09-15 12:18:11 -0300

    api strapi

--- Estaticas/argetina_test.js
+++ Estaticas/argetina_test.js
@@ -552,7 +552,7 @@ const Card = ({ destinos, onContactClick }) => {
                         {console.log('Valor de destino.card:', destino.destino)}
                         {console.log('Valor de title.card:', destino.title)}
 
-                        {pricesByDestino[destino.title] && pricesByDestino[destino.title][destino.card] && (
+                        {pricesByDestino[destino.card] && (
                             <div className="main_container_priceStyle">
                                 <div className="priceStyle left">{pricesByDestino[destino.card]?.Tarifa_Temporada_Baja}</div>
                                 <div className="priceStyle right">{pricesByDestino[destino.card]?.Tarifa_Temporada_Alta}</div>
