fd17fd08350362502757896ecd9127e422e038b8
Multitravel.com <ignacio.peralta@multitravel.com>
2023-09-18 16:20:21 -0300

    strapi produ lading Argentina

--- Estaticas/argentina_satelite.js
+++ Estaticas/argentina_satelite.js
@@ -566,8 +566,8 @@ const Card = ({ destinos, onContactClick }) => {
                         {pricesByDestino[destino.destino] && (
                             pricesByDestino[destino.destino][destino.cardOrden].map((tarifa, index) => (
                                 <div key={index} className="main_container_priceStyle">
-                                    <div className="priceStyle left">${tarifa.Tarifa_Temporada_Bajato.toLocaleString().replace(/,/g, '.')}</div>
-                                    <div className="priceStyle right">$ {tarifa.Tarifa_Temporada_Altato.toLocaleString().replace(/,/g, '.')}</div>
+                                    <div className="priceStyle left">${tarifaTemporadaAlta}</div>
+                                    <div className="priceStyle right">$ {tarifaTemporadaBaja}</div>
                                 </div>
                             ))
                         )}
