101bb2588949da0f7c3f8ba7cef29a0bb2aba0d4
Multitravel.com <ignacio.peralta@multitravel.com>
2024-02-19 13:37:44 -0300

    cambio link logo header

--- General/addHeaderLinks.js
+++ General/addHeaderLinks.js
@@ -196,3 +196,14 @@ function disableMailchimpMobile() {
 }
 
 disableMailchimpMobile();
+// ***********************************************************************
+document.addEventListener('DOMContentLoaded', function () {
+    function cambiarHrefNuevoDestino() {
+        const linkLogo = document.querySelector('#logo');
+        if (linkLogo) {
+            linkLogo.href = "https://multitravel.com.ar/";
+        }
+    }
+
+    cambiarHrefNuevoDestino();
+});
